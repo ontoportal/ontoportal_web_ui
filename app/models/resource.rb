4d9d5dd29b1cb6d2491aeeaeae2a5534fb9e3516
Paul Alexander <palexander@stanford.edu>
2009-10-19 19:40:29 +0000

    Adding link to resource element id. Reformatting table in the annotations list.

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -2,6 +2,6 @@
 
 class Resource
 
-  attr_accessor :name,:url,:description,:logo,:count,:context_numbers,:annotations,:shortname,:main_context
+  attr_accessor :name,:url,:description,:logo,:count,:context_numbers,:annotations,:shortname,:main_context,:resource_element_url
   
 end
