2b68a43022de45a9857a310ee322893d0c617092
Paul Alexander <palexander@stanford.edu>
2009-11-04 21:47:53 +0000

    Adds parameter handling for redirects.

--- app/controllers/redirect_controller.rb
+++ app/controllers/redirect_controller.rb
@@ -3,10 +3,22 @@
 
 class RedirectController < ApplicationController
   def index
-    if params[:url]
-      redirect_to params[:url], :status=>:moved_permanently
+    if params[:url] # We were provided with a url to redirect to
+      params_string = ""
+      # Trigger to switch the placeholders
+      first_param = true
+      # Loop through the given params, ignore ones we know about or are provided by default
+      params.each do |param, value|
+        if !param.eql?("url") && !param.eql?("action") && !param.eql?("controller")
+          seperator = first_param ? "?" : "&"
+          first_param = false
+          params_string += seperator + param + "=" + value
+        end
+      end
+      # Redirect with params intact
+      redirect_to params[:url] + params_string, :status=>:moved_permanently
       return
-    else
+    else # Default redirect to the home page
       redirect_to "/", :status=>:moved_permanently
     end
   end
