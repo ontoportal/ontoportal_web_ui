a09114ade3f3fb61979a1a124a307e75713f84bc
Paul Alexander <palexander@stanford.edu>
2009-12-05 01:56:54 +0000

    Updates metrics to use new core logic to signify how many classes are found when the metric contains more than the arbitrary limit.

--- app/helpers/ontology_metrics_helper.rb
+++ app/helpers/ontology_metrics_helper.rb
@@ -1,53 +1,48 @@
 module OntologyMetricsHelper
   
   def class_list_info(ontology, metric, message, title)
-    if ontology.metrics.send("#{metric}").nil? || ontology.metrics.send("#{metric}").empty?
+    metrics = ontology.metrics
+    if metrics.send("#{metric}").nil? || metrics.send("#{metric}").empty?
       return
     end
     
     # Check to see if all properties are missing (author/doc only)
-    if defined? ontology.metrics.send(:"#{metric}Missing") && ontology.metrics.send(:"#{metric}Missing") == true
-      return "All properties are missing"
+    if metrics.send(:"#{metric}All") == true
+      return metrics.send(:"#{metric}_all")
     end
     
     markup = ""
     
     # Below we will use the 'send' method to call setters/getters based on the metric name we're looking for
-    if ontology.metrics.send(:"#{metric}Percentage") == 1
-      markup << "All classes #{message}"
-    elsif ontology.metrics.send(:"#{metric}LimitPassed") == true && ontology.metrics.numberOfClasses > OntologyMetricsWrapper::CLASS_LIST_LIMIT
-      all_missing = true
-      markup << "We estimate that all classes #{message}"
+    if metrics.send(:"#{metric}LimitPassed") != false
+      percentage = "%0.2f" % (metrics.send(:"#{metric}Percentage") * 100)
+      class_list_length = metrics.send(:"#{metric}LimitPassed")
+      markup << "#{class_list_length} / #{metrics.numberOfClasses} (#{percentage}%) of classes #{message}"
     else
-      percentage = "%0.2f" % (ontology.metrics.send(:"#{metric}Percentage") * 100)
-      class_list_length = ontology.metrics.send(:"#{metric}").length
-      markup << "#{class_list_length} / #{ontology.metrics.numberOfClasses} (#{percentage}%) of classes #{message}"
+      percentage = "%0.2f" % (metrics.send(:"#{metric}Percentage") * 100)
+      class_list_length = metrics.send(:"#{metric}").length
+      markup << "#{class_list_length} / #{metrics.numberOfClasses} (#{percentage}%) of classes #{message}"
     end
     
     markup << ' (<a class="thickbox" href="#TB_inline?height=600&width=800&inlineId=%metric%">details</a>)'.gsub("%title%", title).gsub("%metric%", metric)
     markup << '<div id="%metric%" style="display: none;"><div class="metrics">'.gsub("%metric%", metric)
     
-    if all_missing
-      markup << "<h2>Estimated metrics</h2>
-                 <p>Currently, BioPortal checks to see if an ontology is using a default property
-                 or one chosen by the ontology provider to indicate authorship or definitions for classes.
-                 However, many ontologies do not implement these properties. In order to prevent
-                 listing all classes for ontologies not implementing these properties, we only list the first 200
-                 and make the estimation that all classes in the ontology lack them. Future
-                 implementations will track the total number of classes that are missing these properties
-                 and provide more accurate statistics about the overall ontology.
-                 <a href=\"http://www.bioontology.org/wiki/index.php/Ontology_Metrics\">More information</a></p>"
+    # Message indicating why there are no details
+    if metrics.send(:"#{metric}LimitPassed") == true
+      markup << ""
+    else
+      markup << "<h2>#{title}</h2><p>"
+      metrics.send(:"#{metric}").each do | class_name, count | 
+        markup << "<a href=\"/visualize/#{ontology.id}/?conceptid=#{class_name}\">#{class_name}<\/a>"
+        if count
+          markup << " (#{count} subclasses)"
+        end
+        markup << "<br />"
+      end 
+      markup << "</p></div>"
     end
-    
-    markup << "<h2>#{title} list</h2><p>"
-    ontology.metrics.send(:"#{metric}").each do | class_name, count | 
-      markup << "#{class_name}" 
-      if count
-        markup << " (#{count} subclasses)"
-      end
-      markup << "<br />"
-    end 
-    markup << "</p></div></div>"
+      
+    markup << "</div>"
     
     return markup    
   end
--- app/models/ontology_metrics_wrapper.rb
+++ app/models/ontology_metrics_wrapper.rb
@@ -8,11 +8,11 @@ class OntologyMetricsWrapper
   attr_accessor :maximumDepth
   attr_accessor :maximumNumberOfSiblings
   attr_accessor :averageNumberOfSiblings
-  attr_accessor :classesWithOneSubclass
-  attr_accessor :classesWithMoreThanXSubclasses
-  attr_accessor :classesWithNoDocumentation
-  attr_accessor :classesWithNoAuthor
-  attr_accessor :classesWithMoreThanOnePropertyValue
+  cattr_accessor :classesWithOneSubclass
+  cattr_accessor :classesWithMoreThanXSubclasses
+  cattr_accessor :classesWithNoDocumentation
+  cattr_accessor :classesWithNoAuthor
+  cattr_accessor :classesWithMoreThanOnePropertyValue
   
   # Used to calculate percentage of classes in certain metrics
   CLASS_LIST_LIMIT = 200
@@ -22,15 +22,60 @@ class OntologyMetricsWrapper
   attr_accessor :classesWithNoDocumentationLimitPassed
   attr_accessor :classesWithNoAuthorLimitPassed
   attr_accessor :classesWithMoreThanOnePropertyValueLimitPassed
+  
   # Percentage of classes matching metric
   attr_accessor :classesWithOneSubclassPercentage
   attr_accessor :classesWithMoreThanXSubclassesPercentage
   attr_accessor :classesWithNoDocumentationPercentage
   attr_accessor :classesWithNoAuthorPercentage
   attr_accessor :classesWithMoreThanOnePropertyValuePercentage
-  # Are all of the following properties missing?
-  attr_accessor :classesWithNoDocumentationMissing
-  attr_accessor :classesWithNoAuthorMissing
-
   
+  # Are all of the following properties triggered for every class?
+  attr_accessor :classesWithOneSubclassAll
+  attr_accessor :classesWithMoreThanXSubclassesAll
+  attr_accessor :classesWithNoDocumentationAll
+  attr_accessor :classesWithNoAuthorAll
+  attr_accessor :classesWithMoreThanOnePropertyValueAll
+  
+  # List of all metrics with lists
+  METRICS_WITH_LISTS = ["classesWithOneSubclass", "classesWithMoreThanXSubclasses",
+      "classesWithNoDocumentation", "classesWithNoAuthor", "classesWithMoreThanOnePropertyValue"]
+  
+  # Strings for use in the metrics view
+  ONE_SUBCLASS_STRING = "No definition property specified or not values for the definition property."
+  MORE_THAN_X_SUBLCASSES_STRING = "No definition property specified or not values for the definition property."
+  DOCUMENTATION_MISSING_STRING = "No definition property specified or not values for the definition property."
+  AUTHOR_MISSING_STRING = "No author property specified or not values for the author property."
+  MORE_THAN_ONE_PROPERTY_STRING = "No definition property specified or not values for the definition property."
+  
+  # Initialize values
+  def initialize
+    self.classesWithOneSubclass = Array.new
+    self.classesWithMoreThanXSubclasses = Hash.new
+    self.classesWithNoDocumentation = Array.new
+    self.classesWithNoAuthor = Array.new
+    self.classesWithMoreThanOnePropertyValue = Array.new
+  end
+  
+  # Methods that return appropriate string when all classes are triggered for a given metric
+  def classesWithOneSubclass_all
+    return ONE_SUBCLASS_STRING
+  end
+
+  def classesWithMoreThanXSubclasses_all
+    return MORE_THAN_X_SUBLCASSES_STRING
+  end
+
+  def classesWithNoDocumentation_all
+    return DOCUMENTATION_MISSING_STRING
+  end
+
+  def classesWithNoAuthor_all
+    return AUTHOR_MISSING_STRING
+  end
+
+  def classesWithMoreThanOnePropertyValue_all
+    return MORE_THAN_ONE_PROPERTY_STRING
+  end
+
 end
