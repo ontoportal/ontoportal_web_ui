5a88b1f5165151b1db66058fbffa0265bf8325aa
Paul Alexander <palexander@stanford.edu>
2009-12-12 00:31:56 +0000

    Adds an admin page. Right now this only allows for dealing with memcache.

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,41 @@
+class AdminController < ApplicationController
+  
+  layout 'ontology'
+  
+  def index
+    unless !session[:user].nil? && session[:user].admin?
+      redirect_to :controller => 'login', :action => 'index'
+    end
+
+    @cache = CACHE
+
+    if params[:resetcache]
+      begin
+        @cache.reset
+      rescue
+        @status = "There was a problem reseting the cache connection"
+        render :partial => 'status'
+      end
+      
+      @status = "Cache connection successfully reset"
+      render :partial => 'status'
+    end
+    
+    if params[:clearcache]
+      begin
+        @cache.flush_all
+      rescue
+        @status = "There was a problem flushing the cache"
+        render :partial => 'status'
+      end
+      
+      @status = "Cache successfully flushed"
+      render :partial => 'status'
+    end
+    
+    puts @cache.inspect
+    
+    
+  end
+  
+end
