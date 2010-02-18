ed4a0f8069d6a6a62b68c60c98c2f22b642950c1
Paul Alexander <palexander@stanford.edu>
2010-02-18 00:22:06 +0000

    Updates email notification settings. Email addresses are now stored in bioportal_config.rb (this matches OOR's branch now).

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -1,49 +1,36 @@
 class Notifier < ActionMailer::Base
-  
-  
-  
   def signup(user)
     
   end
   
-  def lost_password( user,password )
+  def lost_password(user, password)
     # Email header info MUST be added here
     recipients user.email
     from  "admin@bioontology.org"
     subject "Password Reset"
   
     # Email body substitutions go here
-    body :user=> user, :password=>password
+    body :user => user, :password => password
   end
   
   def error(error)
-    
-    recipients "ngriff@stanford.edu"
+    recipients $ERROR_EMAIL
     from "admin@bioontology.org"
     subject "Exception Mailer"
     body :exception_message => error.message, :trace => error.backtrace
-    
   end
   
   def endlessloop(node)
-    recipients "ngriff@stanford.edu"
+    recipients $ERROR_EMAIL
     from "admin@bioontology.org"
     subject "Exception Mailer"
     body :node => node.inspect
-    
-    
-    
   end
   
   def feedback(name,email,comment)
-    recipients "bioontology-support@mailman.stanford.edu,#{email}"
+    recipients "#{$SUPPORT_EMAIL},#{email}"
     from "admin@bioontology.org"
     subject "Feedback"
-    body :name => name, :email=>email,:comment=>comment
-    
-    
-    
+    body :name => name, :email => email, :comment => comment
   end
-  
-  
 end
