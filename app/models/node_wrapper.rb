915b5912901bd143078e082a78a179c8403253c2
Paul Alexander <palexander@stanford.edu>
2010-03-25 18:52:30 +0000

    Adds check to make sure that relations contain valid information prior to processing.

--- app/models/node_wrapper.rb
+++ app/models/node_wrapper.rb
@@ -30,116 +30,118 @@ class NodeWrapper
         value.each do |relation_name,relation_value|
           case relation_name
             when "ChildCount"
-            self.child_size = relation_value.to_i
+              self.child_size = relation_value.to_i
             when "SubClass"
-            relation_value.each do |list_value|
-              self.children << NodeWrapper.new(list_value, params) unless list_value.empty?
-            end
-            self.children.sort! { |a,b| a.name.downcase <=> b.name.downcase } unless self.children.empty?
-          else
-            list_values = []
-            
-            unless relation_value.nil?
-              relation_value.each do |list_item|
-                if list_item.kind_of? Hash
-                  list_values << list_item['label'] rescue ""
-                else
-                  list_values << list_item
+              unless relation_value.nil?
+                relation_value.each do |list_value|
+                  self.children << NodeWrapper.new(list_value, params) unless list_value.empty?
+                end
+                self.children.sort! { |a,b| a.name.downcase <=> b.name.downcase } unless self.children.empty?
+              end
+            else
+              list_values = []
+              
+              unless relation_value.nil?
+                relation_value.each do |list_item|
+                  if list_item.kind_of? Hash
+                    list_values << list_item['label'] rescue ""
+                  else
+                    list_values << list_item
+                  end
                 end
               end
+              
+              self.properties[relation_name] = list_values.join(" | ")
             end
-            
-            self.properties[relation_name] = list_values.join(" | ")
+          end
+        else
+          begin
+            self.send("#{key}=", value)
+          rescue Exception
+            LOG.add :debug, "Missing '#{key}' attribute in NodeWrapper"
           end
         end
-      else
-        begin
-          self.send("#{key}=", value)
-        rescue Exception
-          LOG.add :debug, "Missing '#{key}' attribute in NodeWrapper"
-        end
+      end
+      
+      self.child_size = 0 if self.child_size.nil?
+    end
+    
+    def is_browsable
+      self.type.downcase.eql?("class") rescue ""
+    end
+    
+    def store(key, value)
+      begin
+        send("#{key}=", value)
+      rescue Exception
+        LOG.add :debug, "Missing '#{key}' attribute in NodeWrapper"
       end
     end
     
-    self.child_size = 0 if self.child_size.nil?
-  end
-  
-  def is_browsable
-    self.type.downcase.eql?("class") rescue ""
-  end
-  
-  def store(key, value)
-    begin
-      send("#{key}=", value)
-    rescue Exception
-      LOG.add :debug, "Missing '#{key}' attribute in NodeWrapper"
+    def name
+      @label
     end
-  end
-  
-  def name
-    @label
-  end
-  
-  def name=(value)
-    @label = value
-  end
-  
-  def to_param
+    
+    def name=(value)
+      @label = value
+    end
+    
+    def to_param
      "#{URI.escape(self.id,":/?#!")}"
-  end
-  
-  def ontology_name
-    return DataAccess.getOntology(self.version_id).displayLabel
-  end
-  
-  def ontology_id
-    return DataAccess.getOntology(self.version_id).ontologyId
-  end
-  
-  def mapping_count
-    if CACHE.get("#{self.ontology_id}::#{self.id.gsub(" ","%20")}_MappingCount").nil?
+    end
+    
+    def ontology_name
+      return DataAccess.getOntology(self.version_id).displayLabel
+    end
+    
+    def ontology_id
+      return DataAccess.getOntology(self.version_id).ontologyId
+    end
+    
+    def mapping_count
+      if CACHE.get("#{self.ontology_id}::#{self.id.gsub(" ","%20")}_MappingCount").nil?
         count = Mapping.count(:conditions=>{:source_ont => self.ontology_id, :source_id => self.id})
         CACHE.set("#{self.ontology_id}::#{self.id.gsub(" ","%20")}_MappingCount",count)
-      return count
-    else
-      return CACHE.get("#{self.ontology_id}::#{self.id.gsub(" ","%20")}_MappingCount")
+        return count
+      else
+        return CACHE.get("#{self.ontology_id}::#{self.id.gsub(" ","%20")}_MappingCount")
      end
    
    end
    
    def note_count
      if CACHE.get("#{self.ontology_id}::#{self.id.gsub(" ","%20")}_NoteCount").nil?
-      count = MarginNote.count(:conditions=>{:ontology_id => self.ontology_id, :concept_id =>self.id})
-      CACHE.set("#{self.ontology_id}::#{self.id.gsub(" ","%20")}_NoteCount",count)
+        count = MarginNote.count(:conditions=>{:ontology_id => self.ontology_id, :concept_id =>self.id})
+        CACHE.set("#{self.ontology_id}::#{self.id.gsub(" ","%20")}_NoteCount",count)
         return count
      else
         return CACHE.get("#{self.ontology_id}::#{self.id.gsub(" ","%20")}_NoteCount")
+      end
+      
     end
     
-  end
-  
-  def networkNeighborhood(relationships = nil)         
-    DataAccess.getNetworkNeighborhoodImage(self.ontology_name,self.id,relationships)
-  end
-  
-  def pathToRootImage(relationships = nil) 
-    DataAccess.getPathToRootImage(self.ontology_name,self.id,relationships)
-  end
-  
-  # def children(relationship=["is_a"])       
-  #   DataAccess.getChildNodes(self.ontology_name,self.id,relationship)
-  # end
-  
-  def parent(relationship=["is_a"])
+    def networkNeighborhood(relationships = nil)         
+      DataAccess.getNetworkNeighborhoodImage(self.ontology_name,self.id,relationships)
+    end
     
-    DataAccess.getParentNodes(self.ontology_name,self.id,relationship)
-  end
-  
-  def path_to_root
-    return DataAccess.getPathToRoot(self.version_id,self.id)    
-  end
-  
-  def to_s
+    def pathToRootImage(relationships = nil) 
+      DataAccess.getPathToRootImage(self.ontology_name,self.id,relationships)
+    end
+    
+    # def children(relationship=["is_a"])       
+    #   DataAccess.getChildNodes(self.ontology_name,self.id,relationship)
+    # end
+    
+    def parent(relationship=["is_a"])
+      
+      DataAccess.getParentNodes(self.ontology_name,self.id,relationship)
+    end
+    
+    def path_to_root
+      return DataAccess.getPathToRoot(self.version_id,self.id)    
+    end
+    
+    def to_s
      "Node_Name: #{self.name}  Node_ID: #{self.id}"
+    end
   end
-end
