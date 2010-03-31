87e253a9da6e6ae49362dfe42c6ef57219de6dd1
Paul Alexander <palexander@stanford.edu>
2010-03-31 01:18:07 +0000

    Replaces jQuery tag cloud plugin with a custom implementation.

---
+++ public/javascripts/bp_tagcloud.js
@@ -0,0 +1,23 @@
+/**
+ * @author palexander
+ */
+
+// Takes a jQuery selector list element where the elements have a value attribute set to the number of
+// items that the element represents. It them sets the font size based on how far from the average
+// that particular element's value is.
+function createTagCloud(list) { 
+	var mapping_average = 0;
+	
+	// Total the values
+	jQuery(list).children().each(function(){
+		mapping_average += parseInt(jQuery(this).attr("value"));
+	});
+	
+	// Get our average
+	mapping_average = mapping_average / jQuery(list).children().size();
+	
+	// Set font sizes
+	jQuery(list).children().each(function(){
+		jQuery(this).css("fontSize", parseInt(jQuery(this).attr("value")) / mapping_average * 150 + "%");
+	});
+}
