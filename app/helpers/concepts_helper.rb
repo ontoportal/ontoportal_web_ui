dcd5d59bc6d457501804e277e4c4b857bcb63c7d
Paul Alexander <palexander@stanford.edu>
2010-04-08 00:29:51 +0000

    Removes SuperClass, rdfs:subClassOf, and InstanceCount from the details tab view.

--- app/helpers/concepts_helper.rb
+++ app/helpers/concepts_helper.rb
@@ -1,4 +1,8 @@
 module ConceptsHelper
-     
-  
+
+  def include_relation?(relation_to_check)
+    excluded_relations = [ "type", "rdf:type", "[R]", "SuperClass", "rdfs:subClassOf", "InstanceCount" ]
+    excluded_relations.include?(relation_to_check)
+  end
+
 end
