60ada678720f7581653d892a23175cef90973cf7
Paul Alexander <palexander@stanford.edu>
2010-08-11 21:19:14 +0000

    Changes resource details to be pulled from Sean Falconer's new UI deployment at UVIC.

--- app/helpers/resources_helper.rb
+++ app/helpers/resources_helper.rb
@@ -1,2 +1,9 @@
 module ResourcesHelper
+  
+  def generate_details_link(ontology_id, resource, element, concept)
+    ontology = DataAccess.getOntology(ontology_id)
+    conceptid = ontology.is_latest? ? "#{ontology.ontologyId}/#{concept}&virtual=true" : "#{ontology.id}/#{concept}"
+    return "http://keg.cs.uvic.ca/seanf/BioportalTest/get_resource_element_context.php?resourceId=#{resource}&elementId=#{element}&conceptIds=#{conceptid}"
+  end
+  
 end
