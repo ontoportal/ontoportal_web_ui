1e2283663e808c3828629a0aa8d838ca39b4906d
Paul Alexander <palexander@stanford.edu>
2010-09-15 17:53:39 +0000

    Update resource UI location.

--- app/helpers/resources_helper.rb
+++ app/helpers/resources_helper.rb
@@ -3,7 +3,7 @@ module ResourcesHelper
   def generate_details_link(ontology_id, resource, element, concept)
     ontology = DataAccess.getOntology(ontology_id)
     conceptid = ontology.is_latest? ? "#{ontology.ontologyId}/#{concept}&virtual=true" : "#{ontology.id}/#{concept}"
-    return "http://keg.cs.uvic.ca/seanf/BioportalTest/get_resource_element_context.php?resourceId=#{resource}&elementId=#{element}&conceptIds=#{conceptid}"
+    return "http://ncbodev-riui1.stanford.edu/get_resource_element_context.php?resourceId=#{resource}&elementId=#{element}&conceptIds=#{conceptid}"
   end
   
 end
