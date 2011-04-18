797a9d5db61bae3723fdd65e2d377dcbc76ac892
Paul Alexander <palexander@stanford.edu>
2011-04-18 16:31:52 +0000

    Sorts group names alphabetically for [#3082] Display Ontology Groups in alphabetical order

--- app/models/groups.rb
+++ app/models/groups.rb
@@ -2,4 +2,14 @@ class Groups
 
   attr_accessor :group_list
   
+  def to_a
+    groups_array = []
+    self.group_list.each do |group_id, group|
+      groups_array << group
+    end
+    
+    groups_array.sort! {|a,b| a[:name] <=> b[:name]}
+    
+    groups_array
+  end
 end
