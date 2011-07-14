422028ce6f7c564554bf7299043deb1d57b380c8
Paul Alexander <palexander@stanford.edu>
2011-07-14 17:53:20 +0000

    Adds api key to the download_latest URL so it will pass properly

--- app/controllers/ontologies_controller.rb
+++ app/controllers/ontologies_controller.rb
@@ -81,7 +81,7 @@ class OntologiesController < ApplicationController
   
   def download_latest
     @ontology = DataAccess.getLatestOntology(params[:id])
-    redirect_to $REST_URL + "/ontologies/download/#{@ontology.id}?apikey=#{params[:apikey]}"
+    redirect_to $REST_URL + "/ontologies/download/#{@ontology.id}?apikey=#{$API_KEY}"
   end
   
   def update
