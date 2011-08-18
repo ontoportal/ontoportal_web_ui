c73abfc6135f32c3df350106742c2c26b85c1b28
Paul Alexander <palexander@stanford.edu>
2011-08-18 02:11:30 +0000

    Recommender implementation that uses ontology list filter

--- app/controllers/recommender_controller.rb
+++ app/controllers/recommender_controller.rb
@@ -32,22 +32,6 @@ class RecommenderController < ApplicationController
     
     recommendations = DataAccess.createRecommendation(text, ontology_ids)
     
-    recommendations_filtered = []
-    recommendations.each do |rec|
-      includes_group = DataAccess.getOntology(rec["virtualOntologyId"]).groups.include?(params[:group]) rescue false
-      includes_category = DataAccess.getOntology(rec["virtualOntologyId"]).categories.include?(params[:category]) rescue false
-      
-      if !params[:group].nil? && includes_group && !recommendations_filtered.include?(rec)
-        recommendations_filtered << rec
-      end
-      
-      if !params[:category].nil? && includes_category && !recommendations_filtered.include?(rec)
-        recommendations_filtered << rec
-      end
-    end
-    
-    recommendations_filtered = recommendations if recommendations_filtered.empty?
-    
-    render :json => recommendations_filtered
+    render :json => recommendations
   end
 end
