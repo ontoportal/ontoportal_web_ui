3adb34ddc7c45d35b8ee5dc02eddbcd0aecba827
Paul Alexander <palexander@stanford.edu>
2011-08-22 22:06:19 +0000

    Stop filtering equivalentClasses

--- app/helpers/concepts_helper.rb
+++ app/helpers/concepts_helper.rb
@@ -1,7 +1,7 @@
 module ConceptsHelper
 
   def include_relation?(relation_to_check)
-    excluded_relations = [ "type", "rdf:type", "[R]", "SuperClass", "InstanceCount", "owl:equivalentClass", "equivalentClass" ]
+    excluded_relations = [ "type", "rdf:type", "[R]", "SuperClass", "InstanceCount" ]
 
     excluded_relations.each do |relation|
       return true if relation_to_check.include?(relation)
