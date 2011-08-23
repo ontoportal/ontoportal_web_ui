14ae6c7a7efb834a5cfb444d110bea84df48bf77
Paul Alexander <palexander@stanford.edu>
2011-08-23 20:15:22 +0000

    Fix for problem removing user from private ontology ACL

--- app/controllers/recommender_controller.rb
+++ app/controllers/recommender_controller.rb
@@ -2,28 +2,6 @@ class RecommenderController < ApplicationController
   layout 'ontology'
 
   def index
-    ontologies = DataAccess.getOntologyList
-    groups = DataAccess.getGroups.to_a
-    categories = DataAccess.getCategories
-
-    @groups_for_select = []
-    groups.each do |group|
-      @groups_for_select << [ group[:name], group[:id] ]
-    end
-    
-    @categories_for_select = []
-    categories.each do |cat_id, cat|
-      @categories_for_select << [ cat[:name], cat[:id] ]
-    end
-
-    @onts_for_select = []
-    @onts_for_js = [];
-    ontologies.each do |ont|
-      abbreviation = ont.abbreviation.nil? ? "" : "(" + ont.abbreviation + ")"
-      @onts_for_select << [ont.displayLabel.strip + " " + abbreviation, ont.ontologyId.to_i]
-      @onts_for_js << "\"#{ont.displayLabel.strip} #{abbreviation}\": \"#{abbreviation.gsub("(", "").gsub(")", "")}\""
-    end
-    @onts_for_select.sort! { |a,b| a[0].downcase <=> b[0].downcase }
   end
   
   def create
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -243,6 +243,18 @@ module ApplicationHelper
     BLOCK
   end
   
+  def init_recommender
+    ontologies = DataAccess.getOntologyList
+
+    @onts_for_select = []
+    @onts_for_js = [];
+    ontologies.each do |ont|
+      abbreviation = ont.abbreviation.nil? ? "" : "(" + ont.abbreviation + ")"
+      @onts_for_select << [ont.displayLabel.strip + " " + abbreviation, ont.ontologyId.to_i]
+      @onts_for_js << "\"#{ont.displayLabel.strip} #{abbreviation}\": \"#{abbreviation.gsub("(", "").gsub(")", "")}\""
+    end
+    @onts_for_select.sort! { |a,b| a[0].downcase <=> b[0].downcase }
+  end
   
   
   
