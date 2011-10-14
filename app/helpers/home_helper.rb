858528f00ff5e5dfb29a20159c06b7457eaa5b0e
Paul Alexander <palexander@stanford.edu>
2011-10-14 17:23:02 +0000

    Decrease help page cache time

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -10,7 +10,7 @@ module HomeHelper
     if help_text.nil?
       help_page = Hpricot(open($WIKI_HELP_PAGE))
       help_text = (help_page/"//*[@id='bodyContent']").inner_html
-      CACHE.set("help_text", help_text, 60*60*24)
+      CACHE.set("help_text", help_text, 60*60)
     end
 
     return help_text
