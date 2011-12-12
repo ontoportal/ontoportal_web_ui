71395f129cf01772776dfae157c03c8232efdf47
Paul Alexander <palexander@stanford.edu>
2011-12-12 23:26:21 +0000

    Fixes for Recommender to set default options

--- app/controllers/recommender_controller.rb
+++ app/controllers/recommender_controller.rb
@@ -8,6 +8,10 @@ class RecommenderController < ApplicationController
     text = params[:text]
     ontology_ids = params[:ontology_ids]
 
+    # Default values for UI
+    params[:hierarchy] = 5
+    params[:normalization] = 2
+
     recommendations = DataAccess.createRecommendation(text, ontology_ids, params)
 
     render :json => recommendations
