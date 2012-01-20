0305a8476ea350c6891c57878912df1b42a77f49
Paul Alexander <palexander@stanford.edu>
2012-01-20 23:29:53 +0000

    Add 'Unavailable' in the case where ontologies cannot be downloaded

--- app/helpers/ontologies_helper.rb
+++ app/helpers/ontologies_helper.rb
@@ -1,5 +1,5 @@
 module OntologiesHelper
-  
+
   # Provides a link or string based on the status of an ontology.
   def get_status_text(ontology, params = {})
     # Don't display a link for ontologies that aren't browsable
@@ -25,7 +25,7 @@ module OntologiesHelper
       return "archived"
     end
   end
-  
+
   # Link for private/public/licensed ontologies
   def visibility_link(ontology)
     if ontology.metadata_only?
@@ -55,7 +55,7 @@ module OntologiesHelper
       return "Archived, not available to explore"
     end
   end
-  
+
   # Creates a link based on the status of an ontology
   def status_link(ontology)
     version_text = ontology.versionNumber.nil? || ontology.versionNumber.length == 0 ? "unknown" : ontology.versionNumber
@@ -74,7 +74,7 @@ module OntologiesHelper
     else
       status_text = ""
     end
-    
+
     if ontology.valid_tree_view?
       return "<a href='/ontologies/#{ontology.id}?p=terms'>#{version_text}</a> <span style='font-size: x-small; color: gray; padding-left: 10px;'>#{status_text}</span>"
     else
@@ -89,7 +89,7 @@ module OntologiesHelper
     unless $NOT_DOWNLOADABLE.nil?
       $NOT_DOWNLOADABLE.each do |virtual_id|
         if ontology.ontologyId.eql?(virtual_id.to_s)
-          return ""
+          return "<span style='cursor: help;' title='Ontology download is unavailable because of licensing restrictions'>Unavailable</span>"
         end
       end
     end
@@ -112,10 +112,10 @@ module OntologiesHelper
         return "<a href='#{$REST_URL}/diffs/download/#{diff[0]}/#{diff[1]}?format=txt'>Diff</a>"
       end
     end
-    
+
     return ""
   end
-  
+
   # Generates an array for use with version drop-down lists
   def get_versions_array_for_select(ontology_version_id)
     ontology = DataAccess.getOntology(ontology_version_id)
@@ -124,37 +124,37 @@ module OntologiesHelper
     ont_versions.each {|ont| ont_versions_array << [ ont.versionNumber, ont.id ] }
     return ont_versions_array
   end
-  
+
   def get_ont_icons(ontology)
     mappings_count = DataAccess.getMappingCountOntologiesHash
     mappings_count = mappings_count[ontology.ontologyId].to_i
     notes_count = DataAccess.getNotesCounts
     notes_count = notes_count[ontology.ontologyId].to_i
-    
+
     formatting_options = { :thousand => "K", :million => "M", :billion => "B" }
-    
+
     mappings_count_formatted = (mappings_count.nil? || mappings_count == 0) ? "" : number_to_human(mappings_count, :precision => 0, :units => formatting_options).gsub(" ", "")
     notes_count_formatted = (notes_count.nil? || notes_count == 0) ? "" : number_to_human(notes_count, :units => formatting_options).gsub(" ", "")
-    
+
     links = ""
     links << "<a class='ont_icons' href=''>T</a>"
     links << "<a class='ont_icons' href=''>M<span class='ont_counts'>#{mappings_count_formatted}</span></a>"
     links << "<a class='ont_icons' href=''>N<span class='ont_counts'>#{notes_count_formatted}</span></a>"
   end
-  
+
   def terms_link(ontology, count)
     loc = ontology.metadata_only? ? "summary" : "terms"
-    
+
     if count.nil? || count == 0
       return "0"
     else
       return "<a href='/ontologies/#{ontology.ontologyId}/?p=#{loc}'>#{number_with_delimiter(count, :delimiter => ',')}</a>"
     end
   end
-  
+
   def mappings_link(ontology, count)
     loc = "mappings"
-    
+
     if count.nil? || count == 0
       return "0"
     else
@@ -164,7 +164,7 @@ module OntologiesHelper
 
   def notes_link(ontology, count)
     loc = "notes"
-    
+
     if count.nil? || count == 0
       return "0"
     else
