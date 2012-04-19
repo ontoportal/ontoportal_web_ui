a8513e19e0811687b7c248f2edeef223c0b3acdc
Paul Alexander <palexander@stanford.edu>
2012-04-19 23:33:13 +0000

    Remove console output

--- app/controllers/ajax_proxy_controller.rb
+++ app/controllers/ajax_proxy_controller.rb
@@ -10,7 +10,6 @@ class AjaxProxyController < ApplicationController
     
     page = open(params[:url])
     content =  page.read
-    puts content
     render :text => content
     
   end
@@ -41,17 +40,17 @@ class AjaxProxyController < ApplicationController
 
   def render_json(json, options={})  
     callback, variable = params[:callback], params[:variable]  
-	response = begin  
-	  if callback && variable  
-	    "var #{variable} = #{json};\n#{callback}(#{variable});"  
-	  elsif variable  
-	    "var #{variable} = #{json};"  
-	  elsif callback  
-	    "#{callback}(#{json});"  
-	  else  
-	    json  
-	  end  
-	end  
+  	response = begin  
+  	  if callback && variable  
+  	    "var #{variable} = #{json};\n#{callback}(#{variable});"  
+  	  elsif variable  
+  	    "var #{variable} = #{json};"  
+  	  elsif callback  
+  	    "#{callback}(#{json});"  
+  	  else  
+  	    json  
+  	  end  
+  	end  
     render({:content_type => :js, :text => response}.merge(options))
   end
 
