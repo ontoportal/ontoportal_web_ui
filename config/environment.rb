e8ef8a5018c11f0ae8c9e213b7117712f59e5568
Paul Alexander <palexander@stanford.edu>
2012-05-24 18:44:01 +0000

    Remove dependency on resource index gem

--- config/environment.rb
+++ config/environment.rb
@@ -56,7 +56,5 @@ Rails::Initializer.run do |config|
 
   # Make Active Record use UTC-base instead of local time
   # config.active_record.default_timezone = :utc
-
-  config.gem "ncbo_resource_index"
 end
 
