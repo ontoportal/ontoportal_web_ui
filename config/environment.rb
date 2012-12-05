2a61a53091d9145ca7a6220ddba65ff80ba111f2
Paul R Alexander <palexander@stanford.edu>
2012-12-05 12:30:21 -0800

    Set default encoding for Ruby at runtime

--- config/environment.rb
+++ config/environment.rb
@@ -10,6 +10,10 @@ RAILS_GEM_VERSION = '2.3.14' unless defined? RAILS_GEM_VERSION
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
+# Set default encoding
+Encoding.default_external = Encoding::UTF_8
+Encoding.default_internal = Encoding::UTF_8
+
 Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
