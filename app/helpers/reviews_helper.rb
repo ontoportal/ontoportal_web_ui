ebc781c748cfd0ec70ee12529347f9184908f441
Paul R Alexander <palexander@stanford.edu>
2013-04-12 18:31:04 -0700

    Massage ratings

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,12 @@
 module ReviewsHelper
+  def organize_ratings(review)
+    return [
+      { name: :usability, value: review.usabilityRating },
+      { name: :coverage, value: review.coverageRating },
+      { name: :quality, value: review.qualityRating },
+      { name: :formality, value: review.formalityRating },
+      { name: :correctness, value: review.correctnessRating },
+      { name: :documentation, value: review.documentationRating }
+    ]
+  end
 end
