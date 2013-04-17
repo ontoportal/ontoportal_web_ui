ca6a01a03265b9ca71ef1f430b35ec8fc43e8e4f
Paul R Alexander <palexander@stanford.edu>
2013-04-17 15:08:51 -0700

    Migrate controller to new client

--- app/controllers/concepts_controller.rb
+++ app/controllers/concepts_controller.rb
@@ -27,47 +27,30 @@ class ConceptsController < ApplicationController
       return
     end
 
-    @ontology = DataAccess.getOntology(params[:ontology])
+    @ontology = LinkedData::Client::Models::Ontology.find_by_acronym(params[:ontology]).first
+    @submission = @ontology.explore.latest_submission
 
-    if @ontology.statusId.to_i.eql?(6)
-      @latest_ontology = DataAccess.getLatestOntology(@ontology.ontologyId)
-      params[:ontology] = @latest_ontology.id
-      flash[:notice] = "The version of <b>#{@ontology.displayLabel}</b> you were attempting to view (#{@ontology.versionNumber}) has been archived and is no longer available for exploring. You have been redirected to the most recent version (#{@latest_ontology.versionNumber})."
-      concept_id = params[:id] ? "?conceptid=#{params[:id]}" : ""
-      redirect_to "/visualize/#{@latest_ontology.id}#{concept_id}", :status => :moved_permanently
-      return
-    end
+    # TODO_REV: Support moving from archived to latest version
+    # if @ontology.explore.latest_submission.submissionStatus == 6
+    #   @latest_ontology = DataAccess.getLatestOntology(@ontology.ontologyId)
+    #   params[:ontology] = @latest_ontology.id
+    #   flash[:notice] = "The version of <b>#{@ontology.displayLabel}</b> you were attempting to view (#{@ontology.versionNumber}) has been archived and is no longer available for exploring. You have been redirected to the most recent version (#{@latest_ontology.versionNumber})."
+    #   concept_id = params[:id] ? "?conceptid=#{params[:id]}" : ""
+    #   redirect_to "/visualize/#{@latest_ontology.id}#{concept_id}", :status => :moved_permanently
+    #   return
+    # end
 
-    # If we're looking for children, just use the light version of the call
-    if params[:callback].eql?("children")
-      if too_many_children_override
-        @concept = DataAccess.getNode(@ontology.id, params[:id], 99999999999)
-      else
-        @concept = DataAccess.getNode(@ontology.id, params[:id])
-      end
-    else
-      @concept = DataAccess.getNode(@ontology.id, params[:id])
-    end
+    @concept = @ontology.explore.single_class(params[:id])
 
     if @concept.nil?
       raise Error404
     end
 
-    # This handles special cases where a passed concept id is for a concept
-    # that isn't browsable, usually a property for an ontology.
-    if !@concept.is_browsable
-      render :partial => "shared/not_browsable", :layout => "ontology"
-      return
-    end
-
     if request.xhr?
       show_ajax_request # process an ajax call
     else
-      # We only want to log concept loading, not showing a list of child concepts
-      LOG.add :info, 'visualize_concept_direct', request, :ontology_id => @ontology.id, :virtual_id => @ontology.ontologyId, :ontology_name => @ontology.displayLabel, :concept_name => @concept.label, :concept_id => @concept.id if @concept && @ontology
-
       show_uri_request # process a full call
-      render :file=> '/ontologies/visualize',:use_full_path =>true, :layout=>'ontology' # done this way to share a view
+      render :file=> '/ontologies/visualize', :use_full_path => true, :layout => 'ontology'
     end
   end
 
@@ -94,84 +77,14 @@ class ConceptsController < ApplicationController
     end
 
     # Set the ontology we are viewing
+    # TODO_REV: Handle ontology views
     if view
-      @ontology = DataAccess.getView(params[:ontology])
+      @ontology = nil
     else
-      @ontology = DataAccess.getOntology(params[:ontology])
+      @ontology = LinkedData::Client::Models::Ontology.find_by_acronym(params[:ontology]).first
     end
 
-    if !@ontology.flat? && (!params[:conceptid] || params[:conceptid].empty? || params[:conceptid].eql?("root"))
-      # get the top level nodes for the root
-      @root = TreeNode.new()
-      nodes = @ontology.top_level_nodes(view)
-      nodes.sort!{|x,y| x.label.downcase<=>y.label.downcase}
-      for node in nodes
-        if node.label.downcase.include?("obsolete") || node.label.downcase.include?("deprecated")
-          nodes.delete(node)
-          nodes.push(node)
-        end
-      end
-
-      @root.set_children(nodes, @root)
-
-      # get the initial concepts to display
-      @concept = DataAccess.getNode(@ontology.id, @root.children.first.id, nil, view)
-
-      # Some ontologies have "too many children" at their root. These will not process and are handled here.
-      if @concept.nil?
-        raise Error404
-      end
-
-      LOG.add :info, 'visualize_ontology', request, :ontology_id => @ontology.id, :virtual_id => @ontology.ontologyId, :ontology_name => @ontology.displayLabel, :concept_name => @concept.label, :concept_id => @concept.id
-    elsif @ontology.flat? && (!params[:conceptid] || params[:conceptid].empty?)
-      # Don't display any terms in the tree
-      @concept = NodeWrapper.new
-      @concept.label = "Please search for a term using the Jump To field above"
-      @concept.id = "bp_fake_root"
-      @concept.fullId = "bp_fake_root"
-      @concept.child_size = 0
-      @concept.properties = {}
-      @concept.version_id = @ontology.id
-      @concept.children = []
-
-      @tree_concept = TreeNode.new(@concept)
-
-      @root = TreeNode.new
-      @root.children = [@tree_concept]
-    elsif @ontology.flat? && params[:conceptid]
-      # Display only the requested term in the tree
-      @concept = DataAccess.getNode(@ontology.id, params[:conceptid], nil, view)
-      @concept.children = []
-      @concept.child_size = 0
-      @root = TreeNode.new
-      @root.children = [TreeNode.new(@concept)]
-    else
-      # if the id is coming from a param, use that to get concept
-      @concept = DataAccess.getNode(@ontology.id,params[:conceptid],view)
-
-      raise Error404 if @concept.nil?
-
-      # Did we come from the Jump To widget, if so change logging
-      if params[:jump_to_nav]
-        LOG.add :info, 'jump_to_nav', request, :ontology_id => @ontology.id, :virtual_id => @ontology.ontologyId, :ontology_name => @ontology.displayLabel, :concept_name => @concept.label, :concept_id => @concept.id
-      else
-        LOG.add :info, 'visualize_concept_direct', request, :ontology_id => @ontology.id, :virtual_id => @ontology.ontologyId, :ontology_name => @ontology.displayLabel, :concept_name => @concept.label, :concept_id => @concept.id
-      end
-
-      # This handles special cases where a passed concept id is for a concept
-      # that isn't browsable, usually a property for an ontology.
-      if !@concept.is_browsable
-        render :partial => "shared/not_browsable", :layout => "ontology"
-        return
-      end
-
-      # Create the tree
-      rootNode = @concept.path_to_root
-      raise Error404 if rootNode.nil?
-
-      @root = TreeNode.new()
-      @root.set_children(rootNode.children, rootNode)
-    end
+    get_class(params)
 
     render :partial => "ontologies/treeview"
   end
@@ -305,20 +218,10 @@ class ConceptsController < ApplicationController
     when 'load' # Load pulls in all the details of a node
       time = Time.now
       gather_details
-      LOG.add :debug, "Processed concept details (#{Time.now - time})"
-
-      # We only want to log concept loading, not showing a list of child concepts
-      LOG.add :info, 'visualize_concept_browse', request, :ontology_id => @ontology.id, :virtual_id => @ontology.ontologyId, :ontology_name => @ontology.displayLabel, :concept_name => @concept.label, :concept_id => @concept.id if @concept && @ontology
-
       render :partial => 'load'
     when 'children' # Children is called only for drawing the tree
-      @children =[]
-      start_tree = Time.now
-      for child in @concept.children
-        @children << TreeNode.new(child, @concept)
-        @children.sort!{|x,y| x.label.downcase<=>y.label.downcase} unless @children.empty?
-      end
-      LOG.add :debug,  "Get children (#{Time.now - start_tree})"
+      @children = @concept.children || []
+      @children.sort!{|x,y| x.prefLabel.downcase<=>y.prefLabel.downcase} unless @children.empty?
       render :partial => 'childNodes'
     end
   end
@@ -331,15 +234,17 @@ class ConceptsController < ApplicationController
 
     # gathers the information for a node
     def gather_details
-      @mappings = DataAccess.getConceptMappings(@concept.ontology.ontologyId, @concept.id)
+      # TODO_REV: Support mappings for classes
+      # @mappings = DataAccess.getConceptMappings(@concept.ontology.ontologyId, @concept.id)
+      # TODO_REV: Support deleting mappings
       # check to see if user should get the option to delete
-      @delete_mapping_permission = check_delete_mapping_permission(@mappings)
+      # @delete_mapping_permission = check_delete_mapping_permission(@mappings)
       update_tab(@ontology, @concept.id) #updates the 'history' tab with the current node
     end
 
     def build_tree
       # find path to root
-      rootNode = @concept.path_to_root
+      rootNode = @concept.explore.tree.first
       @root = TreeNode.new()
       @root.set_children(rootNode.children, rootNode) unless rootNode.nil?
     end
