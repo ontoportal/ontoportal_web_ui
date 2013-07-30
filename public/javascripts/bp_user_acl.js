c6084368a8c176986c126e15640cd22475469f68
Paul R Alexander <palexander@stanford.edu>
2013-07-30 12:26:28 -0700

    JS for basic haml version

--- public/javascripts/bp_user_acl.js
+++ public/javascripts/bp_user_acl.js
@@ -1,9 +1,9 @@
 jQuery(document).ready(function(){
-  // Select all users in the list by default  
-  jQuery("#ontology_useracl option").attr("selected","selected");
+  // Select all users in the list by default
+  jQuery("#ontology_acl option").attr("selected","selected");
 
   // Make the list styled with chosen
-  jQuery("#ontology_useracl").chosen();
+  jQuery("#ontology_acl").chosen();
 
   // Add account button wire
   jQuery("#user_acl_add_button").bind("click", validateUsername);
@@ -15,43 +15,43 @@ jQuery(document).ready(function(){
       jQuery("#user_acl_add_button").click();
     }
   });
-  
+
   // Maintain the user acl select list, remove unselected options every time somone click's the "X"
-  jQuery("#ontology_useracl").change(function(){
+  jQuery("#ontology_acl").change(function(){
     // Delete unselected options
-    jQuery("#ontology_useracl option").each(function(){
+    jQuery("#ontology_acl option").each(function(){
       if (!jQuery(this).attr("selected")) {
         jQuery(this).remove();
       }
     });
-    
-    jQuery("#ontology_useracl option").attr("selected","selected");
+
+    jQuery("#ontology_acl option").attr("selected","selected");
   });
 });
 
 validateUsername = function(event){
   jQuery(".user_acl_add_error").html("");
   jQuery(".user_acl_add_spinner").show();
-  
+
   jQuery.ajax({
         type: "POST",
         url: "/users/validate_username?username="+jQuery("#useracl_user_acl_add").val(),
         dataType: "json",
         success: function(data) {
           jQuery(".user_acl_add_spinner").hide();
-          
+
           if (data.userValid == true) {
             jQuery("#useracl_user_acl_add").val("");
             jQuery("#useracl_user_acl_add").focus();
-            
+
             // Only add the user to the list if they don't already exist
-            if (!jQuery("#ontology_useracl option[value='"+data.user.id+"']").length) {
-              jQuery('#ontology_useracl').append($('<option>', { "value" : data.user.id, "selected" : "selected" }).text(data.user.username));
-              jQuery("#ontology_useracl option").attr("selected","selected");
+            if (!jQuery("#ontology_acl option[value='"+data.user.id+"']").length) {
+              jQuery('#ontology_acl').append($('<option>', { "value" : data.user.id, "selected" : "selected" }).text(data.user.username));
+              jQuery("#ontology_acl option").attr("selected","selected");
             }
-            
-            jQuery('#ontology_useracl').trigger("change");
-            jQuery('#ontology_useracl').trigger("liszt:updated");
+
+            jQuery('#ontology_acl').trigger("change");
+            jQuery('#ontology_acl').trigger("liszt:updated");
           } else {
             jQuery(".user_acl_add_error").html(" Username does not exist");
           }
