ffc887a1c43f5c6ba94716975c11b3ad74c606d5
Paul R Alexander <palexander@stanford.edu>
2013-08-27 23:18:21 -0700

    History dropdown works now NCBO-110 #resolve

--- app/models/history.rb
+++ app/models/history.rb
@@ -1,9 +1,10 @@
 class History
-  attr_accessor :ontology_id,:ontology_name,:concept
-  
-  def initialize ontology_id,ontology_name, concept
+  attr_accessor :ontology_id, :ontology_name, :ontology_acronym, :concept
+
+  def initialize(ontology_id, ontology_name, ontology_acronym, concept)
     self.ontology_id = ontology_id
     self.ontology_name = ontology_name
+    self.ontology_acronym = ontology_acronym
     self.concept = concept
   end
 end
