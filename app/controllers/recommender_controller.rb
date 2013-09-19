e60a97c3e1db262447b9140c8c4a0e4599c59d06
Darren L. Weber, Ph.D <darren.weber@stanford.edu>
2013-09-18 17:22:11 -0700

    Recommender - extract javascript from view with include, etc.

--- app/controllers/recommender_controller.rb
+++ app/controllers/recommender_controller.rb
@@ -8,11 +8,15 @@ class RecommenderController < ApplicationController
   end
 
   def create
-
+    # Set defaults
+    params[:ontologies] ||= []
+    params[:hierarchy] ||= 5
+    params[:normalization] ||= 2
+    # Parse params
     text = params[:text].strip.gsub("\r\n", " ").gsub("\n", " ")
-    options = { :ontologies => params[:ontologies] ||= "",
-                :hierarchy => params[:hierarchy].to_i ||= 5,
-                :normalization => params[:normalization].to_i ||= 2,
+    options = { :ontologies => params[:ontologies],
+                :hierarchy => params[:hierarchy].to_i,
+                :normalization => params[:normalization].to_i,
                 #:max_level => params[:max_level].to_i ||= 0,
                 #:semanticTypes => params[:semanticTypes] ||= [],
                 #:mappings => params[:mappings] ||= [],
@@ -25,25 +29,23 @@ class RecommenderController < ApplicationController
     query += "&hierarchy=" + options[:hierarchy].to_s
     query += "&normalization=" + options[:normalization].to_s
     #query += "&max_level=" + options[:max_level].to_s
-    query += "&ontologies=" + CGI.escape(options[:ontologies]) unless options[:ontologies].empty?
+    query += "&ontologies=" + CGI.escape(options[:ontologies].join(',')) unless options[:ontologies].empty?
     #query += "&semanticTypes=" + options[:semanticTypes].join(',') unless options[:semanticTypes].empty?
     #query += "&mappings=" + options[:mappings].join(',') unless options[:mappings].empty?
     #query += "&wholeWordOnly=" + options[:wholeWordOnly].to_s unless options[:wholeWordOnly].empty?
     #query += "&withDefaultStopWords=" + options[:withDefaultStopWords].to_s unless options[:withDefaultStopWords].empty?
-
     recommendations = parse_json(query) # See application_controller.rb
     #recommendations = LinkedData::Client::HTTP.get(query)
     LOG.add :debug, "Retrieved #{recommendations.length} recommendations: #{Time.now - start}s"
+    #
+    # TODO: get all the annotated class prefLabel values.
+    #
     # Modify the annotated classes (methods in application_controller.rb)
-    massage_annotated_classes(recommendations, options) unless recommendations.empty?
-
-    binding.pry
-
-    sorted = recommendations.sort {|a,b| a['numTermsMatched'] <=> b['numTermsMatched'] }.reverse
+    #massage_annotated_classes(recommendations, options) unless recommendations.empty?
+    #sorted = recommendations.sort {|a,b| a['numTermsMatched'] <=> b['numTermsMatched'] }.reverse
+    sorted = recommendations.sort {|a,b| a['score'] <=> b['score'] }.reverse
     # Reduce the data package sent to the browser (via ajax)
     #simple = sorted.map {|r| [r['score'], r['ontology']['acronym'], r["numTermsMatched"], r["numTermsTotal"]] }
-
-
     # reduce data package size to suit reasonable display size
     render :json => sorted[0..24]
   end
