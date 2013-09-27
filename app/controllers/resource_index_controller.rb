e1736b252bc16a633a1a21e06c0faa22ed50607a
Darren L. Weber, Ph.D <darren.weber@stanford.edu>
2013-09-27 11:46:53 -0700

    NCBO-284 #resolve #comment post all RI ontology acronyms to REST /search

--- app/controllers/resource_index_controller.rb
+++ app/controllers/resource_index_controller.rb
@@ -62,34 +62,25 @@ class ResourceIndexController < ApplicationController
     end
     params[:q] = params[:q].strip
     params[:q] = params[:q] + '*' unless params[:q].end_with?("*") # Add wildcard
-    # check ontologies
-    # Resource Index ontologies - REST API filters them for those that are in the triple store.
-    # Data structure is a list of ontology models
-
-    # TODO: BEGIN TODO CHANGE BLOCK
-    # TODO: USE THIS WHEN THE SEARCH API WORKS FOR ALL ONTOLOGIES
-    #ri_ont_acronym_key = 'ri_ont_acronym_key'
-    #ri_ont_acronyms = Rails.cache.read(ri_ont_acronym_key)
-    #if ri_ont_acronyms.nil?
-    #  @ri_ontologies ||= get_resource_index_ontologies # application_controller
-    #  ri_ont_acronyms = @ri_ontologies.map {|o| o.acronym }.join(',')
-    #  # RI_CACHE_EXPIRY set in application controller
-    #  Rails.cache.write(ri_ont_acronym_key, ri_ont_acronyms, expires_in: RI_CACHE_EXPIRY)
-    #end
-    #params[:ontologies] = ri_ont_acronyms
-    # TODO: REMOVE THIS HACK WHEN THE SEARCH API WORKS FOR ALL ONTOLOGIES
-    params[:ontologies] = "GEOSPECIES,TEO,ICD9CM-KM,TMO,IDO,FB-CV,TMA,SIO,PSIMOD,RPO,BIRNLEX,PW,OPL,BP-METADATA,HOMERUN,CMS,GFO-BIO,FHHO,ONTODM-KDD,OGMS,BRO,PTO,MEDO,COGAT,HL7,COGPO,AAO,ICDO3,CANONT,CCONT,ONTOKBCF,MAO,EPILONT,EFO,TM-OTHER-FACTORS,I2B2-PATVISDIM,GENE-CDS,ICF,LHN,IEV,PEO,GLOB,VARIO,UCSFORTHO,OCRE,TM-SIGNS-AND-SYMPTS,NTDO,GLYCO,RCTONT,OPB,PAE,UCSFICD910CM,NHDS,SPD,CANCO,OMRSE,DDI,MEDDRA,SITBAC,REX,NMR,I2B2-LOINC,TOP-MENELAS,CDAO,TGMA,IXNO,NDDF,ICD9CM,ICNP,PR,BDO,OBIWS,ATC,MAT,TM-CONST,PMR,CHEBI,FAO,EPICMEDS,QUDT,ONTODM-CORE,BCO,MMO,TADS,PATHLEX,XAO,ERO,WB-PHENOTYPE,EDAM,GEXO,CNO,CHEMINF,GFO,FBbi,RS,DOID,CAO,EHDA,MIXSCV,OAE,ICPC2P,PHARE,CPT-KM,XEO,RH-MESH,HOM-TEST,UCSFEPIC,GRO-CPGA,HUGO,AMINO-ACID,SPTO,WHO-ART,BHO,MP,ELIXHAUSER,BP,LSM,MCCV,NCIT,AI-RHEUM,MDDB,COSTART,UCSFXPLANT,SPO,MO,NONRCTO,REXO,PLATSTG,ZIP5,IDODEN,LOINC,OGDI,GENETRIAL,pseudo,MPATH,MEDABBS,IMGT-ONTOLOGY,IMMDIS,OMIT,MESH,RNAO,VO,CLO,MCCL,OGR,ZFA,HCPCS,ICD10CM,GCC,HLTHINDCTRS,FB-DV,HAO,ICD10PCS,PSDS,CMO,ICD10,ENVO,RXNORM,CRISP,BTO,GRO,IDOMAL,CL,TTO,CPTAC,HINO,TAXRANK,VHOG,WSIO,PATO,CTCAE,APO,NCBITAXON,MPO,PPIO,NIFSUBCELL,USSOC,BILA,SNOMEDCT,IMR,ONTODT,SOY,LDA,NEUMORE,MEGO,MIRNAO,FB-SP,GALEN,BRIDG,ICD11-BODYSYSTEM,VIVO,AERO,SBO,EHDAA,CCO,GAZ,VT,ZEA,OGI,ICECI,ONTOPNEUMO,PROVO,RCD,UO,BCGO,NEOMARK3,IDOBRU,OSHPD,MCBCC,ROLEO,DERMLEX,BT,ONTOMA,EMAP,PRO-ONT,MS,DIKB,FYPO,SHR,ONSTR,MF,UCSFICU,XCO,WB-BT,PMA,SAO,NEMO,MDCDRG,DEMOGRAPH,PTRANS,CARO,UNITSONT,NEOMARK4,NIGO,OBOE-SBC,REPO,VSO,FDA-MEDDEVICE,OMIM,VANDF,ICD09,INO,EP,SSO,ATO,MFOEM,ACGT-MO,TOK,MFO,ECO,SBRO,SYN,SEP,EHDAA2,AEO,RETO,IAO,ADW,KISAO,NPO,PVONTO,CLINIC,HP,CTX,BSPO,ABA-AMB,PHYFIELD,MIRO,GRO-CPD,YPO,TM-MER,GPI,CO-WHEAT,DIAGONT,ZIP3,SWEET,SNPO,CTONT,ATMO,PORO,FMA,SYMP,CBO,HIMC-CPT,TEDDY,CPTH,EXO,CARELEX,HPIO,DC-CL,BAO,MA,CPRO,ICPS,CHEMBIO,TAO,VSAO,WB-LS,PROPREO,PCO,SOPHARM,MEDLINEPLUS,HOM,NDFRT,OOEVV,PHYLONT,GO-EXT,QIBO,SO,OBI,PDQ,FB-BT,IFAR,GO,BFO,MHC,CLIN-EVAL,SDO,NATPRO,ICPC,NBO,PEDTERM,DDANAT,JERM,PECO,PHENX,ECG,PO"
-    # TODO: END TODO CHANGE BLOCK
-
+    # Get Resource Index ontologies - REST API filters them for those that are in the triple store.
+    ri_ont_acronym_key = 'ri_ont_acronym_key'
+    ri_ont_acronyms = Rails.cache.read(ri_ont_acronym_key)
+    if ri_ont_acronyms.nil?
+      @ri_ontologies ||= get_resource_index_ontologies # application_controller
+      ri_ont_acronyms = @ri_ontologies.map {|o| o.acronym }.join(',')
+      # EXPIRY_RI_ONTOLOGIES set in application controller
+      Rails.cache.write(ri_ont_acronym_key, ri_ont_acronyms, expires_in: EXPIRY_RI_ONTOLOGIES)
+    end
+    params[:ontologies] = ri_ont_acronyms
     # Get the first 50 classes matching the query
     search_page = LinkedData::Client::Models::Class.search(params[:q], params)
     classes = simplify_classes( search_page.collection[0...50] ) # application_controller
-    response = classes.to_json
+    classes_json = classes.to_json
     if params[:response].eql?("json")
-      response = response.gsub("\"","'")
-      response = "#{params[:callback]}({data:\"#{response}\"})"
+      classes_json = classes_json.gsub("\"","'")
+      classes_json = "#{params[:callback]}({data:\"#{classes_json}\"})"
     end
-    render :text => response
+    render :text => classes_json
   end
 
 
