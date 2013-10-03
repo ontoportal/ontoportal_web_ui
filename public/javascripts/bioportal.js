49b89479004943fbdce7eccb3d497a43a384eef0
Paul R Alexander <palexander@stanford.edu>
2013-10-03 12:17:10 -0700

    Query string function that doesn't use closure

--- public/javascripts/bioportal.js
+++ public/javascripts/bioportal.js
@@ -478,6 +478,18 @@ function selectTab(id,tab){
     })(window.location.search.substr(1).split('&'))
 })(jQuery);
 
+function BP_queryString() {
+  var a = window.location.search.substr(1).split('&');
+  var b = {};
+  for (var i = 0; i < a.length; ++i)
+  {
+      var p=a[i].split('=');
+      if (p.length != 2) continue;
+      b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+  }
+  return b;
+}
+
 
 
 function bpPopWindow(e) {
