5e8fe8d2720ff2bde06c7f1a437e7afddce88b64
Paul R Alexander <palexander@stanford.edu>
2013-12-04 17:04:17 -0800

    Symbolize keys so the params hash looks like we expect

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -7,7 +7,7 @@ class LoginController < ApplicationController
     if params[:redirect]
       # Get the original, encoded redirect
       uri = URI.parse(request.url)
-      orig_params = Hash[uri.query.split("&").map {|e| e.split("=",2)}]
+      orig_params = Hash[uri.query.split("&").map {|e| e.split("=",2)}].symbolize_keys
       session[:redirect] = orig_params[:redirect]
     else
       session[:redirect] = request.referer
