230c1e283de69d6d56925e9d632d9e7c9409730d
Darren L. Weber, Ph.D <darren.weber@stanford.edu>
2013-12-05 16:29:04 -0800

    NCBO-447 #comment Revise user subscription controller - initial code to be reviewed and tested.

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,21 +1,41 @@
 class SubscriptionsController < ApplicationController
 
   def create
+    # Try to get the user linked data instance
     user_id = params[:user_id]
+    u = LinkedData::Client::Models::User.find(user_id)
+    raise Exception if u.nil?
+    # Try to get the ontology linked data instance
     ontology_id = params[:ontology_id]
-    subbed = params[:subbed]
-
-    if subbed.eql?("true")
-      subs = DataAccess.deleteUserSubscriptions(user_id, ontology_id, NOTIFICATION_TYPES[:notes])
-    elsif subbed.eql?("false")
-      subs = DataAccess.createUserSubscriptions(user_id, ontology_id, NOTIFICATION_TYPES[:notes])
+    if ontology_id.start_with? 'http'
+      ont = LinkedData::Client::Models::Ontology.find(ontology_id)
     else
-      raise Exception
+      ont = LinkedData::Client::Models::Ontology.find_by_acronym(ontology_id).first
     end
-
-    updated_sub = true
-
-    render :json => { :updated_sub => updated_sub, :user_subscriptions => subs }
+    raise Exception if ont.nil?
+    # Is this request to add or remove a subscription?
+    subscribed = params[:subbed]  # string (not boolean)
+    if subscribed.eql?("true")
+      # Already subscribed, so this request must be a delete
+      # Note that this routine removes ALL subscriptions for the ontology, regardless of type.
+      u.subscription.delete_if {|sub| sub[:ontology].eql?(ont.acronym) }
+    else
+      # Not subscribed yet, so this request must be for adding subscription
+      subscription = {ontology: ont.acronym, notification_type: NOTIFICATION_TYPES[:notes]}
+      u.subscription.push(subscription)
+    end
+    #
+    # TODO: DOUBLE CHECK THIS BEGIN/RESCUE BLOCK
+    #
+    begin
+      u.update # this calls with an HTTP PATCH to the user id
+      # How do we get success or failure status from u.update?
+      #updated_sub = u.valid?
+      updated_sub = true
+    rescue
+      updated_sub = false
+    end
+    render :json => { :updated_sub => updated_sub, :user_subscriptions => u.subscription }
   end
 
 end
