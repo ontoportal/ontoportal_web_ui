051c57f631d41ba355e52ca0ccc716157ac291a4
Paul R Alexander <palexander@stanford.edu>
2014-03-06 19:15:01 -0800

    Don't fail when old table is missing

--- db/migrate/20140306012927_drop_old_tables.rb
+++ db/migrate/20140306012927_drop_old_tables.rb
@@ -1,21 +1,26 @@
 class DropOldTables < ActiveRecord::Migration
   def self.up
-    drop_table :custom_ontologies
-    drop_table :event_items
-    drop_table :groups
-    drop_table :mapping_import_errors
-    drop_table :mappings
-    drop_table :margin_notes
-    drop_table :notes
-    drop_table :notes_indices
-    drop_table :projects
-    drop_table :rating_types
-    drop_table :ratings
-    drop_table :reviews
-    drop_table :surveys
-    drop_table :users
-    drop_table :uses
-    drop_table :widget_logs
+    old_tables = [
+      :custom_ontologies,
+      :event_items,
+      :groups,
+      :mapping_import_errors,
+      :mappings,
+      :margin_notes,
+      :notes,
+      :notes_indices,
+      :projects,
+      :rating_types,
+      :ratings,
+      :reviews,
+      :surveys,
+      :users,
+      :uses,
+      :widget_logs
+    ]
+    old_tables.each do |table|
+      drop_table table rescue next
+    end
   end
 
   def self.down
