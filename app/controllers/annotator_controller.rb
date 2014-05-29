5448ab10aacb61e2f13e7e7e8302c0e122b0aa11
Paul R Alexander <palexander@stanford.edu>
2014-05-29 14:51:58 -0700

    Remove commented code

--- app/controllers/annotator_controller.rb
+++ app/controllers/annotator_controller.rb
@@ -63,41 +63,7 @@ class AnnotatorController < ApplicationController
       }
     end
 
-    # Avoid highlighted match context in controller; it's more
-    # overhead on controller and more data transfer to the client.
-    # This is handled in bp_annotator.js, to move the processing load onto
-    # the client side in JS.  (TODO: evaluate if JS handles UTF8.)
-    #annotations.each do |a|
-    #  #annotation = a['annotations'].first
-    #  #=> {"from"=>1, "to"=>8, "matchType"=>"PREF", "text"=>"MELANOMA"}
-    #  a['annotations'].each do |annotation|
-    #    position = [annotation['from'], annotation['to']]
-    #    context = highlight_and_get_context(text_to_annotate, position)
-    #    annotation['context'] = context
-    #  end
-    #end
     render :json => response
   end
-
-
-private
-
-  ## This method can be used to highlight matched classes in the annotation text.  Currently done in JS on the client.
-  #def highlight_and_get_context(text, position, words_to_keep = 4)
-  #  # Process the highlighted text
-  #  #highlight = ["<span style='color: #006600; padding: 2px 0; font-weight: bold;'>", "", "</span>"]
-  #  highlight = ["<span class='annotator_context_highlight'>", "", "</span>"]
-  #  highlight[1] = text.utf8_slice(position[0] - 1, position[1] - position[0] + 1)
-  #  # Use scan to split the text on spaces while keeping the spaces
-  #  #scan_filter = Regexp.new(/[ ]+?[-\?'"\+\.,]+\w+|[ ]+?[-\?'"\+\.,]+\w+[-\?'"\+\.,]|\w+[-\?'"\+\.,]+|[ ]+?\w+/)
-  #  before = text.utf8_slice(0, position[0] - 1).match(/(\s+\S+|\S+\s+){0,4}$/).to_s
-  #  after = text.utf8_slice(position[1], ActiveSupport::Multibyte::Chars.new(text).length - position[1]).match(/^(\S+\s+\S+|\s+\S+|\S+\s+){0,4}/).to_s
-  #  # The process above will not keep a space right before the highlighted word, so let's keep it here if needed
-  #  # 32 is the character code for space
-  #  kept_space = text.utf8_slice(position[0] - 2) == " " ? " " : ""
-  #  # Put it all together
-  #  [before, kept_space, highlight.join, after].join
-  #end
-
 end
 
