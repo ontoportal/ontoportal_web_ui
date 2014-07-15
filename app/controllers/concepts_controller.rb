c99f56f20ca0c1da870b17d43e97989d6d3759ba
Paul R Alexander <palexander@stanford.edu>
2014-07-15 12:04:37 -0700

    Extend limit for children on expansion

--- app/controllers/concepts_controller.rb
+++ app/controllers/concepts_controller.rb
@@ -165,7 +165,7 @@ private
       gather_details
       render :partial => 'load'
     when 'children' # Children is called only for drawing the tree
-      @children = @concept.explore.children(full: true).collection || []
+      @children = @concept.explore.children(full: true, pagesize: 750).collection || []
       @children.sort!{|x,y| (x.prefLabel || "").downcase <=> (y.prefLabel || "").downcase} unless @children.empty?
       render :partial => 'child_nodes'
     end
