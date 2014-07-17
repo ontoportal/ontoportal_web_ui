7b302816c1aa9f55e1caf0c739d16231c1610f18
Paul R Alexander <palexander@stanford.edu>
2014-07-17 16:38:24 -0700

    Don't just look for any http in the string, must be a link at beginning

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -486,7 +486,7 @@ module ApplicationHelper
     else
       target = " target='#{target}' "
     end
-    if cls_id.include? 'http'
+    if cls_id.start_with? 'http://'
       href_cls = " href='#{bp_class_link(cls_id, ont_acronym)}' "
       data_cls = " data-cls='#{cls_id}' "
       data_ont = " data-ont='#{ont_acronym}' "
