1717e145d3053ae4bd3097f3127abab309b37f24
Paul R Alexander <palexander@stanford.edu>
2014-08-15 14:35:51 -0700

    Allow periods in resource ids for users

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ ActionController::Routing::Routes.draw do |map|
 
   map.resources :users, :as => :accounts
 
-  map.resources :users
+  map.resources :users, :requirements => { :id => /.*/ }
 
   map.resources :reviews
 
