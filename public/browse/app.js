09b1ef94d38970667c31856a5cf396b9b2edba31
Paul R Alexander <palexander@stanford.edu>
2014-11-17 18:02:51 -0800

    Set up watch for filtering ontology list

--- public/browse/app.js
+++ public/browse/app.js
@@ -14,13 +14,13 @@ var app = angular.module('FacetedBrowsing.OntologyList', ["checklist-model"])
 
 .controller('OntologyList', ['$scope', function($scope) {
   $scope.facets = {
-    types: [],
+    types: ["ontology"],
     formats: [],
     groups: [],
     categories: [],
     artifacts: []
   }
-  $scope.types = ["ontology", "ontology_views"];
+  $scope.types = ["ontology", "ontology_view"];
   $scope.artifacts = ["notes", "reviews", "projects"];
   $scope.ontologies = jQuery(document).data().bp.ontologies;
   $scope.formats = jQuery(document).data().bp.formats.sort();
@@ -35,7 +35,21 @@ var app = angular.module('FacetedBrowsing.OntologyList', ["checklist-model"])
     return 0;
   });
 
-}]);
+  $scope.$watch('facets', function() {
+    var ontology;
+    for (var i = 0; i < $scope.ontologies.length; i++) {
+      ontology = $scope.ontologies[i];
+      ontology.show = true;
+      if ($scope.facets.types.length > 0) {
+        if ($scope.facets.types.indexOf(ontology.type) === -1) {
+          ontology.show = false;
+          continue;
+        }
+      }
+    }
+  }, true);
+}])
+
 .filter('idToTitle', function() {
   return function(input) {
     if (input) {
