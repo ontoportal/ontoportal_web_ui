a2a1d0eff246f2e95784016a84e71d758b40af09
Paul R Alexander <palexander@stanford.edu>
2015-02-04 18:29:22 -0800

    Sort oldest first

--- app/helpers/ontologies_helper.rb
+++ app/helpers/ontologies_helper.rb
@@ -115,10 +115,10 @@ module OntologiesHelper
   def visits_data
     return @visits_data if @visits_data
     visits_data = {visits: [], labels: []}
-    years = @analytics[@ontology.acronym.to_sym].to_h.keys.map {|e| e.to_s.to_i}.select {|e| e > 0}.sort.reverse
+    years = @analytics[@ontology.acronym.to_sym].to_h.keys.map {|e| e.to_s.to_i}.select {|e| e > 0}.sort
     now = Time.now
     years.each do |year|
-      months = @analytics[@ontology.acronym.to_sym].to_h[year.to_s.to_sym].to_h.keys.map {|e| e.to_s.to_i}.select {|e| e > 0}.sort.reverse
+      months = @analytics[@ontology.acronym.to_sym].to_h[year.to_s.to_sym].to_h.keys.map {|e| e.to_s.to_i}.select {|e| e > 0}.sort
       months.each do |month|
         next if now.year == year && now.month <= month || (year == 2013 && month < 10) # we don't have good data going back past Oct 2013
         visits_data[:visits] << @analytics[@ontology.acronym.to_sym].to_h[year.to_s.to_sym][month.to_s.to_sym]
