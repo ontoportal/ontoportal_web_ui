b657b3bc21ff96fbc3ef991888da3b730e48636c
Jennifer Vendetti <vendetti@stanford.edu>
2015-02-20 16:53:32 -0800

    Upgrade routes to Rails 3 syntax
    Used helper script: “rake rails:upgrade:routes”.

--- config/routes.rb
+++ config/routes.rb
@@ -56,150 +56,146 @@ BioportalWebUi::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
 
-  map.resources :notes
+  resources :notes
 
-  map.resources :projects
+  resources :projects
 
-  map.resources :users, :as => :accounts, :requirements => { :id => /.+/ }
+  resources :users
 
-  map.resources :reviews
+  resources :reviews
 
-  map.resources :mappings
+  resources :mappings
 
-  map.resources :margin_notes
+  resources :margin_notes
 
-  map.resources :concepts
+  resources :concepts
 
-  map.resources :ontologies, :has_many => :submissions
+  resources :ontologies
 
-  map.resources :submissions, :belongs_to => :ontologies
+  resources :submissions
 
-  map.resources :login
+  resources :login
 
-  map.resources :admin
+  resources :admin
 
-  map.resources :subscriptions
+  resources :subscriptions
 
-  map.resources :recommender
+  resources :recommender
 
-  map.resources :annotator
+  resources :annotator
 
-  map.resources :virtual_appliance
+  resources :virtual_appliance
 
-  # The priority is based upon order of creation: first created -> highest priority.
-
-  # You can have the root of your site routed by hooking up ''
-  map.connect '', :controller => "home"
-
+  match '' => 'home#index'
+  
   # Top-level pages
-  map.connect '/feedback', :controller => 'home', :action => 'feedback'
-  map.connect '/account', :controller => 'home', :action => 'account'
-  map.connect '/help', :controller => 'home', :action => 'help'
-  map.connect '/robots.txt', :controller => 'home', :action => 'robots'
-  map.connect '/site_config', :controller => 'home', :action => 'site_config'
-  map.connect '/validate_ontology_file', :controller => 'home', :action => 'validate_ontology_file', conditions: {method: :post}
-  map.connect '/validate_ontology_file', :controller => 'home', :action => 'validate_ontology_file_show'
-  map.connect '/layout_partial/:partial', :controller => 'home', :action => 'render_layout_partial'
-
+  match '/feedback' => 'home#feedback'
+  match '/account' => 'home#account'
+  match '/help' => 'home#help'
+  match '/robots.txt' => 'home#robots'
+  match '/site_config' => 'home#site_config'
+  match '/validate_ontology_file' => 'home#validate_ontology_file', :via => :post
+  match '/validate_ontology_file' => 'home#validate_ontology_file_show'
+  match '/layout_partial/:partial' => 'home#render_layout_partial'
+  
   # Analytics endpoint
-  map.connect '/analytics', :controller => 'analytics', :action => 'track'
+  match '/analytics' => 'analytics#track'
 
   # Ontologies
-  map.connect '/ontologies/view/edit/:id', :controller => 'ontologies', :action => 'edit_view', :requirements => { :id => %r([^/?]+) }
-  map.connect '/ontologies/view/new/:id', :controller => 'ontologies', :action => 'new_view'
-  map.ontology_virtual '/ontologies/virtual/:ontology', :controller => 'ontologies', :action => 'virtual'
-  map.connect '/ontologies/success/:id', :controller => 'ontologies', :action => 'submit_success'
-  map.connect '/ontologies/:acronym', controller: 'ontologies', action: 'update', conditions: {method: :post}
-  map.connect '/ontologies/:acronym/submissions/:id', controller: 'submissions', action: 'update', conditions: {method: :post}
-  map.connect '/ontologies/:ontology_id/submissions/new', :controller => 'submissions', :action => 'new', :ontology_id => /.+/
-  map.connect '/ontologies/:ontology_id/submissions', :controller => 'submissions', :action => 'create', :ontology_id => /.+/, conditions: {method: :post}
-  map.connect '/ontologies/:acronym/classes/:purl_conceptid', controller: 'ontologies', action: 'show', purl_conceptid: "root"
-  map.connect '/ontologies/:acronym/:purl_conceptid', controller: 'ontologies', action: 'show'
+  match '/ontologies/view/edit/:id' => 'ontologies#edit_view', :constraints => { :id => /[^\/?]+/ }
+  match '/ontologies/view/new/:id' => 'ontologies#new_view'
+  match '/ontologies/virtual/:ontology' => 'ontologies#virtual', :as => :ontology_virtual
+  match '/ontologies/success/:id' => 'ontologies#submit_success'
+  match '/ontologies/:acronym' => 'ontologies#update', :via => :post
+  match '/ontologies/:acronym/submissions/:id' => 'submissions#update', :via => :post
+  match '/ontologies/:ontology_id/submissions/new' => 'submissions#new', :ontology_id => /.+/
+  match '/ontologies/:ontology_id/submissions' => 'submissions#create', :ontology_id => /.+/, :via => :post
+  match '/ontologies/:acronym/classes/:purl_conceptid' => 'ontologies#show', :purl_conceptid => 'root'
+  match '/ontologies/:acronym/:purl_conceptid' => 'ontologies#show'
 
   # Analytics
-  map.connect '/analytics/:action', :controller => "analytics", :action => /search_result_clicked|user_intention_surveys/
+  match '/analytics/:action' => 'analytics#(?-mix:search_result_clicked|user_intention_surveys)'
 
   # New Notes
-  map.note '/notes/:id', :controller => 'notes', :action => 'show', :id => /.+/
-  map.note_virtual 'ontologies/:ontology/notes/:noteid', :controller => 'notes', :action => 'virtual_show', :noteid => /.+/
-  map.note_ajax_single 'notes/ajax/single/:ontology', :controller => 'notes', :action => 'show_single'
-  map.note_ajax_single_list 'notes/ajax/single_list/:ontology', :controller => 'notes', :action => 'show_single_list'
+  match '/notes/:id' => 'notes#show', :as => :note, :id => /.+/
+  match 'ontologies/:ontology/notes/:noteid' => 'notes#virtual_show', :as => :note_virtual, :noteid => /.+/
+  match 'notes/ajax/single/:ontology' => 'notes#show_single', :as => :note_ajax_single
+  match 'notes/ajax/single_list/:ontology' => 'notes#show_single_list', :as => :note_ajax_single_list
 
   # Ajax
-  map.ajax '/ajax/', :controller => 'ajax_proxy', :action => 'get'
-  map.connect '/ajax_concepts/:ontology/', :controller => 'concepts', :action => 'show', :requirements => { :id => %r([^/?]+) }
-  map.connect '/ajax/class_details', :controller => 'concepts', :action => 'details'
-  map.connect "/ajax/mappings/get_concept_table", :controller => "mappings", :action => "get_concept_table"
-  map.connect "/ajax/json_ontology", :controller => "ajax_proxy", :action => "json_ontology"
-  map.connect "/ajax/json_class", :controller => "ajax_proxy", :action => "json_class"
-  map.connect "/ajax/jsonp", :controller => "ajax_proxy", :action => "jsonp"
-  map.connect "/ajax/recaptcha", :controller => "ajax_proxy", :action => "recaptcha"
-  map.connect "/ajax/loading_spinner", :controller => "ajax_proxy", :action => "loading_spinner"
-  map.connect "/ajax/notes/delete", :controller => "notes", :action => "destroy"
-  map.connect "/ajax/notes/concept_list", :controller => "notes", :action => "show_concept_list"
-  map.connect "/ajax/classes/label", :controller => "concepts", :action => "show_label"
-  map.connect "/ajax/classes/definition", :controller => "concepts", :action => "show_definition"
-  map.connect "/ajax/classes/treeview", :controller => "concepts", :action => "show_tree"
-  map.connect "/ajax/properties/tree", :controller => "concepts", :action => "property_tree"
-  map.connect '/ajax/biomixer', :controller => 'concepts', :action => 'biomixer'
+  match '/ajax/' => 'ajax_proxy#get', :as => :ajax
+  match '/ajax_concepts/:ontology/' => 'concepts#show', :constraints => { :id => /[^\/?]+/ }
+  match '/ajax/class_details' => 'concepts#details'
+  match '/ajax/mappings/get_concept_table' => 'mappings#get_concept_table'
+  match '/ajax/json_ontology' => 'ajax_proxy#json_ontology'
+  match '/ajax/json_class' => 'ajax_proxy#json_class'
+  match '/ajax/jsonp' => 'ajax_proxy#jsonp'
+  match '/ajax/recaptcha' => 'ajax_proxy#recaptcha'
+  match '/ajax/loading_spinner' => 'ajax_proxy#loading_spinner'
+  match '/ajax/notes/delete' => 'notes#destroy'
+  match '/ajax/notes/concept_list' => 'notes#show_concept_list'
+  match '/ajax/classes/label' => 'concepts#show_label'
+  match '/ajax/classes/definition' => 'concepts#show_definition'
+  match '/ajax/classes/treeview' => 'concepts#show_tree'
+  match '/ajax/properties/tree' => 'concepts#property_tree'
+  match '/ajax/biomixer' => 'concepts#biomixer'
 
   # User
-  map.logout '/logout', :controller => 'login', :action => 'destroy'
-  map.lost_pass '/lost_pass', :controller => 'login', :action => 'lost_password'
-  map.lost_pass '/reset_password', :controller => 'login', :action => 'reset_password'
-  map.custom_ontologies '/accounts/:id/custom_ontologies', :controller => 'users', :action => 'custom_ontologies'
-  map.connect '/login_as/:login_as', :controller => 'login', :action => 'login_as'
+  match '/logout' => 'login#destroy', :as => :logout
+  match '/lost_pass' => 'login#lost_password', :as => :lost_pass
+  match '/reset_password' => 'login#reset_password', :as => :lost_pass
+  match '/accounts/:id/custom_ontologies' => 'users#custom_ontologies', :as => :custom_ontologies
+  match '/login_as/:login_as' => 'login#login_as'
 
   # Resource Index
-  map.obr_details '/res_details/:id', :controller => 'resources', :action => 'details'
-  map.obr '/resources/:ontology/:id', :controller => 'resources', :action => 'show'
-  map.obrpage '/respage/', :controller => 'resources', :action => 'page'
-  map.connect '/resource_index/resources', :controller => 'resource_index', :action => 'index'
-  map.connect '/resource_index/resources/:resource_id', :controller => 'resource_index', :action => 'index'
-  map.connect '/resource_index/:action', :controller => "resource_index", :action => /element_annotations|results_paginate|resources_table/
-  map.connect '/resource_index/search_classes', :controller => "resource_index", :action => 'search_classes'
-  map.resources :resource_index
+  match '/res_details/:id' => 'resources#details', :as => :obr_details
+  match '/resources/:ontology/:id' => 'resources#show', :as => :obr
+  match '/respage/' => 'resources#page', :as => :obrpage
+  match '/resource_index/resources' => 'resource_index#index'
+  match '/resource_index/resources/:resource_id' => 'resource_index#index'
+  match '/resource_index/:action' => 'resource_index#(?-mix:element_annotations|results_paginate|resources_table)'
+  match '/resource_index/search_classes' => 'resource_index#search_classes'
+  resources :resource_index
 
   # History
-  map.remove_tab '/tab/remove/:ontology',:controller => 'history', :action => 'remove'
-  map.update_tab '/tab/update/:ontology/:concept', :controller => 'history', :action=>'update'
+  match '/tab/remove/:ontology' => 'history#remove', :as => :remove_tab
+  match '/tab/update/:ontology/:concept' => 'history#update', :as => :update_tab
 
   # Install the default route as the lowest priority.
-  map.connect ':controller/:action/:id.:format'
-  map.connect ':controller/:action/:id'
-  map.jam 'jambalaya/:ontology/:id', :controller => 'visual', :action => 'jam'
+  match '/:controller(/:action(/:id))'
+  match 'jambalaya/:ontology/:id' => 'visual#jam', :as => :jam
 
   # Admin
-  map.connect '/admin/clearcache', controller: 'admin', action: 'clearcache', conditions: {method: :post}
-  map.connect '/admin/resetcache', controller: 'admin', action: 'resetcache', conditions: {method: :post}
-
+  match '/admin/clearcache' => 'admin#clearcache', :via => :post
+  match '/admin/resetcache' => 'admin#resetcache', :via => :post
+  
   #####
   ## OLD ROUTES
   ## All of these should redirect to new addresses in the controller method or using the redirect controller
   #####
 
   # Redirects from old URL locations
-  map.connect '/annotate', :controller => 'redirect', :url => '/annotator'
-  map.connect '/all_resources', :controller =>'redirect', :url => '/resources'
-  map.connect '/resources', :controller => 'redirect', :url => '/resource_index'
-  map.connect '/visconcepts/:ontology/', :controller => 'redirect', :url => '/visualize/'
-  map.connect '/ajax/terms/label', :controller => 'redirect', :url => '/ajax/classes/label'
-  map.connect '/ajax/terms/definition', :controller => 'redirect', :url => '/ajax/classes/definition'
-  map.connect '/ajax/terms/treeview', :controller => 'redirect', :url => '/ajax/classes/treeview'
-  map.connect '/ajax/term_details/:ontology', :controller => 'redirect', :url => '/ajax/class_details'
-  map.connect "/ajax/json_term", :controller => 'redirect', :url => '/ajax/json_class'
+  match '/annotate' => 'redirect#index', :url => '/annotator'
+  match '/all_resources' => 'redirect#index', :url => '/resources'
+  match '/resources' => 'redirect#index', :url => '/resource_index'
+  match '/visconcepts/:ontology/' => 'redirect#index', :url => '/visualize/'
+  match '/ajax/terms/label' => 'redirect#index', :url => '/ajax/classes/label'
+  match '/ajax/terms/definition' => 'redirect#index', :url => '/ajax/classes/definition'
+  match '/ajax/terms/treeview' => 'redirect#index', :url => '/ajax/classes/treeview'
+  match '/ajax/term_details/:ontology' => 'redirect#index', :url => '/ajax/class_details'
+  match '/ajax/json_term' => 'redirect#index', :url => '/ajax/json_class'
 
   # Visualize
-  map.virtual_visualize '/visualize/virtual/:ontology', :controller => 'concepts', :action => 'virtual', :requirements => { :id => %r([^/?]+), :conceptid => %r([^/?]+) }
-  map.virtual_uri '/visualize/virtual/:ontology/:id', :controller => 'concepts', :action => 'virtual', :requirements => { :id => %r([^/?]+) }
-  map.visualize '/visualize/:ontology', :controller => 'ontologies', :action =>'visualize', :requirements => { :ontology => %r([^/?]+) }
-  map.uri '/visualize/:ontology/:conceptid', :controller => 'ontologies', :action => 'visualize', :requirements => { :ontology => %r([^/?]+), :conceptid => %r([^/?]+) }
-  map.visualize_concept '/visualize', :controller => 'ontologies', :action => 'visualize', :requirements => { :ontology => %r([^/?]+), :id => %r([^/?]+),
-                                                                                                              :ontologyid => %r([^/?]+), :conceptid => %r([^/?]+) }
-  map.flexviz '/flexviz/:ontologyid', :controller => 'concepts', :action => 'flexviz', :requirements => { :ontologyid => %r([^/?]+) }
-  map.connect '/exhibit/:ontology/:id', :controller => 'concepts', :action=>'exhibit'
+  match '/visualize/virtual/:ontology' => 'concepts#virtual', :as => :virtual_visualize, :constraints => { :id => /[^\/?]+/, :conceptid => /[^\/?]+/ }
+  match '/visualize/virtual/:ontology/:id' => 'concepts#virtual', :as => :virtual_uri, :constraints => { :id => /[^\/?]+/ }
+  match '/visualize/:ontology' => 'ontologies#visualize', :as => :visualize, :constraints => { :ontology => /[^\/?]+/ }
+  match '/visualize/:ontology/:conceptid' => 'ontologies#visualize', :as => :uri, :constraints => { :ontology => /[^\/?]+/, :conceptid => /[^\/?]+/ }
+  match '/visualize' => 'ontologies#visualize', :as => :visualize_concept, :constraints => { :ontology => /[^\/?]+/, :id => /[^\/?]+/, :ontologyid => /[^\/?]+/, :conceptid => /[^\/?]+/ }
 
+  match '/flexviz/:ontologyid' => 'concepts#flexviz', :as => :flexviz, :constraints => { :ontologyid => /[^\/?]+/ }
+  match '/exhibit/:ontology/:id' => 'concepts#exhibit'
+  
   # Virtual
-  map.virtual_ont '/virtual/:ontology', :controller => 'concepts', :action => 'virtual', :requirements => { :ontology => %r([^/?]+) }
-  map.virtual '/virtual/:ontology/:conceptid', :controller => 'concepts', :action => 'virtual', :requirements => { :ontology => %r([^/?]+), :conceptid => %r([^/?]+) }
+  match '/virtual/:ontology' => 'concepts#virtual', :as => :virtual_ont, :constraints => { :ontology => /[^\/?]+/ }
+  match '/virtual/:ontology/:conceptid' => 'concepts#virtual', :as => :virtual, :constraints => { :ontology => /[^\/?]+/, :conceptid => /[^\/?]+/ }
 end
