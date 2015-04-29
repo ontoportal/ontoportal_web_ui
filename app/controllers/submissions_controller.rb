7304b655b32e5bc446cae02af7134166a05e6104
Paul R Alexander <palexander@stanford.edu>
2015-04-29 16:43:33 -0700

    Add functionality to ask user for master file

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -19,6 +19,10 @@ class SubmissionsController < ApplicationController
     @submission_saved = @submission.save
     if !@submission_saved || @submission_saved.errors
       @errors = response_errors(@submission_saved) # see application_controller::response_errors
+      if @errors[:error][:uploadFilePath] && @errors[:error][:uploadFilePath].first[:options]
+        @masterFileOptions = @errors[:error][:uploadFilePath].first[:options]
+        @errors = ["Please select a main ontology file from your uploaded zip"]
+      end
       render "new"
     else
       # Update summaryOnly on ontology object
