5227e2978a4dcf060a7be33d06d135f4eca04fcb
Jennifer Vendetti <vendetti@stanford.edu>
2015-05-08 16:56:28 -0700

    Fix JS load order
    NCBO-1261

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,36 +10,6 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery
-//= require jquery-migrate-1.2.1.min
-//= require jquery_ujs
-//= require jquery-ui
-//
-//  3rd party library code
-//= require jquery.blockUI
-//= require facebox
-//= require thickbox
-//= require fg.menu
-//= require jquery.tools.min
-//= require jquery.dataTables
-//= require datatables.formatted.number
-//= require dataTables.fixedHeader
-//= require chosen.jquery
-//= require ajax-chosen
-//= require superfish
-//= require jquery.cookie
-//= require splitter
-//= require autocomplete
-//= require jquery.hoverIntent
-//= require jquery.simple.tree
-//= require jquery.scrollTo-1.4.0-min
-//= require jquery.rating.pack
-//= require history/jquery.history
-//= require alertify
-//= require ie6notice
-//= require jquery.tooltip
-//
-//  BioPortal code
 //= require bioportal
 //= require bp_class_tree
 //= require bp_ajax_controller
---
+++ app/assets/javascripts/vendor.js
@@ -0,0 +1,28 @@
+//= require jquery
+//= require jquery-migrate-1.2.1.min
+//= require jquery_ujs
+//= require jquery-ui
+//
+//  3rd party library code
+//= require jquery.blockUI
+//= require facebox
+//= require thickbox
+//= require fg.menu
+//= require jquery.tools.min
+//= require jquery.dataTables
+//= require datatables.formatted.number
+//= require dataTables.fixedHeader
+//= require chosen.jquery
+//= require ajax-chosen
+//= require superfish
+//= require jquery.cookie
+//= require splitter
+//= require autocomplete
+//= require jquery.hoverIntent
+//= require jquery.simple.tree
+//= require jquery.scrollTo-1.4.0-min
+//= require jquery.rating.pack
+//= require history/jquery.history
+//= require alertify
+//= require ie6notice
+//= require jquery.tooltip
