022b47af67c2080cc5837d66773c97b0337b7ab8
Jennifer Vendetti <vendetti@stanford.edu>
2015-10-08 20:51:47 -0700

    Fix undefined method error

--- app/controllers/mappings_controller.rb
+++ app/controllers/mappings_controller.rb
@@ -24,7 +24,7 @@ class MappingsController < ApplicationController
     if ontologies_mapping_count
       ontologies_mapping_count.members.each do |ontology_acronym|
         ontology = ontologies_hash[ontology_acronym.to_s]
-        mapping_count = ontologies_mapping_count[ontology_acronym]
+        mapping_count = ontologies_mapping_count[ontology_acronym] || 0
         next unless ontology && mapping_count > 0
         select_text = "#{ontology.name} - #{ontology.acronym} (#{number_with_delimiter(mapping_count, delimiter: ',')})"
         @options[select_text] = ontology_acronym
