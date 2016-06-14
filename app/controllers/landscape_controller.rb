697f40c8f823f4be4723f70cc98ba8d44cc936fa
Vincent Emonet <vincent.emonet@gmail.com>
2016-06-14 12:09:39 +0200

    generate the pie chart from the naturalLanguages

--- app/controllers/landscape_controller.rb
+++ app/controllers/landscape_controller.rb
@@ -5,20 +5,42 @@ class LandscapeController < ApplicationController
     @ontologies = LinkedData::Client::Models::Ontology.all(include_views: false)
     @submissions = LinkedData::Client::Models::OntologySubmission.all
 
-    #[{"text":"study","size":40},{"text":"motion","size":15},{"text":"forces","size":10},{"text":"electricity","size":15},{"text":"movement","size":10},{"text":"relation","size":5},{"text":"things","size":10},{"text":"force","size":5},{"text":"ad","size":5},{"text":"energy","size":85},{"text":"living","size":5},{"text":"nonliving","size":5},{"text":"laws","size":15},{"text":"speed","size":45},{"text":"velocity","size":30},{"text":"define","size":5},{"text":"constraints","size":5},{"text":"universe","size":10},{"text":"physics","size":120},{"text":"describing","size":5},{"text":"matter","size":90},{"text":"physics-the","size":5},{"text":"world","size":10},{"text":"works","size":10},{"text":"science","size":70},{"text":"interactions","size":30},{"text":"studies","size":5},{"text":"properties","size":45},{"text":"nature","size":40},{"text":"branch","size":30},{"text":"concerned","size":25},{"text":"source","size":40},{"text":"google","size":10},{"text":"defintions","size":5},{"text":"two","size":15},{"text":"grouped","size":15},{"text":"traditional","size":15},{"text":"fields","size":15},{"text":"acoustics","size":15},{"text":"optics","size":15},{"text":"mechanics","size":20},{"text":"thermodynamics","size":15},{"text":"electromagnetism","size":15},{"text":"modern","size":15},{"text":"extensions","size":15},{"text":"thefreedictionary","size":15},{"text":"interaction","size":15},{"text":"org","size":25},{"text":"answers","size":5},{"text":"natural","size":15},{"text":"objects","size":5},{"text":"treats","size":10},{"text":"acting","size":5},{"text":"department","size":5},{"text":"gravitation","size":5},{"text":"heat","size":10},{"text":"light","size":10},{"text":"magnetism","size":10},{"text":"modify","size":5},{"text":"general","size":10},{"text":"bodies","size":5},{"text":"philosophy","size":5},{"text":"brainyquote","size":5},{"text":"words","size":5},{"text":"ph","size":5},{"text":"html","size":5},{"text":"lrl","size":5},{"text":"zgzmeylfwuy","size":5},{"text":"subject","size":5},{"text":"distinguished","size":5},{"text":"chemistry","size":5},{"text":"biology","size":5},{"text":"includes","size":5},{"text":"radiation","size":5},{"text":"sound","size":5},{"text":"structure","size":5},{"text":"atoms","size":5},{"text":"including","size":10},{"text":"atomic","size":10},{"text":"nuclear","size":10},{"text":"cryogenics","size":10},{"text":"solid-state","size":10},{"text":"particle","size":10},{"text":"plasma","size":10},{"text":"deals","size":5},{"text":"merriam-webster","size":5},{"text":"dictionary","size":10},{"text":"analysis","size":5},{"text":"conducted","size":5},{"text":"order","size":5},{"text":"understand","size":5},{"text":"behaves","size":5},{"text":"en","size":5},{"text":"wikipedia","size":5},{"text":"wiki","size":5},{"text":"physics-","size":5},{"text":"physical","size":5},{"text":"behaviour","size":5},{"text":"collinsdictionary","size":5},{"text":"english","size":5},{"text":"time","size":35},{"text":"distance","size":35},{"text":"wheels","size":5},{"text":"revelations","size":5},{"text":"minute","size":5},{"text":"acceleration","size":20},{"text":"torque","size":5},{"text":"wheel","size":5},{"text":"rotations","size":5},{"text":"resistance","size":5},{"text":"momentum","size":5},{"text":"measure","size":10},{"text":"direction","size":10},{"text":"car","size":5},{"text":"add","size":5},{"text":"traveled","size":5},{"text":"weight","size":5},{"text":"electrical","size":5},{"text":"power","size":5}]
+    # Array with color codes for the pie chart
+    pie_colors_array = ["#2484c1", "#0c6197", "#4daa4b", "#90c469", "#daca61", "#e4a14b", "#e98125", "#cb2121", "#830909", "#923e99", "#ae83d5", "#bf273e", "#ce2aeb", "#bca44a", "#618d1b", "#1ee67b", "#b0ec44", "#a4a0c9", "#322849", "#86f71a", "#d1c87f", "#7d9058", "#44b9b0", "#7c37c0", "#cc9fb1", "#e65414", "#8b6834", "#248838"];
 
+    # Generate the JSON to put natural languages in the pie chart
+    @natural_language_json_pie = []
+
+    # Get the different naturalLanguage of submissions to generate a tag cloud
     @natural_language_json_cloud = []
 
+    # A hash with the language label and the number of time it appears in sub.naturalLanguage
+    natural_language_hash = {}
+
+    color_index = 0
+    # Iterate submissions to get the natural languages
     @submissions.each do |sub|
       if !sub.naturalLanguage.nil? && !sub.naturalLanguage.empty?
         sub.naturalLanguage.each do |sub_lang|
-          sub_lang_json = {"text"=>sub_lang.to_s,"size"=>10}
-          @natural_language_json_cloud.push(sub_lang_json)
+          if natural_language_hash.has_key?(sub_lang.to_s)
+            natural_language_hash[sub_lang.to_s] = natural_language_hash[sub_lang.to_s] + 1
+          else
+            natural_language_hash[sub_lang.to_s] = 1
+          end
         end
       end
     end
 
+    natural_language_hash.each do |lang,no|
+      @natural_language_json_cloud.push({"text"=>lang.to_s,"size"=>10})
+
+      @natural_language_json_pie.push({"label"=>lang.to_s,"value"=>no, "color"=>pie_colors_array[color_index]})
+      color_index += 1
+    end
+
     @natural_language_json_cloud = @natural_language_json_cloud.to_json.html_safe
+    @natural_language_json_pie = @natural_language_json_pie.to_json.html_safe
+
 
   end
 end
