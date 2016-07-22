658c4070f8c8ebdcb3d90b52cec85f97d238f76d
Vincent Emonet <vincent.emonet@gmail.com>
2016-07-22 23:35:26 +0200

    fix bug where only metadata submissions where uploaded before updating the ontology

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -16,6 +16,9 @@ class SubmissionsController < ApplicationController
 
     @submission = LinkedData::Client::Models::OntologySubmission.new(values: params[:submission])
     @ontology = LinkedData::Client::Models::Ontology.find(@submission.ontology)
+    # Update summaryOnly on ontology object
+    @ontology.summaryOnly = @submission.isRemote.eql?("3")
+    @ontology.update
     @submission_saved = @submission.save
     if !@submission_saved || @submission_saved.errors
       @errors = response_errors(@submission_saved) # see application_controller::response_errors
@@ -24,11 +27,6 @@ class SubmissionsController < ApplicationController
         @errors = ["Please select a main ontology file from your uploaded zip"]
       end
       render "new"
-    else
-      # Update summaryOnly on ontology object
-      @ontology.summaryOnly = @submission.isRemote.eql?("3")
-      @ontology.save
-      redirect_to "/ontologies/success/#{@ontology.acronym}"
     end
   end
 
@@ -45,12 +43,12 @@ class SubmissionsController < ApplicationController
     @ontology = LinkedData::Client::Models::Ontology.get(params[:submission][:ontology])
     submissions = @ontology.explore.submissions
     @submission = submissions.select {|o| o.submissionId == params["id"].to_i}.first
-    @submission.update_from_params(params[:submission])
-    error_response = @submission.update
 
+    @submission.update_from_params(params[:submission])
     # Update summaryOnly on ontology object
     @ontology.summaryOnly = @submission.isRemote.eql?("3")
-    @ontology.save
+    @ontology.update
+    error_response = @submission.update
 
     if error_response
       @errors = response_errors(error_response) # see application_controller::response_errors
