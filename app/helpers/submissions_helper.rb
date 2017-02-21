ddc27a530b56750c502bcec8f3bb5ac768687a1e
Vincent Emonet <vincent.emonet@gmail.com>
2017-02-21 18:48:38 +0100

    now properly handle arrays of URI

--- app/helpers/submissions_helper.rb
+++ app/helpers/submissions_helper.rb
@@ -21,13 +21,23 @@ module SubmissionsHelper
       else
         uri_value = @submission.send(attr["attribute"])
       end
-      input_html << url_field(:submission, attr["attribute"].to_s.to_sym, value: uri_value)
 
       if attr["enforce"].include?("list")
+        input_html << url_field(:submission, attr["attribute"].to_s.to_sym, value: uri_value[0], :style => "margin-bottom: 0.3em;")
+        # Add field if list of metadata
+        @submission.send(attr["attribute"]).each_with_index do |metadata_val, index|
+          if index != 0
+            input_html << url_field_tag("added" + attr["attribute"].to_s + "[]", metadata_val, :id => "added" + attr["attribute"].to_s, :style => "margin-bottom: 0.3em;")
+          end
+        end
+
         input_html << button_tag("Add new value", :id => "add#{attr["attribute"]}", :style => "margin-bottom: 0.5em;margin-top: 0.5em;",
                                  :type => "button", :class => "btn btn-info", :onclick => "addInput('#{attr["attribute"]}', 'url')")
         input_html << content_tag(:div, "", id: "#{attr["attribute"]}Div")
-        return input_html
+
+      else
+        # if single value
+        input_html << text_field(:submission, attr["attribute"].to_s.to_sym, value: uri_value)
       end
       return input_html
 
