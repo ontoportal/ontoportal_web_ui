c0ebbe7fb1acd93364868fa23b505230c40f2427
Vincent Emonet <vincent.emonet@gmail.com>
2017-04-14 10:24:14 +0200

    add horizontal bar chart

--- app/assets/javascripts/bp_landscape.js
+++ app/assets/javascripts/bp_landscape.js
@@ -114,3 +114,38 @@ function draw(words) {
         })
         .text(function(d) { return d.text; });
 }
+
+
+// Horizontal bar charts for format (OWL, SKOS, UMLS)
+
+var MeSeContext = document.getElementById("formatCanvas").getContext("2d");
+var MeSeData = {
+  labels: [
+    "ME",
+    "SE"
+  ],
+  datasets: [{
+    label: "Test",
+    data: [100, 75],
+    backgroundColor: ["#669911", "#119966"],
+    hoverBackgroundColor: ["#66A2EB", "#FCCE56"]
+  }]
+};
+
+var MeSeChart = new Chart(MeSeContext, {
+  type: 'horizontalBar',
+  data: MeSeData,
+  options: {
+    scales: {
+      xAxes: [{
+        ticks: {
+          min: 60
+        }
+      }],
+      yAxes: [{
+        stacked: true
+      }]
+    }
+
+  }
+});
