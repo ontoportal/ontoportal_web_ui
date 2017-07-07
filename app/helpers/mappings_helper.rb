636dd74f91c57f5f1d3404344f49100078002fcc
Jennifer Vendetti <vendetti@stanford.edu>
2017-07-06 17:26:42 -0700

    More UI refactors for the create new mapping dialog
    Resolves #41

--- app/helpers/mappings_helper.rb
+++ app/helpers/mappings_helper.rb
@@ -13,4 +13,17 @@ module MappingsHelper
     "<a href='#{uri}' target='_blank'>#{name}</a>"
   end
 
+  def onts_and_views_for_select
+    @onts_and_views_for_select = []
+    ontologies = LinkedData::Client::Models::Ontology.all(include: "acronym,name", include_views: true)
+    ontologies.each do |ont|
+      next if (ont.acronym.nil? || ont.acronym.empty?)
+      ont_acronym = ont.acronym
+      ont_display_name = "#{ont.name.strip} (#{ont_acronym})"
+      @onts_and_views_for_select << [ont_display_name, ont_acronym]
+    end
+    @onts_and_views_for_select.sort! { |a,b| a[0].downcase <=> b[0].downcase }
+    return @onts_and_views_for_select
+  end
+
 end
