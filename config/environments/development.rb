7a342123e97b9e8bdc4c637d8d7b559549b5399b
Syphax Bouazzouni <gs_bouazzouni@esi.dz>
2021-12-07 16:27:30 +0100

    fix development.rb config file , to have hot reloading work again

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
 
   # Add custom data attributes to sanitize allowed list
   config.action_view.sanitized_allowed_attributes = ['id', 'class', 'style', 'data-cls', 'data-ont']
-  
+
   config.file_watcher = ActiveSupport::FileUpdateChecker
 
   # Include BioPortal-specific configuration options
