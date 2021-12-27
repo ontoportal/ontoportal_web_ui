002a2653e60cf5fb2a634fb4784522c6db9a7042
Syphax Bouazzouni <gs_bouazzouni@esi.dz>
2021-12-27 16:48:26 +0100

    instances js files refactor

---
+++ app/assets/javascripts/components/instances/instances_table.js
@@ -0,0 +1,113 @@
+/**
+ * Render a jquery data table of an ontology (and class) instances inside a <table> element
+ */
+class InstancesTable{
+    constructor(tableElem , ontologyAcronym , classUri = ""){
+        this.tableElem = tableElem
+        this.ontologyAcronym = ontologyAcronym
+        this.classUri = classUri
+        this.dataTable = null
+    }
+
+    init(){
+        if ( $.fn.dataTable.isDataTable( this.tableElem ) ) {
+            $(this.tableElem).DataTable().destroy();
+
+        }
+        this.dataTable = $(this.tableElem).DataTable( {
+            "paging": true,
+            "pagingType": "full",
+            "info": true,
+            "searching": false,
+            "ordering": false,
+            "serverSide":true,
+            "processing": true,
+            "ajax": {
+                "url": this.getAjaxUrl(),
+                "contentType": "application/json",
+                "dataSrc":  (json) => {
+                    json.recordsTotal = json["totalCount"];
+                    json.recordsFiltered = json.recordsTotal
+                    return  json["collection"].map(x => [
+                        x["@id"],
+                        x["types"],
+                        x["properties"]
+                    ])
+                },
+                "data": (d) => {
+                    return {page: (d.start/d.length)+ 1 , pagesize: d.length}
+                }
+            },
+            "columnDefs": this.render(),
+            "language": {
+                'loadingRecords': '&nbsp;',
+                'processing': `           
+                                <div class="spinner-border m-2" role="status"> 
+                                    <span class="sr-only">Loading...</span>
+                                </div>           
+                `
+            },
+            "createdRow":  (row, data, dataIndex ) => {
+                $(row).click(() => this.openPopUpDetail(data));
+            }
+
+        })
+    }
+
+    static mount(tableId , ontologyAcronym , conceptId){
+        if(tableId.toString().length>0){
+            const tableElm = document.querySelector(tableId)
+
+            if(tableElm){
+                const instanceTable = new InstancesTable( tableElm , ontologyAcronym , conceptId)
+                instanceTable.init()
+                return instanceTable
+            }
+        }
+    }
+
+    render(){
+        const toLink = function (uri) {
+            return `<a id="${uri}" href="javascript:void(0)" title="${uri}">${getLabel(uri)}</a>`
+        }
+        const arr =["ID"];
+        if(!this.isClassURISet()){
+            arr.push("Types")
+        }
+        return arr.map((x,i) => {
+            return {
+                "targets" : i ,
+                "title":x,
+                "render": function (data, type ,row ,meta){
+                    if(typeof data === "string")
+                        data = [data]
+                    return data.map((x) => toLink(x)).join(',')
+                }
+            }
+        })
+    }
+
+    getAjaxUrl(page= null , size = null) {
+        let url = "/ajax/"+ this.ontologyAcronym
+        let params =  ["page="+page,"pagesize="+size].filter(x => x !== null).join("&")
+        if(this.isClassURISet() > 0){
+            url += "/classes/" + encodeURIComponent(this.classUri)
+        }
+        url +=  "/instances" + (page!==null || size!=null ? "?"+params : "");
+        return url
+    }
+
+    isClassURISet(){
+        return this.classUri.length > 0
+    }
+
+    openPopUpDetail(data){
+        $.facebox(() => {
+            let uri = data[0]
+            let types = data[1]
+            let properties = data[2]
+
+            $.facebox( new InstanceDetails(this.ontologyAcronym, uri , types , properties).render().html())
+        })
+    }
+}
