ede11d38ce4a48274f2b068b5f102e605a2785b4
Syphax Bouazzouni <gs_bouazzouni@esi.dz>
2021-12-28 14:24:47 +0100

    clean the InstancesHelper code

--- app/assets/javascripts/helpers/instances/instances.js
+++ app/assets/javascripts/helpers/instances/instances.js
@@ -1,55 +1,44 @@
-/**
- * Get a label from an uri
- * TODO: see if it's not a duplicate method
- * @param uri
- * @returns {string}
- */
-function getLabel(uri) {
-    let label = uri
-    if (isALink(uri)) { // is a link
-        let index = uri.toString().indexOf('#')
-        if (index > -1) {
-            label = uri.toString().substr(index + 1)
-        } else {
-            index = uri.toString().lastIndexOf('/')
-            if (index > -1)
-                label = uri.toString().substr(index + 1)
-        }
+class InstancesHelper {
+    // function to recover the redirection link to properties tab
+    static getPropertyHref(uri){
+        return  `?p=properties`
     }
-    return label
-}
+    // function to recover the redirection link to instances tab
+    static getInstanceHref(uri) {
+        return `?p=instances&conceptid=${encodeURIComponent(uri)}`
+    }
+    // function to recover the redirection link to instances tab
+    static getClassHref(uri) {
+        return `?p=classes&conceptid=${encodeURIComponent(uri)}`
+    }
 
-function isALink(uri){
-    return uri.startsWith("http") || uri.startsWith("https")
-}
+    /**
+     * Do ajax call to get an instance details (properties)
+     * @param ontology{String} acronym
+     * @param uri{String} an instance concept id
+     * @returns {Promise<unknown>}
+     */
+    static  getInstanceDetailsFromURI(ontology , uri){
+        return new Promise((resolve , reject) => {
+            $.getJSON("/ajax/"+ontology+"/instances/"+ encodeURIComponent(uri))
+                .done((data) => resolve(data))
+                .fail((error)=> reject(error))
+        })
+    }
+
+    /**
+     * Return the conceptid from
+     * the section url if the section is "instances"
+     * @returns {*|string}
+     */
+    static getInstanceConceptId() {
+        const ont_viewer_data =  jQuery(document).data().bp.ont_viewer
+        const concept_id = ont_viewer_data.concept_id
+        return ont_viewer_data.content_section === "instances" ? (concept_id === "root" ? "" :concept_id)  : ""
+    }
 
 
-/**
- * Do ajax call to get an instance details (properties)
- * @param ontology{String} acronym
- * @param uri{String} an instance concept id
- * @returns {Promise<unknown>}
- */
-function  getInstanceDetailsFromURI(ontology , uri){
-    return new Promise((resolve , reject) => {
-        $.getJSON("/ajax/"+ontology+"/instances/"+ encodeURIComponent(uri))
-            .done((data) => resolve(data))
-            .fail((error)=> reject(error))
-    })
-}
-
-/**
- * Return the conceptid from
- * the section url if the section is "instances"
- * @returns {*|string}
- */
-function getInstanceConceptId() {
-    const ont_viewer_data =  jQuery(document).data().bp.ont_viewer
-    const concept_id = ont_viewer_data.concept_id
-    return ont_viewer_data.content_section === "instances" ? (concept_id === "root" ? "" :concept_id)  : ""
-}
-
-
-function setConceptId(conceptId) {
-    jQuery(document).data().bp.ont_viewer.concept_id = conceptId
+    static setConceptId(conceptId) {
+        jQuery(document).data().bp.ont_viewer.concept_id = conceptId
+    }
 }
