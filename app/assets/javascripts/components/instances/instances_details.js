951f2df40f03b560ee9b036457f752e2d9507671
Syphax Bouazzouni <gs_bouazzouni@esi.dz>
2021-12-29 18:36:03 +0100

    update code  to use InstaneLabelLink

--- app/assets/javascripts/components/instances/instances_details.js
+++ app/assets/javascripts/components/instances/instances_details.js
@@ -3,20 +3,24 @@
  */
 class InstanceDetails{
 
-    constructor(ontology, uri , types , properties) {
-        this.uri = uri
-        this.types = types
-        this.properties = properties
+    /**
+     *
+     * @param ontology {String}
+     * @param instance {Instance}
+     */
+    constructor(ontology, instance) {
+        this.instance = instance
         this.ontology = ontology
+        console.log(this)
     }
 
     render(){
 
-        const instanceLabel = ConceptLabelLink.render(this.uri , InstancesHelper.getInstanceHref(this.uri) , "_blank")
-        const classesLabels = this.types.map(x => AjaxConceptLabelLink.render(this.ontology, x), "_blank")
+        const instanceLabel = InstanceLabelLink.render(this.instance, "","_blank" )
+        const classesLabels = this.instance.types.map(x => AjaxConceptLabelLink.render(this.ontology, x), "_blank")
         const propertyLabel = (x) => ConceptLabelLink.render(x , InstancesHelper.getPropertyHref(x))
         const propertyValueLabel = (x) => (UriHelper.isURI(x) ? ConceptLabelLink.render(x , InstancesHelper.getInstanceHref(x) , "_blank") : x)
-
+        let properties = this.instance.properties
         let container = $(`<div>
                     <h4>Details of  ${instanceLabel} of type : ${classesLabels}</h4>
             </div>`)
@@ -30,8 +34,8 @@ class InstanceDetails{
             </table>`)
 
         let tbody = $(`<tbody></tbody>`)
-        delete this.properties["http://www.w3.org/1999/02/22-rdf-syntax-ns#type"]
-        Object.entries(this.properties).forEach((x) => {
+        delete properties["http://www.w3.org/1999/02/22-rdf-syntax-ns#type"]
+        Object.entries(properties).forEach((x) => {
             let row = `<tr><td>${propertyLabel(x[0])}</td><td>${x[1].map(x => propertyValueLabel(x)).join(',')}</td></tr>`
             tbody.append(row)
         })
