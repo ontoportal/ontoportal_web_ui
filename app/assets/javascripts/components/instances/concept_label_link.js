78fd6f4e697afedc7cc67c2300d2b5a4478ce60a
Syphax Bouazzouni <gs_bouazzouni@esi.dz>
2021-12-29 18:30:32 +0100

    clean unused function

--- app/assets/javascripts/components/instances/concept_label_link.js
+++ app/assets/javascripts/components/instances/concept_label_link.js
@@ -1,7 +1,3 @@
-function ajaxGetClassLabelLink(ontologyAcronym, conceptId ,  target="") {
-    return `<a id="${conceptId}" href="${getClassHref(conceptId)}" target="${target}">${conceptId}</a>`
-}
-
 class ConceptLabelLink {
     static render(conceptId , href , target="" , label=""){
         label ||= UriHelper.extractLabelFrom(conceptId)
