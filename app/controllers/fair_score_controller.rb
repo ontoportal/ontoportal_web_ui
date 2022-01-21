0fb3bbc22f520b820b574805a4962e746eac784c
Syphax Bouazzouni <gs_bouazzouni@esi.dz>
2022-01-21 07:11:07 +0100

    fix #54, print a description for the FAIR score details view

--- app/controllers/fair_score_controller.rb
+++ app/controllers/fair_score_controller.rb
@@ -16,15 +16,16 @@ class FairScoreController < ApplicationController
 
   def get_fair
     not_found if params[:ontologies].nil? || params[:ontologies].empty?
-    ontologies = params[:ontologies]
+    @ontologies = params[:ontologies]
     begin
-      if ontologies.include? ','
-        @fair_scores_data = create_fair_scores_data(get_fair_combined_score(ontologies), ontologies.split(',').length)
-      elsif ontologies.eql? 'all'
-        tmp = get_fairness_json(ontologies)
+      if @ontologies.include? ','
+        @fair_scores_data = create_fair_scores_data(get_fair_combined_score(@ontologies), @ontologies.split(',').length)
+      elsif @ontologies.eql? 'all'
+        tmp = get_fairness_json(@ontologies)
         @fair_scores_data = create_fair_scores_data(tmp['combinedScores'], tmp['ontologies'].keys.length)
       else
-        @fair_scores_data = create_fair_scores_data(get_fair_score(ontologies).values.first ,1)
+        @rest_uri = "#{REST_URI}/ontologies/#{@ontologies}/latest_submission?display=all"
+        @fair_scores_data = create_fair_scores_data(get_fair_score(@ontologies).values.first,1)
       end
     rescue NameError
       raise StandardError, 'Error: load failed'
