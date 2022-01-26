3e3a9f718f5a53d5971391c23025ab8033b88ca4
Syphax Bouazzouni <gs_bouazzouni@esi.dz>
2022-01-26 11:40:18 +0100

    fix #30 add the help tooltip for the dates in the submissions table

--- app/controllers/ontologies_controller.rb
+++ app/controllers/ontologies_controller.rb
@@ -8,7 +8,7 @@ class OntologiesController < ApplicationController
   layout :determine_layout
 
   before_action :authorize_and_redirect, :only=>[:edit,:update,:create,:new]
-
+  before_action :submission_metadata, only: [:show]
   KNOWN_PAGES = Set.new(["terms", "classes", "mappings", "notes", "widgets", "summary", "properties"])
   EXTERNAL_MAPPINGS_GRAPH = "http://data.bioontology.org/metadata/ExternalMappings"
   INTERPORTAL_MAPPINGS_GRAPH = "http://data.bioontology.org/metadata/InterportalMappings"
