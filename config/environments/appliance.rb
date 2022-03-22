632daf387334fae363d8e581fb6d1e8a9b5a28f4
rasmi <abdelwadoudrasmi@gmail.com>
2022-03-22 09:52:38 -0400

    working version on the server

--- config/environments/appliance.rb
+++ config/environments/appliance.rb
@@ -21,7 +21,7 @@ BioportalWebUi::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :terser
+  config.assets.js_compressor = Uglifier.new(harmony: true)
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
@@ -78,8 +78,8 @@ BioportalWebUi::Application.configure do
   require Rails.root.join('config', "bioportal_config_#{Rails.env}.rb")
 
   # memcache setup
-  config.cache_store = ActiveSupport::Cache::MemCacheStore.new('localhost', namespace: 'BioPortal' , expires_in: 1.day)
-
+  # https://github.com/mperham/dalli#usage-with-rails-3x-and-4x
+  config.cache_store = :dalli_store, nil, { :namespace => 'bioportal_web_ui', :expires_in => 1.day }
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
