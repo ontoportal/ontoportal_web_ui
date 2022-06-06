281ba3af7896d826f96b779000e0aeae71cde801
Jennifer Vendetti <vendetti@stanford.edu>
2022-06-06 16:55:19 -0700

    Remove errant binding.pry

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,5 +1,4 @@
 if ENV['USE_RECAPTCHA'].present? && ENV['USE_RECAPTCHA'] == 'true'
-  binding.pry
   Recaptcha.configure do |config|
     config.site_key   = Rails.application.credentials.recaptcha[:site_key]
     config.secret_key = Rails.application.credentials.recaptcha[:secret_key]
