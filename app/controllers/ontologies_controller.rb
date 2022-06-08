9eb3b331c4160c27f525e1669385916372bd990c
Syphax Bouazzouni <gs_bouazzouni@esi.dz>
2022-06-08 20:02:19 +0200

    add get schemes URIs in ontologies classes controller

--- app/controllers/ontologies_controller.rb
+++ app/controllers/ontologies_controller.rb
@@ -176,22 +176,26 @@ display_context: false, include: browse_attributes)
   end
 
   def classes
-    @instance_details, type = get_instance_and_type(params[:instanceid])
+    @submission = get_ontology_submission_ready(@ontology)
+    get_class(params)
 
-
-    unless @instance_details.empty? || type.nil? || concept_id_param_exist?(params)
-      params[:conceptid] = type # set class id from the type of the specified instance id
+    if @submission.hasOntologyLanguage === 'SKOS'
+      @schemes =  get_schemes(@ontology.acronym)
+    else
+      @instance_details, type = get_instance_and_type(params[:instanceid])
+      unless @instance_details.empty? || type.nil? || concept_id_param_exist?(params)
+        params[:conceptid] = type # set class id from the type of the specified instance id
+      end
+      @instances_concept_id = get_concept_id(params, @concept, @root)
     end
 
-    get_class(params)
-    @instances_concept_id = get_concept_id(params, @concept, @root)
 
     if ['application/ld+json', 'application/json'].include?(request.accept)
       render plain: @concept.to_jsonld, content_type: request.accept and return
     end
 
     @current_purl = @concept.purl if $PURL_ENABLED
-    @submission = get_ontology_submission_ready(@ontology)
+
     unless @concept.id == 'bp_fake_root'
       @notes = @concept.explore.notes
       @mappings = get_concept_mappings(@concept)
