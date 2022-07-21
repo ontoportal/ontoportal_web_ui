d231dc69b74c4c6511aa2332f516cb6363f71c4f
Mouad Bouchnaf <mouadbouchnaf25@gmail.con>
2022-07-21 10:30:23 +0200

    fix errors on the last PR (optimizing code )

--- app/controllers/ontologies_metadata_curator_controller.rb
+++ app/controllers/ontologies_metadata_curator_controller.rb
@@ -1,42 +1,40 @@
 class OntologiesMetadataCuratorController < ApplicationController
     layout :determine_layout
-
-
+    before_action :submission_metadata, only: [:result, :edit, :update]
 
     def result
+        @ontology = LinkedData::Client::Models::Ontology.all.detect {|e| e.acronym == "INRAETHES"}
+        @submission = @ontology.explore.latest_submission
         @ontologies_ids = params[:ontology][:ontologyId].drop(1)
         @metadata_sel = params[:search][:metadata].drop(1)
         @ontologies = []
         @submissions = []
+        @allOntologies = LinkedData::Client::Models::Ontology.all
+        @allsubmissions = LinkedData::Client::Models::OntologySubmission.all
         @ontologies_ids.each do |data|
-            @ontologies << LinkedData::Client::Models::Ontology.all.detect {|e| e.acronym == data}
-            @submissions << LinkedData::Client::Models::OntologySubmission.all.detect {|e| e.ontology.acronym == data}
+            @ontologies << @allOntologies.detect {|e| e.acronym == data}
+            @submissions << @allsubmissions.detect {|e| e.ontology.acronym == data}
         end
         if @ontologies.empty?
-            @ontologies = LinkedData::Client::Models::Ontology.all.sort_by {|e| e.name}
-            @submissions = LinkedData::Client::Models::OntologySubmission.all.sort_by {|e| e.ontology.name}
+            @ontologies = @allOntologies.sort_by {|e| e.name}
+            @submissions = @allsubmissions.sort_by {|e| e.ontology.name}
         end
         respond_to do |format|
             format.html { redirect_to admin_index_path}
-            format.turbo_stream { render turbo_stream: turbo_stream.append("choices", partial: "ontologies_metadata_curator/result") }
+            format.turbo_stream { render turbo_stream: turbo_stream.append("selection_metadata_form", partial: "ontologies_metadata_curator/result") }
         end             
     end
     
     def edit
         @selected_ontologies_to_edit = params[:selected_acronyms]
         @selected_metadata_to_edit = params[:selected_metadata]
-        @ontology = LinkedData::Client::Models::Ontology.find_by_acronym(@selected_ontologies_to_edit[0])
-        @submission = LinkedData::Client::Models::OntologySubmission.all.detect {|e| e.ontology.acronym == @ontology[0].acronym}
+        @ontology = LinkedData::Client::Models::Ontology.find_by_acronym("INRAETHES").first
+        #@submission = LinkedData::Client::Models::OntologySubmission.all.detect {|e| e.ontology.acronym == @ontology[0].acronym}
+        @submission = @ontology.explore.latest_submission
         respond_to do |format|
             format.html { redirect_to admin_index_path}
-            format.turbo_stream { render turbo_stream: turbo_stream.replace("editmodal", partial: "ontologies_metadata_curator/form_edit") }
+            format.turbo_stream { render turbo_stream: turbo_stream.replace("edition_metadata_form", partial: "ontologies_metadata_curator/form_edit") }
         end
-        # @selected_ontologies_edit = @ontologies.where(acronym: params[:ontologies_acronymes])
-        #@ontologies = LinkedData::Client::Models::Ontology.find_by_acronym(params[:id])
-        #@ontology = @ontologies.first
-        #@ontology = LinkedData::Client::Models::Ontology.find_by_acronym(params[:id]).first
-        #@submission = @ontology.explore.latest_submission
-        #@submission = LinkedData::Client::Models::OntologySubmission.all.detect {|e| e.ontology.acronym == @ontology.instance_values["acronym"]}
     end    
 
     def update
@@ -52,7 +50,7 @@ class OntologiesMetadataCuratorController < ApplicationController
     private
 
     def metadata_params
-      p = params.require(:submission).permit(:submissionId)
+      p = params.require(:submission).permit(:maxChildCount)
       p.to_h
     end
   
