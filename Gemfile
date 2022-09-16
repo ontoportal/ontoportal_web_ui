08f4ce636893e32dc3435042f28ad293bf5aed11
Alex Skrenchuk <alexskr@stanford.edu>
2022-09-16 12:57:52 -0700

    Add github action deployment workflow
    Uses capistrano to deploy to staging UI on push.
    Production deployments can be done manually

--- Gemfile
+++ Gemfile
@@ -56,12 +56,15 @@ group :staging, :production do
 end
 
 group :development do
+   # Capistrano Deployment
+  gem 'bcrypt_pbkdf', '>= 1.0', '< 2.0', require: false # https://github.com/miloserdow/capistrano-deploy/issues/42
   gem 'capistrano', '~> 3.11', require: false
   gem 'capistrano-bundler', require: false
   gem 'capistrano-locally', require: false
   gem 'capistrano-passenger', require: false
   gem 'capistrano-rails', '~> 1.4', require: false
   gem 'capistrano-yarn', require: false
+  gem 'ed25519', '>= 1.2', '< 2.0', require: false # https://github.com/miloserdow/capistrano-deploy/issues/42
   gem 'html2haml'
   gem 'listen'
   # static code analysis
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -4,9 +4,8 @@
 # server in each group is considered to be the first
 # unless any hosts have the primary property set.
 # Don't declare `role :all`, it's a meta role
-role :app, %w{deploy@example.com}
-role :web, %w{deploy@example.com}
-role :db,  %w{deploy@example.com}
+role :app, %w{ui1.stage.ontoportal.org ui2.stage.ontoportal.org}
+role :db, %w{ui1.stage.ontoportal.org} # sufficient to run db:migrate only on one system
 
 # Extended Server Syntax
 # ======================
@@ -14,8 +13,8 @@ role :db,  %w{deploy@example.com}
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
-
+#server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
+set :log_level, :error
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
 # you can see them in [net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start)
@@ -37,3 +36,14 @@ server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
 #     # password: 'please use keys'
 #   }
 # setting per server overrides global ssh_options
+set :ssh_options, {
+  user: 'deployer',
+  forward_agent: 'true',
+  keys: %w(config/deploy_id_rsa),
+  auth_methods: %w(publickey),
+  # use ssh proxy if UI servers are on a private network
+  proxy: Net::SSH::Proxy::Command.new('ssh deployer@sshproxy.ontoportal.org -W %h:%p')
+}
+
+#private git repo for configuraiton
+PRIVATE_CONFIG_REPO = ENV.include?('PRIVATE_CONFIG_REPO') ? ENV['PRIVATE_CONFIG_REPO'] : 'git@github.com:author/private_config_repo.git'
