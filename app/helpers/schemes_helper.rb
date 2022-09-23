f0f239e3429b667ae9fae9f264e231ee82aa1dce
Syphax Bouazzouni <gs_bouazzouni@esi.dz>
2022-09-23 03:17:27 +0200

    add access classes JSON link

--- app/helpers/schemes_helper.rb
+++ app/helpers/schemes_helper.rb
@@ -47,4 +47,13 @@ module SchemesHelper
   def concept_label_to_show(submission: @submission_latest)
     submission.hasOntologyLanguage == 'SKOS' ? 'Concepts' : 'Classes'
   end
+
+  def section_name(section)
+    if section.eql?('classes')
+      concept_label_to_show
+    else
+      section.capitalize
+    end
+  end
 end
+
