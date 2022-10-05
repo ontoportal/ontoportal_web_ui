a81073c0f29d4486256f286010275659074727d7
Syphax Bouazzouni <gs_bouazzouni@esi.dz>
2022-10-05 09:23:13 +0200

    update turbo helpers for alerts

--- app/helpers/turbo_helper.rb
+++ app/helpers/turbo_helper.rb
@@ -1,10 +1,10 @@
 module TurboHelper
-  def frame_id(suffix)
-    "#{controller_name}_#{suffix}"
+  def frame_id(id, suffix)
+    "#{id}_#{suffix}"
   end
 
-  def alerts_container_id
-    frame_id('alerts_container')
+  def alerts_container_id(id = nil)
+    frame_id(id || controller_name, 'alerts_container')
   end
 
   def alert(type: 'success', &block)
@@ -12,4 +12,9 @@ module TurboHelper
       AlertMessageComponent.new(type: type).render_in(view_context, &block)
     end
   end
+
+  def render_alerts_container(controller_class = nil)
+    render AlertsContainerComponent.new(id: alerts_container_id(controller_class&.controller_name))
+  end
+
 end
