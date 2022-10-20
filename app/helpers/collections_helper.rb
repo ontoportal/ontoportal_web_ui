23ab671bf2ce5ed0dd25be219f761683fcfd8d5c
Syphax Bouazzouni <gs_bouazzouni@esi.dz>
2022-10-20 17:30:51 +0200

    add collections controller and helpers

---
+++ app/helpers/collections_helper.rb
@@ -0,0 +1,52 @@
+module CollectionsHelper
+
+  def collections_namespace(ontology_acronym)
+    "/ontologies/#{ontology_acronym}/collections"
+  end
+
+  def get_collections(ontology_acronym)
+    LinkedData::Client::HTTP
+      .get(collections_namespace(ontology_acronym))
+  end
+
+  def get_collection(ontology_acronym, collection_uri)
+    LinkedData::Client::HTTP
+      .get("#{collections_namespace(ontology_acronym)}/#{CGI.escape(collection_uri)}", { include: 'all' })
+  end
+
+  def get_collection_label(collection)
+    if collection['prefLabel'].nil? || collection['prefLabel'].empty?
+      extract_label_from(collection['@id']).html_safe
+    else
+      collection['prefLabel']
+    end
+  end
+
+  def get_collections_labels(collections, main_uri = '')
+
+    selected_label = nil
+    collections_labels = []
+    collections.each do  |x|
+      id = x['@id']
+      label = get_collection_label(x)
+      if id.eql? main_uri
+        selected_label = { 'prefLabel' => label, '@id' => id }
+      else
+        collections_labels.append( { 'prefLabel' => label, '@id' => id })
+      end
+    end
+    collections_labels.sort_by! { |s|  s['prefLabel']}
+    collections_labels.unshift selected_label if selected_label
+    [collections_labels, selected_label]
+  end
+
+
+  def collection_path(collection_id = '')
+    "/ontologies/#{@ontology.acronym}/collections/show_collection?id=#{escape(collection_id)}"
+  end
+
+  def request_collection_id
+    params[:id] || params[:collection_id] || params[:concept_collection]
+  end
+end
+
