fe5ce8f1f654b8f50d50c317dbba4047a75d822d
Syphax Bouazzouni <gs_bouazzouni@esi.dz>
2022-10-20 17:28:42 +0200

    update show_section?(section) to sections_to_show in ontologies_viewer

--- app/helpers/ontologies_helper.rb
+++ app/helpers/ontologies_helper.rb
@@ -395,16 +395,15 @@ module OntologiesHelper
      }].to_json
   end
 
-  def show_section?(section)
-    if @ontology.summaryOnly
-      section.eql?('summary')
-    elsif section.eql?('schemes')
-      @submission_latest.hasOntologyLanguage == 'SKOS'
-    elsif section.eql?('instances')
-      @submission_latest.hasOntologyLanguage != 'SKOS'
-    else
-      true
+  def sections_to_show
+    sections = ['summary']
+
+    unless @ontology.summaryOnly
+      sections += %w[classes properties notes mappings]
+      sections += %w[schemes collections] if @submission_latest.hasOntologyLanguage == 'SKOS'
+      sections += %w[instances] if @submission_latest.hasOntologyLanguage != 'SKOS'
     end
+    sections
   end
 end
 
