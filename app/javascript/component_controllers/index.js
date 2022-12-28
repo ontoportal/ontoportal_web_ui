06eff93c8a96b28a1a3fabb3775be73a19a6c481
Syphax Bouazzouni <gs_bouazzouni@esi.dz>
2022-12-28 11:02:42 +0100

    add select input view component

--- app/javascript/component_controllers/index.js
+++ app/javascript/component_controllers/index.js
@@ -4,5 +4,8 @@ import TurboModalController from "../../components/turbo_modal_component/turbo_m
 import FileInputLoaderController
     from "../../components/file_input_loader_component/file_input_loader_component_controller";
 
+import Select_input_component_controller
+    from "../../components/select_input_component/select_input_component_controller";
 application.register("turbo-modal", TurboModalController)
 application.register("file-input", FileInputLoaderController)
+application.register("select-input", Select_input_component_controller)
