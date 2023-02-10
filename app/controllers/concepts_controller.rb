acaf97cffb17909c16021c648e1d56ca2025755d
Jennifer Vendetti <vendetti@stanford.edu>
2023-02-10 12:38:03 -0800

    Move repetitive code to a before_action filter

--- app/controllers/concepts_controller.rb
+++ app/controllers/concepts_controller.rb
@@ -2,6 +2,7 @@
 
 class ConceptsController < ApplicationController
   include MappingsHelper
+  before_action :redirect_new_api, except: [:show, :biomixer]
 
   layout 'ontology'
 
@@ -28,49 +29,26 @@ class ConceptsController < ApplicationController
   end
 
   def show_label
-    cls_id = params[:concept]
-    ont_id = params[:ontology]
-    if ont_id.to_i.positive?
-      params_cleanup_new_api
-      stop_words = %w[controller action]
-      redirect_to "#{request.path}#{params_string_for_redirect(params, stop_words: stop_words)}",
-                  status: :moved_permanently
-      return
-    end
-    @ontology = LinkedData::Client::Models::Ontology.find(ont_id)
-    @ontology ||= LinkedData::Client::Models::Ontology.find_by_acronym(ont_id).first
+    @ontology = LinkedData::Client::Models::Ontology.find(params[:ontology])
+    @ontology ||= LinkedData::Client::Models::Ontology.find_by_acronym(params[:ontology]).first
     not_found unless @ontology
     # Retrieve a class prefLabel or return the class ID (URI)
     # - mappings may contain class URIs that are not in bioportal (e.g. obo-xrefs)
-    cls = @ontology.explore.single_class(cls_id)
+    cls = @ontology.explore.single_class(params[:concept])
     # TODO: log any cls.errors
     # TODO: NCBO-402 might be implemented here, but it throws off a lot of ajax result rendering.
     # cls_label = cls.prefLabel({:use_html => true}) || cls_id
-    cls_label = cls.prefLabel || cls_id
+    cls_label = cls.prefLabel || params[:concept]
     render plain: cls_label
   end
 
   def show_definition
-    if params[:ontology].to_i.positive?
-      params_cleanup_new_api
-      stop_words = %w[controller action]
-      redirect_to "#{request.path}#{params_string_for_redirect(params, stop_words: stop_words)}",
-                  status: :moved_permanently
-      return
-    end
     @ontology = LinkedData::Client::Models::Ontology.find(params[:ontology])
     cls = @ontology.explore.single_class(params[:concept])
     render text: cls.definition
   end
 
   def show_tree
-    if params[:ontology].to_i.positive?
-      params_cleanup_new_api
-      stop_words = %w[controller action]
-      redirect_to "#{request.path}#{params_string_for_redirect(params, stop_words: stop_words)}",
-                  status: :moved_permanently
-      return
-    end
     @ontology = LinkedData::Client::Models::Ontology.find_by_acronym(params[:ontology]).first
     if @ontology.nil?
       not_found
@@ -81,13 +59,6 @@ class ConceptsController < ApplicationController
   end
 
   def property_tree
-    if params[:ontology].to_i.positive?
-      params_cleanup_new_api
-      stop_words = %w[controller action]
-      redirect_to "#{request.path}#{params_string_for_redirect(params, stop_words: stop_words)}",
-                  status: :moved_permanently
-      return
-    end
     @ontology = LinkedData::Client::Models::Ontology.find_by_acronym(params[:ontology]).first
     not_found if @ontology.nil?
     @root = @ontology.property_tree
@@ -96,15 +67,6 @@ class ConceptsController < ApplicationController
 
   def details
     not_found if params[:conceptid].blank?
-
-    if params[:ontology].to_i.positive?
-      orig_id = params[:ontology]
-      params_cleanup_new_api
-      options = { stop_words: %w[controller action id] }
-      redirect_to "#{request.path.sub(orig_id, params[:ontology])}#{params_string_for_redirect(params, options)}",
-                  status: :moved_permanently
-      return
-    end
 
     @ontology = LinkedData::Client::Models::Ontology.find_by_acronym(params[:ontology]).first
     not_found if @ontology.nil?
@@ -129,6 +91,14 @@ class ConceptsController < ApplicationController
     @immediate_load = true
 
     render partial: 'biomixer', layout: false
+  end
+
+  def redirect_new_api
+    return unless params[:ontology].to_i.positive?
+
+    params_cleanup_new_api
+    redirect_to "#{request.path}#{params_string_for_redirect(params, stop_words: %w[controller action])}",
+                status: :moved_permanently
   end
 
   private
