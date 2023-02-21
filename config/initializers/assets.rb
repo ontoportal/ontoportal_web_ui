46a2d860a4bce1234f477079caa98eecb51aff93
Nasreddine Bouchemel <bouchemelnasreddine@gmail.com>
2023-02-21 16:38:41 +0100

    add jsonld to precompile

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,8 +7,8 @@ Rails.application.config.assets.version = "1.0"
 # Rails.application.config.assets.paths << Emoji.images_path
 
 
-# Precompile additional assets.
+# Precompile additional assets. 
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
-Rails.application.config.assets.precompile += %w( vendor.js)
+Rails.application.config.assets.precompile += %w( vendor.js jsonld.js)
