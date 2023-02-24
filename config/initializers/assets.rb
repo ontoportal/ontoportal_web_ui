7b8bf852ad48e00979c24cc9aba6bb628d85207c
Nasreddine Bouchemel <bouchemelnasreddine@gmail.com>
2023-02-24 15:58:40 +0100

    add assets to precompile to fix annotator and landscape

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,4 +11,4 @@ Rails.application.config.assets.version = "1.0"
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
-Rails.application.config.assets.precompile += %w( vendor.js jsonld.js)
+Rails.application.config.assets.precompile += %w( vendor.js jsonld.js d3pie.min jqcloud-1.0.4.min.js vis.min.js bp_landscape.js bp_annotator.js)
