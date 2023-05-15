b5c921d533233c31f19087d682ab26738a85e20f
Syphax Bouazzouni <gs_bouazzouni@esi.dz>
2023-05-15 09:35:47 +0200

    fix notification alert preview and style

--- test/components/previews/notification_component_preview.rb
+++ test/components/previews/notification_component_preview.rb
@@ -2,7 +2,7 @@ class NotificationComponentPreview < ViewComponent::Preview
 
   # @param title text
   # @param message text
-  # @param type select [success, warning, error]
+  # @param type select [success, alert, error]
   # @param auto_remove toggle
 
   def default(title: "Notification message", message: "Here we can type a success or failure message to the user", type: 'success', auto_remove: false)
