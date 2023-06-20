a14b71d6dab502e7629316169e13c190b1d670a3
Jennifer Vendetti <vendetti@stanford.edu>
2023-06-20 14:35:09 -0700

    Result of app:update and db:migrate

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,8 @@ module BioportalWebUi
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
+
     config.exceptions_app = self.routes
 
     config.settings = config_for :settings
