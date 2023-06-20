fc6f9b9383a28bac4f85325445accb06ba4d74ef
Jennifer Vendetti <vendetti@stanford.edu>
2023-06-20 16:34:58 -0700

    Configure column permitted classes
    Addresses the 500 errors reported in #269

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,10 @@ module BioportalWebUi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.active_record.yaml_column_permitted_classes = [
+      ActionController::Parameters,
+      HashWithIndifferentAccess
+    ]
 
     # Configuration for the application, engines, and railties goes here.
     #
