fea1cf09cc8e1fbb856c785a3ef7ce24943a2f6f
Syphax Bouazzouni <gs_bouazzouni@esi.dz>
2023-06-27 19:09:29 +0200

    update search-input component to make it more generic

--- app/components/search_input_component.rb
+++ app/components/search_input_component.rb
@@ -4,12 +4,19 @@ class SearchInputComponent < ViewComponent::Base
 
   renders_one :template
 
-  def initialize(name: '', placeholder:'', actions_links: {}, scroll_down: true, use_cache: true)
+  def initialize(name: '', placeholder: '', actions_links: {},
+                 scroll_down: true, use_cache: true,
+                 ajax_url:,
+                 item_base_url:,
+                 id_key:)
     super
     @name = name
     @placeholder = placeholder
     @actions_links = actions_links
     @use_cache = use_cache
     @scroll_down = scroll_down
+    @ajax_url = ajax_url
+    @item_base_url = item_base_url
+    @id_key = id_key
   end
 end
