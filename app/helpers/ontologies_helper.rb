f08e5a96533e80a7fda679f532fe75f92c891006
Sirine Mhedhbi <sirine18mhedhbi@gmail.com>
2023-06-28 11:27:20 +0200

    use the  view component to restyle button

--- app/helpers/ontologies_helper.rb
+++ app/helpers/ontologies_helper.rb
@@ -483,7 +483,7 @@ module OntologiesHelper
       if definition.is_a?(String)
         html += '<p class="prefLabel">' + definition + '</p>'
       elsif definition.respond_to?(:uri) && definition.uri
-        html+= '<a class="btn btn-sm btn-light ajax-modified-cls" href="' + definition.uri + '">' + definition.uri + '</a>'
+        html+= '<a class="chip_button_container_clickable" href="' + definition.uri + '">' + definition.uri + '</a>'
       end
     end
     return html.html_safe
