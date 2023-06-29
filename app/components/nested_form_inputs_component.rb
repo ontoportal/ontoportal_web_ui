43a9241d8bfeafee1d7efd9a20d9446474529760
Syphax Bouazzouni <gs_bouazzouni@esi.dz>
2023-06-29 21:43:56 +0200

    add header slot to the  nested form input component

--- app/components/nested_form_inputs_component.rb
+++ app/components/nested_form_inputs_component.rb
@@ -3,5 +3,11 @@
 class NestedFormInputsComponent < ViewComponent::Base
 
   renders_one :template
+  renders_one :header
   renders_many :rows
+
+  def initialize(object_name: '')
+    super
+    @object_name = object_name
+  end
 end
