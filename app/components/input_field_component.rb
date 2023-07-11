192f73e33ba2c115d9ce64f5b4205c35914d7701
Bilel Kihal <61744974+Bilelkihal@users.noreply.github.com>
2023-07-11 19:37:19 +0000

    remove style params from input field component

--- app/components/input_field_component.rb
+++ app/components/input_field_component.rb
@@ -1,10 +1,8 @@
 class InputFieldComponent < ViewComponent::Base
-    def initialize(label: , name:, type: "text", choices:[], width: "100%", margin_bottom: "0px")
+    def initialize(label: , name:, type: "text", choices:[])
         @label = label
         @name = name
         @type = type
-        @width = width
         @choices = choices
-        @margin_bottom = margin_bottom
     end
 end
--- test/components/previews/input_field_component_preview.rb
+++ test/components/previews/input_field_component_preview.rb
@@ -3,19 +3,19 @@ class InputFieldComponentPreview < ViewComponent::Preview
     # @param label text    
     
     def default(label: "Label")
-        render InputFieldComponent.new(label: label, name: "name", type: "text", width: "100%", margin_bottom: "0")
+        render InputFieldComponent.new(label: label, name: "name", type: "text")
     end
 
     # @param label text   
 
     def date(label: "Label")
-        render InputFieldComponent.new(label: label, name: "name", type: "date", width: "100%", margin_bottom: "0")
+        render InputFieldComponent.new(label: label, name: "name", type: "date")
     end
 
     # @param label text   
 
     def select(label: "Label")
-        render InputFieldComponent.new(label: label, name: "name", type: "select", choices: ["bilel", "kihal", "best"], width: "100%", margin_bottom: "0")
+        render InputFieldComponent.new(label: label, name: "name", type: "select", choices: ["bilel", "kihal", "best"])
     end
 
 
