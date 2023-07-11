4034e71f54437c53588e339a23ca05ccc4c046ab
Bilel Kihal <61744974+Bilelkihal@users.noreply.github.com>
2023-07-11 22:53:09 +0000

    combine text area input component with input field component

--- app/components/text_area_component.rb
+++
@@ -1,8 +0,0 @@
-class TextAreaComponent < ViewComponent::Base
-    def initialize(label: , name:, width: "100%", margin_bottom: "0px" )
-        @label = label
-        @name = name
-        @width = width
-        @margin_bottom = margin_bottom
-    end
-end
--- test/components/previews/input_field_component_preview.rb
+++ test/components/previews/input_field_component_preview.rb
@@ -18,6 +18,23 @@ class InputFieldComponentPreview < ViewComponent::Preview
         render InputFieldComponent.new(label: label, name: "name", type: "text", hint: hint, error_message: error_message, helper_text: helper_text)
     end
 
+
+    # This is a textarea field:
+    # - To use it without a label: don't give a value to the param label or leave it empty.
+    # - To give it a hint (placeholder): define the param hint with the hind you want to be displayed.
+    # - To put it in error state: define the param error_message with the error message you want to be displayed.
+    # - To give it a helper text (a text displayed under the input field): define the param helper_text with the helper text you want to be displayed.
+    # @param label text    
+    # @param hint text
+    # @param error_message text
+    # @param helper_text text
+
+    def textarea(label: "Label", hint: "", error_message: "", helper_text: "")
+        render InputFieldComponent.new(label: label, name: "name", type: "textarea", hint: hint, error_message: error_message, helper_text: helper_text)
+    end
+
+
+
     # This is a date input field:
     # - To use it without a label: don't give a value to the param label or leave it empty.
     # - To put it in error state: define the param error_message with the error message you want to be displayed.
