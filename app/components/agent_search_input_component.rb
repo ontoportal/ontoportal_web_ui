16e3e931dfb14b8b533806740431d06e17d6479d
Syphax Bouazzouni <gs_bouazzouni@esi.dz>
2023-07-11 05:03:22 +0200

    update agent search input to be more customizable

--- app/components/agent_search_input_component.rb
+++ app/components/agent_search_input_component.rb
@@ -2,9 +2,12 @@
 
 class AgentSearchInputComponent < ViewComponent::Base
 
-  def initialize(id:, organization_only: false )
+  def initialize(id:, agent_type: nil, name_prefix: nil, parent_id: , edit_on_modal: false)
     super
     @id = id
-    @organization_only = organization_only
+    @agent_type = agent_type
+    @name_prefix = name_prefix
+    @parent_id = parent_id
+    @edit_on_modal = edit_on_modal
   end
 end
