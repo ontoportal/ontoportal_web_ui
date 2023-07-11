b44eaae1826350d111d8c9bc8d698754629d0d0b
Bilel Kihal <61744974+Bilelkihal@users.noreply.github.com>
2023-07-11 22:15:05 +0000

    add helper text to input field component

--- app/components/input_field_component.rb
+++ app/components/input_field_component.rb
@@ -1,10 +1,11 @@
 class InputFieldComponent < ViewComponent::Base
-    def initialize(label: "" , name:, type: "text", choices:[], hint: "", error_message: "")
+    def initialize(label: "" , name:, type: "text", choices:[], hint: "", error_message: "", helper_text: "")
         @label = label
         @name = name
         @type = type
         @choices = choices
         @hint  = hint
         @error_message = error_message
+        @helper_text = helper_text
     end
 end
--- test/components/previews/input_field_component_preview.rb
+++ test/components/previews/input_field_component_preview.rb
@@ -4,25 +4,28 @@ class InputFieldComponentPreview < ViewComponent::Preview
     # @param label text    
     # @param hint text
     # @param error_message text
+    # @param helper_text text
 
-    def default(label: "Label", hint: "", error_message: "")
-        render InputFieldComponent.new(label: label, name: "name", type: "text", hint: hint, error_message: error_message)
+    def default(label: "Label", hint: "", error_message: "", helper_text: "")
+        render InputFieldComponent.new(label: label, name: "name", type: "text", hint: hint, error_message: error_message, helper_text: helper_text)
+    end
+
+
+    # @param label text   
+    # @param error_message text
+    # @param helper_text text
+
+    def date(label: "Label", error_message: "", helper_text: "")
+        render InputFieldComponent.new(label: label, name: "name", type: "date", error_message: error_message, helper_text: helper_text)
     end
 
 
     # @param label text   
     # @param error_message text 
+    # @param helper_text text
 
-    def date(label: "Label", error_message: "")
-        render InputFieldComponent.new(label: label, name: "name", type: "date", error_message: error_message)
-    end
-
-
-    # @param label text   
-    # @param error_message text 
-
-    def select(label: "Label", error_message: "")
-        render InputFieldComponent.new(label: label, name: "name", type: "select", choices: ["choice 1", "choice 2", "choice 3"], error_message: error_message)
+    def select(label: "Label", error_message: "", helper_text: "")
+        render InputFieldComponent.new(label: label, name: "name", type: "select", choices: ["choice 1", "choice 2", "choice 3"], error_message: error_message, helper_text: helper_text)
     end
 
 
