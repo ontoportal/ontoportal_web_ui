a78ef1d88b7c9f5dd19a74495dc21a3c597cfdf8
Syphax Bouazzouni <gs_bouazzouni@esi.dz>
2023-07-11 05:21:15 +0200

    update and finalize agents controller actions

--- app/controllers/agents_controller.rb
+++ app/controllers/agents_controller.rb
@@ -1,5 +1,5 @@
 class AgentsController < ApplicationController
-  include TurboHelper
+  include TurboHelper, AgentHelper
   before_action :authorize_and_redirect, :only => [:edit, :update, :create, :new]
 
   def index
@@ -7,19 +7,22 @@ class AgentsController < ApplicationController
   end
 
   def show
-    id = params[:id]&.eql?('fake_id') ? params[:name] : params[:id]
-    @agent = LinkedData::Client::Models::Agent.all(name: id).find { |x| x.name.eql?(id) }
-    @name_prefix = params[:parent_id] ? "[affiliations][#{params[:parent_id]}]" : ''
+    @agent = LinkedData::Client::Models::Agent.all(name: params[:name]).find { |x| x.name.eql?(params[:name]) }
+    @agent_id = params[:id] || agent_id(@agent)
+    @name_prefix = params[:name_prefix] ? "#{params[:name_prefix]}[#{params[:id]}]" : ''
+    @edit_on_modal = params[:edit_on_modal]&.eql?('true')
   end
 
   def ajax_agents
-    @agents = LinkedData::Client::Models::Agent.all(name: params[:name], agentType: params[:organization_only]&.eql?('true') ? 'organization' : '')
+    filters = { name: params[:name] }
+    filters[:agentType] = params[:agent_type] if params[:agent_type]
+    @agents = LinkedData::Client::Models::Agent.all(filters)
     agents_json = @agents.map do |x|
       {
         id: x.id,
         name: x.name,
         type: x.agentType,
-        identifiers: x.identifiers.map { |i| i.schemaAgency + ':' + i.notation }.join(', ')
+        identifiers: x.identifiers.map { |i| "#{i.schemaAgency}:#{i.notation}" }.join(', ')
       }
     end
 
@@ -28,36 +31,59 @@ class AgentsController < ApplicationController
 
   def new
     @agent = LinkedData::Client::Models::Agent.new
+    @agent.id = params[:id]
     @agent.creator = session[:user].id
     @agent.agentType = params[:type] || 'person'
     @agent.name = params[:name]
     @new_agent = params[:new_agent].nil? || params[:new_agent].eql?('true')
+    @name_prefix = params[:name_prefix] || ''
+    @show_affiliations = params[:show_affiliations]&.eql?('true')
   end
 
   def create
     new_agent = save_agent(agent_params)
+    parent_id = params[:parent_id]
+    name_prefix = params[:name_prefix]
+    alert_id = agent_id_alert_container_id(params[:id], parent_id)
+
     if new_agent.errors
-      render_turbo_stream alert_error { JSON.pretty_generate(response_errors(new_agent)) }
+      render_turbo_stream alert_error(id: alert_id) { JSON.pretty_generate(response_errors(new_agent)) }
     else
       success_message = 'New Agent added successfully'
-      render_turbo_stream(alert_success { success_message },
-                          prepend('agents_table_content', partial: 'agents/show_line', locals: { agent: new_agent }))
+      streams = [alert_success(id: alert_id) { success_message }]
+
+      streams << prepend('agents_table_content', partial: 'agents/show_line', locals: { agent: new_agent })
+
+      streams << replace_agent_form(new_agent, frame_id: params[:id], parent_id: parent_id, name_prefix: name_prefix) if params[:parent_id]
+
+      render_turbo_stream(*streams)
     end
   end
 
   def edit
     @agent = LinkedData::Client::Models::Agent.find("#{REST_URI}/Agents/#{params[:id]}")
+    @name_prefix = params[:parent_id] || ''
+    @show_affiliations = params[:show_affiliations].nil? || params[:show_affiliations].eql?('true')
   end
 
   def update
     agent_update, agent = update_agent(params[:id].split('/').last, agent_params)
+    parent_id = params[:parent_id]
+    alert_id = agent_alert_container_id(agent, parent_id)
 
     if response_error?(agent_update)
-      render_turbo_stream(alert_error { JSON.pretty_generate(response_errors(agent_update)) })
+      render_turbo_stream(alert_error(id: alert_id) { JSON.pretty_generate(response_errors(agent_update)) })
     else
       success_message = 'Agent successfully updated'
-      render_turbo_stream(alert_success { success_message },
-                          replace(agent.id.split('/').last, partial: 'agents/show_line', locals: { agent: agent }))
+      table_line_id = agent_table_line_id(agent_id(agent))
+
+      streams = [alert_success(id: alert_id) { success_message },
+                 replace(table_line_id, partial: 'agents/show_line', locals: { agent: agent })
+      ]
+
+      streams << replace_agent_form(agent, parent_id: parent_id) if params[:parent_id]
+
+      render_turbo_stream(*streams)
     end
   end
 
@@ -83,7 +109,7 @@ class AgentsController < ApplicationController
         if error.nil?
           render turbo_stream: [
             alert(type: 'success') { success_text },
-            turbo_stream.remove(params[:id])
+            turbo_stream.remove(agent_table_line_id(params[:id]))
           ]
 
         else
@@ -96,6 +122,14 @@ class AgentsController < ApplicationController
   end
 
   private
+
+  def replace_agent_form(agent, frame_id: nil, parent_id:, partial: 'agents/agent_show', name_prefix: '')
+
+    frame_id = frame_id ? agent_id_frame_id(frame_id, parent_id) : agent_frame_id(agent, parent_id)
+
+    replace(frame_id, partial: partial,
+            locals: { agent: agent, name_prefix: name_prefix, parent_id: parent_id, edit_on_modal: false })
+  end
 
   def save_agent(params)
     agent = LinkedData::Client::Models::Agent.new(values: params)
@@ -113,8 +147,7 @@ class AgentsController < ApplicationController
   end
 
   def agent_params
-    p = params.permit(:agentType, :name, :email, :acronym, :homepage,
-                      :creator,
+    p = params.permit(:agentType, :name, :email, :acronym, :homepage, :creator,
                       { identifiers: [:notation, :schemaAgency, :creator] },
                       { affiliations: [:id, :agentType, :name, :homepage, :acronym, :creator, { identifiers: [:notation, :schemaAgency, :creator] }] }
     )
--- app/helpers/agent_helper.rb
+++ app/helpers/agent_helper.rb
@@ -1,18 +1,68 @@
 module AgentHelper
 
+  def agent_id_alert_container_id(agent_id, parent_id)
+    "agents_alerts_#{agent_id_frame_id(agent_id, parent_id)}"
+  end
 
+  def agent_alert_container_id(agent, parent_id)
+    agent_id_alert_container_id(agent_id(agent), parent_id)
+  end
+
+  def agent_alert_container(agent, parent_id)
+    render_alerts_container(agent_alert_container_id(agent, parent_id))
+  end
+
+  def agent_id_alert_container(agent_id, parent_id)
+    render_alerts_container(agent_alert_container_id(agent, parent_id))
+  end
+
+  def agent_table_line_id(id)
+    "#{id}_table_item"
+  end
+
+  def agent_frame_id(agent, parent_id)
+    agent_id_frame_id(agent_id(agent), parent_id)
+  end
+
+  def agent_id_frame_id(agent_id, parent_id)
+    return 'application_modal_content' if parent_id.nil?
+
+    return agent_id if parent_id.empty?
+
+    "#{parent_id}_#{agent_id}"
+  end
+
+  def agent_id(agent)
+    agent_id = agent.id
+    agent_id ? agent.id.split('/').last : ''
+  end
+
+  def link_to_agent_edit_modal(agent, parent_id = nil)
+
+    link_to_modal(nil, edit_agent_path(agent_id(agent), parent_id: parent_id, show_affiliations: parent_id.nil? || parent_id.empty?), class: 'btn btn-sm btn-light', data: { show_modal_title_value: "Edit agent #{agent.id}" }) do
+      content_tag(:i, '', class: 'far fa-edit')
+    end
+  end
+
+  def link_to_agent_edit(agent, parent_id = nil)
+    link_to(edit_agent_path(agent_id(agent), parent_id: parent_id, show_affiliations: parent_id.nil? || parent_id.empty?), class: 'btn btn-sm btn-light') do
+      content_tag(:i, '', class: 'far fa-edit')
+    end
+  end
 
   def affiliation?(agent)
     agent.agentType.eql?('organization')
   end
 
-
   def display_identifiers(identifiers)
-    Array(identifiers).map {|i| "#{i["schemaAgency"]} / #{i["notation"]}"}.join(', ')
+    Array(identifiers).map { |i| "#{i["schemaAgency"]}:#{i["notation"]}" }.join(', ')
   end
 
-  def agent_field_name(name, name_prefix='')
-    name_prefix.empty?  ? name : "#{name_prefix}[#{name}]"
+  def display_agent(agent)
+    agent.name + '(' + display_identifiers(agent.identifiers) + ')'
+  end
+  def agent_field_name(name, name_prefix = '')
+    name_prefix&.empty? ? name : "#{name_prefix}[#{name}]"
   end
 
   def agent_identifier_name(index, name, name_prefix)
