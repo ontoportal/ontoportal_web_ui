51eb8ba6f3c9a616fbbc68f253d9f63b11f75552
Bilel Kihal <61744974+Bilelkihal@users.noreply.github.com>
2023-07-11 22:02:29 +0000

    add error state to input field component

--- app/components/input_field_component.rb
+++ app/components/input_field_component.rb
@@ -1,9 +1,10 @@
 class InputFieldComponent < ViewComponent::Base
-    def initialize(label: "" , name:, type: "text", choices:[], hint: "")
+    def initialize(label: "" , name:, type: "text", choices:[], hint: "", error_message: "")
         @label = label
         @name = name
         @type = type
         @choices = choices
         @hint  = hint
+        @error_message = error_message
     end
 end
--- test/components/previews/input_field_component_preview.rb
+++ test/components/previews/input_field_component_preview.rb
@@ -3,23 +3,26 @@ class InputFieldComponentPreview < ViewComponent::Preview
 
     # @param label text    
     # @param hint text
+    # @param error_message text
 
-    def default(label: "Label", hint: "")
-        render InputFieldComponent.new(label: label, name: "name", type: "text", hint: hint)
+    def default(label: "Label", hint: "", error_message: "")
+        render InputFieldComponent.new(label: label, name: "name", type: "text", hint: hint, error_message: error_message)
     end
 
 
     # @param label text   
+    # @param error_message text 
 
-    def date(label: "Label")
-        render InputFieldComponent.new(label: label, name: "name", type: "date")
+    def date(label: "Label", error_message: "")
+        render InputFieldComponent.new(label: label, name: "name", type: "date", error_message: error_message)
     end
 
 
     # @param label text   
+    # @param error_message text 
 
-    def select(label: "Label")
-        render InputFieldComponent.new(label: label, name: "name", type: "select", choices: ["bilel", "kihal", "best"])
+    def select(label: "Label", error_message: "")
+        render InputFieldComponent.new(label: label, name: "name", type: "select", choices: ["choice 1", "choice 2", "choice 3"], error_message: error_message)
     end
 
 
