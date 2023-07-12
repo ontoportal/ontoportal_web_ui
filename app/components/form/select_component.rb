106b79c1dab67491f7ad4b3db874040d8d906436
Syphax Bouazzouni <gs_bouazzouni@esi.dz>
2023-07-12 19:55:57 +0200

    update select component to use InputFieldComponent to have a label

--- app/components/form/select_component.rb
+++ app/components/form/select_component.rb
@@ -1,8 +1,13 @@
 # frozen_string_literal: true
 
-class Form::SelectComponent < SelectInputComponent
+class Form::SelectComponent < InputFieldComponent
 
-  def initialize(id:, name:, values:, selected:, multiple: false, open_to_add_values: false)
-    super(id: id, name: name, values: values, selected: selected, multiple: multiple, open_to_add_values: open_to_add_values)
+  def initialize(id: nil, label: '', name:, value: [], selected: '', placeholder: '', error_message: '', helper_text: '', multiple: false, open_to_add_values: false)
+    super(label: label, name: name, value: value,  placeholder: placeholder, error_message: error_message, helper_text: helper_text)
+    @values = value
+    @selected = selected
+    @open_to_add_values = open_to_add_values
+    @multiple = multiple
+    @id = id
   end
 end
