7e01a59be31343b6434a66a92dd9105e624cf69c
Syphax Bouazzouni <gs_bouazzouni@esi.dz>
2023-07-12 19:22:11 +0200

    make the input field component generic using a the content slot

--- app/components/input_field_component.rb
+++ app/components/input_field_component.rb
@@ -1,12 +1,29 @@
 class InputFieldComponent < ViewComponent::Base
-    def initialize(label: "" , name:, type: "text", choices:[], hint: "", error_message: "", helper_text: "")
+    def initialize(label: "" , name:, value: 'Syphax', type: 'text', placeholder: "", error_message: "", helper_text: "")
         @label = label
         @name = name
-        @type = type
-        @choices = choices
-        @hint  = hint
+        @placeholder  = placeholder
         @error_message = error_message
         @helper_text = helper_text
+        @value = value
+        @type = type
+    end
+
+
+    def error_style
+        "border-color: var(--error-color);" if error?
+    end
+
+    def error?
+        !@error_message.empty?
+    end
+
+    def help?
+        !@helper_text.empty?
+    end
+
+    def label?
+        !@label.empty?
     end
 end
 
