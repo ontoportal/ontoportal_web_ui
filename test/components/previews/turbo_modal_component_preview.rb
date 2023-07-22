9cd1b41946e12486bde2746969f14915c3ffd464
Syphax Bouazzouni <gs_bouazzouni@esi.dz>
2023-07-22 21:52:26 +0200

    add modal component preview

---
+++ test/components/previews/turbo_modal_component_preview.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class TurboModalComponentPreview < ViewComponent::Preview
+  layout 'component_preview_not_centred'
+
+  include ActionView::Helpers::TagHelper
+  include ActionView::Context
+
+  # @param title text
+  # @param message text
+  def default(message: 'hello you !', title: 'title')
+    render TurboModalComponent.new(title: title, show: true) do
+      content_tag(:div, message, class: 'p-5')
+    end
+  end
+end
