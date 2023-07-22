4afb0f9c08aa81d0cc24e15f4d8605acad20203c
Syphax Bouazzouni <gs_bouazzouni@esi.dz>
2023-07-22 21:51:52 +0200

    add option to modal to show in connect alone

--- app/components/turbo_modal_component.rb
+++ app/components/turbo_modal_component.rb
@@ -3,11 +3,12 @@
 class TurboModalComponent < ViewComponent::Base
   include Turbo::FramesHelper
 
-  def initialize(id: '', title:'', size: 'modal-lg')
+  def initialize(id: '', title: '', size: 'modal-lg', show: false)
     super
     @id = id
     @title = title
     @size = size
+    @show = show
   end
 
 end
