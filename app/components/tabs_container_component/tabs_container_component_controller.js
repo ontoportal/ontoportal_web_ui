5cbff05340f2739668e9d5e6fafe5fbd1703616d
Syphax Bouazzouni <gs_bouazzouni@esi.dz>
2023-07-24 09:52:52 +0200

    add to the ontology viewer tabs a language selector pinned to the right

--- app/components/tabs_container_component/tabs_container_component_controller.js
+++ app/components/tabs_container_component/tabs_container_component_controller.js
@@ -1,39 +1,43 @@
-import { Controller } from "@hotwired/stimulus";
+import {Controller} from "@hotwired/stimulus";
 import {HistoryService} from "../../javascript/mixins/useHistory";
 
 export default class extends Controller {
 
-  connect() {
-    this.event = null
-  }
-
-
-  selectTab(event) {
-    this.event = event
-    if(this.#parameter()){
-      this.#updateURL()
+    connect() {
+        this.event = null
     }
-  }
-
-  #pageId(){
-    return this.event.currentTarget.getAttribute("data-tab-id")
-  }
-
-  #title(){
-    return this.event.currentTarget.getAttribute("data-tab-title")
-  }
-
-  #parameter(){
-    return this.event.currentTarget.getAttribute("data-url-parameter")
-  }
 
 
-  #url(){
-    return `?${this.#parameter()}=${this.#pageId()}`
-  }
+    selectTab(event) {
+        this.event = event
+        if (this.#parameter()) {
+            this.#updateURL()
+        }
+        this.element.dispatchEvent(new CustomEvent("tab-selected", {
+            bubbles: true,
+            detail: {data: {selectedTab: this.#pageId()}}
+        }))
+    }
 
-  #updateURL(){
-    (new HistoryService()).pushState({[this.#parameter()]: this.#pageId()}, this.#title(), this.#url());
-  }
+    #pageId() {
+        return this.event.currentTarget.getAttribute("data-tab-id")
+    }
+
+    #title() {
+        return this.event.currentTarget.getAttribute("data-tab-title")
+    }
+
+    #parameter() {
+        return this.event.currentTarget.getAttribute("data-url-parameter")
+    }
+
+
+    #url() {
+        return `?${this.#parameter()}=${this.#pageId()}`
+    }
+
+    #updateURL() {
+        (new HistoryService()).pushState({[this.#parameter()]: this.#pageId()}, this.#title(), this.#url());
+    }
 
 }
