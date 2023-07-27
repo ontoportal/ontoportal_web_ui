06bce492b0a803c03685ad47f66ef1ef5b588103
Bilel Kihal <61744974+Bilelkihal@users.noreply.github.com>
2023-07-27 12:21:05 +0000

    fix button component in lookbook

--- test/components/previews/buttons/primary_button_component_preview.rb
+++ test/components/previews/buttons/primary_button_component_preview.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 class Buttons::PrimaryButtonComponentPreview < ViewComponent::Preview
+  layout 'component_preview_not_centred'
   def default
-    
-    
-
     def default()
       render Buttons::PrimaryButtonComponent.new(value: "Login", name: "login", type: "regular")
     end
