62786dccdd347e81cba152a146c826244719daf3
Syphax Bouazzouni <gs_bouazzouni@esi.dz>
2023-08-07 01:28:59 +0200

    migrate date, email, password, url and text  component to inline call

--- app/components/input/password_component.rb
+++ app/components/input/password_component.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
-class Input::PasswordComponent < InputFieldComponent
+class Input::PasswordComponent < Input::InputFieldComponent
   def initialize(label: '', name:, value: nil, placeholder: '', error_message: '', helper_text: '')
     super(label: label, name: name, value: value,  placeholder: placeholder, error_message: error_message, helper_text: helper_text)
+  end
+
+  def call
+    render Input::InputFieldComponent.new(label: @label, name: @name, value: @value,  placeholder: @placeholder, error_message: @error_message, helper_text: @helper_text, type: "password")
   end
 end
