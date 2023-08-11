98bc1ec3eb286b044815dc401bc9dce75b7e41f7
Syphax Bouazzouni <gs_bouazzouni@esi.dz>
2023-08-11 03:32:20 +0200

    add info tooltip component

---
+++ app/components/display/info_tooltip_component.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class Display::InfoTooltipComponent < ViewComponent::Base
+
+  def initialize(text: )
+    super
+    @text = text
+  end
+  def call
+    image_tag("icons/info.svg", data:{controller:'tooltip'}, title: @text)
+  end
+
+end
