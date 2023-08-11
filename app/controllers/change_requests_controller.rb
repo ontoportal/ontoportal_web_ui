f07cbf04d5d23d2a3dc485882673ded46a78ae15
Jennifer Vendetti <vendetti@stanford.edu>
2023-08-11 15:19:29 -0700

    Remove exception from before_action

--- app/controllers/change_requests_controller.rb
+++ app/controllers/change_requests_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ChangeRequestsController < ApplicationController
-  before_action :require_login, except: [:create]
+  before_action :require_login
 
   def node_obsoletion
     @concept_label = params[:concept_label]
