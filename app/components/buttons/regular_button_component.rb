6ef57ea50b856fef1e286588cef58b74d6f8c2c6
Syphax Bouazzouni <gs_bouazzouni@esi.dz>
2023-08-21 10:55:03 +0200

    make the call the to displayAnimation event triggered only if enabled

--- app/components/buttons/regular_button_component.rb
+++ app/components/buttons/regular_button_component.rb
@@ -2,7 +2,7 @@ class Buttons::RegularButtonComponent < ViewComponent::Base
   renders_one :icon_left
   renders_one :icon_right
 
-  def initialize(id: "", value:, variant: "primary", color: "normal", href: "", size: "normal", state: "animate", type: 'button')
+  def initialize(id: , value:, variant: "primary", color: "normal", href: "", size: "normal", state: "animate", type: 'button')
     @id = id
     @value = value
     @variant = variant
@@ -23,12 +23,14 @@ class Buttons::RegularButtonComponent < ViewComponent::Base
     warning_class = @color == "warning" ? "warning-button " : " "
     disabled_class = @state == "disabled" ? "disabled-button " : " "
     class_style = "#{@variant}-button regular-button " + danger_class + warning_class + disabled_class + slim_class
+    on_click_event =  load_animation? ?  "displayAnimation(this)" : ''
+
     if link?
-      link_to(@href, class: class_style, onclick: "displayAnimation()", id: @id) do
+      link_to(@href, class: class_style, onclick: on_click_event, id: @id) do
         button_label
       end
     else
-      button_tag(type: @type, class: class_style, onclick: "displayAnimation()", id: @id) do
+      button_tag(type: @type, class: class_style, onclick: on_click_event, id: @id) do
         button_label
       end
     end
