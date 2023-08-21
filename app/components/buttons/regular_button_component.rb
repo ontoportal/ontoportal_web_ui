5cd1c44d4fa712b63d09476c0530dd06a884c0da
Syphax Bouazzouni <gs_bouazzouni@esi.dz>
2023-08-21 07:18:06 +0200

    refactor regular button components to have less arguments and code duplication

--- app/components/buttons/regular_button_component.rb
+++ app/components/buttons/regular_button_component.rb
@@ -1,15 +1,44 @@
 class Buttons::RegularButtonComponent < ViewComponent::Base
-    def initialize(name: "", value: ,variant: "primary", type: "regular", style: "regular", color: "normal", icon: "", icon_type:"left", href: "#", size: "normal", state: "animate")
-        @name = name
-        @value = value
-        @variant = variant
-        @type = type
-        @style = style
-        @color = color
-        @icon = icon
-        @icon_type = icon_type
-        @href = href
-        @size = size
-        @state = state
+  renders_one :icon_left
+  renders_one :icon_right
+
+  def initialize(id: "", value:, variant: "primary", color: "normal", href: "", size: "normal", state: "animate")
+    @id = id
+    @value = value
+    @variant = variant
+    @color = color
+    @href = href
+    @size = size
+    @state = state
+  end
+
+  def button_label
+    content_tag(:span, icon_left, class: "#{@variant}-button-icon left-button-icon") + @value + content_tag(:span, icon_right, class: "#{@variant}-button-icon right-button-icon")
+  end
+
+  def button_elem
+    slim_class = @size == "slim" ? "slim " : " "
+    danger_class = @color == "danger" ? "danger-button " : " "
+    warning_class = @color == "warning" ? "warning-button " : " "
+    disabled_class = @state == "disabled" ? "disabled-button " : " "
+    class_style = "#{@variant}-button regular-button " + danger_class + warning_class + disabled_class + slim_class
+    if link?
+      link_to(@href, class: class_style, onclick: "displayAnimation()", id: @id) do
+        button_label
+      end
+    else
+      button_tag(type: 'button', class: class_style, onclick: "displayAnimation()", id: @id) do
+        button_label
+      end
     end
+  end
+
+  def link?
+    @href && !@href.empty?
+  end
+
+  def load_animation?
+    @state == "animate"
+  end
+
 end
