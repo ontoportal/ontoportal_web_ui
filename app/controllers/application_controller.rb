b7cdcbe480dba9f292015c229592d4fcad15e618
SirineMhedhbi <31127782+SirineMhedhbi@users.noreply.github.com>
2023-10-03 10:53:21 +0200

    Fix: missing roots display (#348)
    * remove dead code
    
    * return the first concept if there is no roots
    
    * return AlertMessageComponent if skos and empty roots
    
    * Fix message text

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -466,8 +466,8 @@ class ApplicationController < ActionController::Base
         root_child = @root.children.first
 
         @concept = root_child.explore.self(full: true)
-        # Some ontologies have "too many children" at their root. These will not process and are handled here.
-        if @concept.nil?
+         # Some ontologies have "too many children" at their root. These will not process and are handled here.
+         if @concept.nil?
           LOG.add :debug, "Missing class #{root_child.links.self}"
           not_found("Missing class #{root_child.links.self}")
         end
