1f05f387d65be064d92685b7cfb05d6a9cd5c823
Syphax bouazzouni <gs_bouazzouni@esi.dz>
2023-11-18 18:03:54 +0100

    Feature: Implement CI deployment  for development servers  (#251)
    * add test and appliance environment to deploy action
    
    * add PRIVATE_CONFIG_REPO environment for the deploy action
    
    * revert removing the jumphost step in deploy action
    
    * set default deploy branch for each of the environments
    
    * set the deployment rails_env to "appliance" for all the environments
    
    * use a variable for the config folder path to use in the get_config task
    
    * extract deploy ssh_options into the deploy.rb file
    
    * add test environment capistrano deploy file
    
    * make the jumpbox  host and username configurable for the deployment
    
    * make PRIVATE_CONFIG_REPO default example use https with github PAT token
    
    * remove the on push in development branch auto deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # Don't declare `role :all`, it's a meta role
 role :app, %w{ui1.prd.ontoportal.org ui2.prd.ontoportal.org}
 role :db, %w{ui1.prd.ontoportal.org} # sufficient to run db:migrate only on one system
-
+set :branch, ENV.include?('BRANCH') ? ENV['BRANCH'] : 'master'
 # Extended Server Syntax
 # ======================
 # This can be used to drop a more detailed server
@@ -15,35 +15,3 @@ role :db, %w{ui1.prd.ontoportal.org} # sufficient to run db:migrate only on one 
 # extended properties on the server.
 #server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
 set :log_level, :error
-# you can set custom ssh options
-# it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
-# you can see them in [net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start)
-# set it globally
-#  set :ssh_options, {
-#    keys: %w(/home/rlisowski/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
-# and/or per server
-# server 'example.com',
-#   user: 'user_name',
-#   roles: %w{web app},
-#   ssh_options: {
-#     user: 'user_name', # overrides user setting above
-#     keys: %w(/home/user_name/.ssh/id_rsa),
-#     forward_agent: false,
-#     auth_methods: %w(publickey password)
-#     # password: 'please use keys'
-#   }
-# setting per server overrides global ssh_options
-set :ssh_options, {
-  user: 'deployer',
-  forward_agent: 'true',
-  keys: %w(config/deploy_id_rsa),
-  auth_methods: %w(publickey),
-  # use ssh proxy if UI servers are on a private network
-  proxy: Net::SSH::Proxy::Command.new('ssh deployer@sshproxy.ontoportal.org -W %h:%p')
-}
-
-#private git repo for configuraiton
-PRIVATE_CONFIG_REPO = ENV.include?('PRIVATE_CONFIG_REPO') ? ENV['PRIVATE_CONFIG_REPO'] : 'git@github.com:author/private_config_repo.git'
--- config/deploy/test.rb
+++ config/deploy/test.rb
@@ -4,9 +4,8 @@
 # server in each group is considered to be the first
 # unless any hosts have the primary property set.
 # Don't declare `role :all`, it's a meta role
-role :app, %w{stageportal.lirmm.fr}
-role :db, %w{stageportal.lirmm.fr} # sufficient to run db:migrate only on one system
-set :branch, ENV.include?('BRANCH') ? ENV['BRANCH'] : 'stage'
+role :app, %w{testportal.lirmm.fr}
+role :db, %w{testportal.lirmm.fr} # sufficient to run db:migrate only on one system
 # Extended Server Syntax
 # ======================
 # This can be used to drop a more detailed server
@@ -15,3 +14,4 @@ set :branch, ENV.include?('BRANCH') ? ENV['BRANCH'] : 'stage'
 # extended properties on the server.
 #server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
 set :log_level, :error
+set :branch, ENV.include?('BRANCH') ? ENV['BRANCH'] : 'test'
