06ac94c1a89e7845257a45119fbefab4b796bd72
Jennifer Vendetti <vendetti@stanford.edu>
2024-01-31 16:02:19 -0800

    Update manifest.js

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,7 @@
 //= link_tree ../images
-//= link_directory ../javascripts .js
-//= link_directory ../stylesheets .css
 //= link_tree ../builds
+//= link application.js
+//= link bp_annotator.js
+//= link bp_annotatorplus.js
+//= link vendor.js
+//= link_directory ../stylesheets .css
