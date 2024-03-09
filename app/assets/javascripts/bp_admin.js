364a48439823e386a4b936383076585fa88afa5c
Jennifer Vendetti <vendetti@stanford.edu>
2024-03-08 16:01:58 -0800

    Encode username component in login_as link
    Resolves #299

--- app/assets/javascripts/bp_admin.js
+++ app/assets/javascripts/bp_admin.js
@@ -892,7 +892,7 @@ function populateUserRows(data) {
         let actions = [
             '<a href="/accounts/'+ user['username'] +'"  class="mx-1">Detail</a>' ,
             '<a href="javascript:;" class="delete-user mx-1" data-account-name="' + username + '">Delete</a>',
-            '<a href="/login_as/'+ username +'" class="mx-1">Login as</a>',
+            '<a href="/login_as/'+ encodeURIComponent(username) +'" class="mx-1">Login as</a>',
 
         ]
         let row = [firstname, lastname, username, email , roles , id , created , actions.join('|')];
