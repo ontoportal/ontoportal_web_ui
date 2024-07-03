2f0f39d748bde0c92208c74843c98d5fb117c468
Jennifer Vendetti <vendetti@stanford.edu>
2024-07-03 16:47:17 -0700

    Fix some RuboCop warnings

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,5 +1,6 @@
-class LoginController < ApplicationController
+# frozen_string_literal: true
 
+class LoginController < ApplicationController
   layout :determine_layout
 
   def index
@@ -7,7 +8,7 @@ class LoginController < ApplicationController
     if params[:redirect]
       # Get the original, encoded redirect
       uri = URI.parse(request.url)
-      orig_params = Hash[uri.query.split("&").map {|e| e.split("=",2)}].symbolize_keys
+      orig_params = Hash[uri.query.split("&").map { |e| e.split("=", 2) }].symbolize_keys
       session[:redirect] = orig_params[:redirect]
     else
       session[:redirect] = request.referer
@@ -17,31 +18,30 @@ class LoginController < ApplicationController
   # logs in a user
   def create
     @errors = validate(params[:user])
-    if @errors.size < 1
+    if @errors.empty?
       logged_in_user = LinkedData::Client::Models::User.authenticate(params[:user][:username], params[:user][:password])
       if logged_in_user && !logged_in_user.errors
         login(logged_in_user)
-        redirect = "/"
+        redirect = '/'
 
         if session[:redirect]
           redirect = CGI.unescape(session[:redirect])
         end
 
-
         redirect_to redirect
       else
-        @errors << "Invalid account name/password combination"
-        render :action => 'index'
+        @errors << 'Invalid account name/password combination'
+        render action: 'index'
       end
     else
-      render :action => 'index'
+      render action: 'index'
     end
   end
 
   # Login as the provided username (only for admin users)
   def login_as
     unless session[:user] && session[:user].admin?
-      redirect_to "/"
+      redirect_to '/'
       return
     end
 
@@ -54,8 +54,8 @@ class LoginController < ApplicationController
       session[:user].apikey = session[:admin_user].apikey
     end
 
-    #redirect_to request.referer rescue redirect_to "/"
-    redirect_to "/"
+    # redirect_to request.referer rescue redirect_to "/"
+    redirect_to '/'
   end
 
   # logs out a user
@@ -67,25 +67,24 @@ class LoginController < ApplicationController
       flash[:success] = "Logged out <b>#{old_user.username}</b>, returned to <b>#{session[:user].username}</b>".html_safe
     else
       session[:user] = nil
-      flash[:success] = "You have successfully logged out"
+      flash[:success] = 'You have successfully logged out'
     end
-    redirect_to request.referer || "/"
+    redirect_to request.referer || '/'
   end
 
-  def lost_password
-  end
+  def lost_password; end
 
   # Sends a new password to the user
   def send_pass
     username = params[:user][:account_name]
     email = params[:user][:email]
-    resp = LinkedData::Client::HTTP.post("/users/create_reset_password_token", {username: username, email: email})
+    resp = LinkedData::Client::HTTP.post('/users/create_reset_password_token', { username: username, email: email })
 
     if resp.nil?
-      redirect_to login_index_path, notice: "Please check your email for a message with reset instructions"
+      redirect_to login_index_path, notice: 'Please check your email for a message with reset instructions'
     else
-      flash[:notice] = resp.errors.first + ". Please try again."
-      redirect_to "/lost_pass"
+      flash[:notice] = "#{resp.errors.first}. Please try again."
+      redirect_to '/lost_pass'
     end
   end
 
@@ -93,13 +92,13 @@ class LoginController < ApplicationController
     username = params[:un]
     email = params[:em]
     token = params[:tk]
-    @user = LinkedData::Client::HTTP.post("/users/reset_password", {username: username, email: email, token: token})
+    @user = LinkedData::Client::HTTP.post('/users/reset_password', { username: username, email: email, token: token })
     if @user.is_a?(LinkedData::Client::Models::User)
       login(@user)
       render 'passwords/edit'
     else
-      flash[:notice] = @user.errors.first + ". Please reset your password again."
-      redirect_to "/lost_pass"
+      flash[:notice] = "#{@user.errors.first}. Please reset your password again."
+      redirect_to '/lost_pass'
     end
   end
 
@@ -107,6 +106,7 @@ class LoginController < ApplicationController
 
   def login(user)
     return unless user
+
     session[:user] = user
     custom_ontologies_text = session[:user].customOntology && !session[:user].customOntology.empty? ? "The display is now based on your <a href='/account#custom_ontology_set'>Custom Ontology Set</a>." : ""
     notice = "Welcome <b>" + user.username.to_s + "</b>! " + custom_ontologies_text
@@ -114,17 +114,15 @@ class LoginController < ApplicationController
   end
 
   def validate(params)
-    errors=[]
+    errors = []
 
-    if params[:username].nil? || params[:username].length <1
-      errors << "Please enter an account name"
+    if params[:username].empty?
+      errors << 'Please enter an account name'
     end
-    if params[:password].nil? || params[:password].length <1
-      errors << "Please enter a password"
+    if params[:password].empty?
+      errors << 'Please enter a password'
     end
 
-    return errors
+    errors
   end
-
-
 end
