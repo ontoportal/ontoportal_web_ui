a60be0dc24d35d87a6a67cd8caad26cbea658996
Jennifer Vendetti <vendetti@stanford.edu>
2024-09-11 10:10:43 -0700

    Account for https in class IDs
    Resolves #326

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -357,7 +357,7 @@ module ApplicationHelper
     else
       target = " target='#{target}' "
     end
-    if cls_id.start_with? 'http://'
+    if cls_id.start_with?('http://', 'https://')
       href_cls = " href='#{bp_class_link(cls_id, ont_acronym)}' "
       data_cls = " data-cls='#{cls_id}' "
       data_ont = " data-ont='#{ont_acronym}' "
