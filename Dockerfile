f730111e6c53acc57d281383cdf903e9359acf73
Alex Skrenchuk <alexskr@stanford.edu>
2024-10-29 16:56:49 -0700

    docker refactor
    Dockerfile:
    * Change ruby image OS from Alpine to Debian slim-bookworm.
      Alpine is effective for reducing image size; however, using the slim version
      of Debian achieves a similar reduction. We can further reduce image size
      by adopting staged builds and moving Node/Yarn into a separate build stage.
    * Use ruby 3.2
    * Removed docker binaries from the image.
    * Set bundler gem path to /usr/local/bundle
    docker-compose:
    * Add internal/external networks
    * Make node a non-runtime  dependency
    * Do not expose ports for caching/db by default
    config:
    * Add development and test config files.  Config files are redesigend to be
      used with .env so is safe to add

--- Dockerfile
+++ Dockerfile
@@ -1,53 +1,54 @@
 # Make sure it matches the Ruby version in .ruby-version and Gemfile
-ARG RUBY_VERSION=3.2.0
-FROM ruby:${RUBY_VERSION}-alpine
+ARG RUBY_VERSION=3.2
+FROM ruby:${RUBY_VERSION}-slim-bookworm
 
-# Install libvips for Active Storage preview support
-RUN apk add --no-cache build-base \
-                       libxml2-dev \
-                       libxslt-dev \
-                       mariadb-dev \
-                       git \
-                       tzdata \
-                       nodejs yarn \
-                       less \
-                       bash \
-                       docker \
-                       docker-compose \
-    && mkdir /node_modules
-
-# Rails app lives here
 WORKDIR /app
 
-# Set production environment
+RUN bash -c "set -o pipefail && apt-get update && \
+  apt-get install -y --no-install-recommends \
+     build-essential \
+     libxml2 \
+     libxslt-dev \
+     libmariadb-dev \
+     git \
+     curl \
+  && curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key -o /etc/apt/keyrings/nodesource.asc \
+  && echo 'deb [signed-by=/etc/apt/keyrings/nodesource.asc] https://deb.nodesource.com/node_20.x nodistro main' | tee /etc/apt/sources.list.d/nodesource.list \
+  && apt-get update \
+  && apt-get install -y --no-install-recommends nodejs \
+  && corepack enable \
+  && apt-get clean \
+  && rm -rf /var/lib/apt/lists/* /usr/share/doc /usr/share/man \
+  && mkdir /node_modules"
+
 ARG RAILS_ENV="production"
+ARG NODE_ENV="production"
 ARG BUNDLE_WITHOUT="development test"
 
 ENV RAILS_LOG_TO_STDOUT="1" \
     RAILS_SERVE_STATIC_FILES="true" \
     RAILS_ENV="${RAILS_ENV}" \
+    NODE_ENV="${NODE_ENV}" \
     BUNDLE_PATH=/usr/local/bundle \
     BUNDLE_WITHOUT="${BUNDLE_WITHOUT}"
 
-RUN gem update --system 3.4.22 # the 3.4.22 can be removed if we support Ruby version > 3.0
+#PATH="${PATH}:/node_modules/.bin"
+COPY Gemfile* .
+RUN bundle install
+
+COPY package.json *yarn* .
+RUN yarn install
 
 COPY . .
-
-RUN bundle install
-RUN yarn install && yarn build
-
-
 
 RUN cp config/bioportal_config_env.rb.sample config/bioportal_config_production.rb
 RUN cp config/bioportal_config_env.rb.sample config/bioportal_config_development.rb
 RUN cp config/database.yml.sample config/database.yml
 
-# Precompile bootsnap code for faster boot times
-RUN bundle exec bootsnap precompile --gemfile app/ lib/
+RUN if [ "${RAILS_ENV}" != "development" ]; then \
+  bundle exec bootsnap precompile --gemfile app/ lib/ && \
+  SECRET_KEY_BASE_DUMMY=1 ./bin/rails assets:precompile; fi
 
-RUN SECRET_KEY_BASE_DUMMY="1" ./bin/rails assets:precompile
-
-ENV BINDING="0.0.0.0"
 EXPOSE 3000
 
-CMD ["bash"]
+CMD ["rails", "s"]
---
+++ config/bioportal_config_development.rb
@@ -0,0 +1,309 @@
+# Organization info
+$ORG = ENV['ORG']
+$ORG_URL = ENV['ORG_URL']
+
+# Site name (required)
+$SITE = ENV['SITE']
+
+# Full string for site, EX: "NCBO BioPortal", do not modify
+$ORG_SITE = $ORG.nil? || $ORG.empty? ? $SITE : "#{$ORG} #{$SITE}"
+
+# The URL for the BioPortal Rails UI (this application)
+$UI_URL = ENV['UI_URL']
+
+# If you are running a PURL server to provide URLs for ontologies in your BioPortal instance, enable this option
+$PURL_ENABLED = false
+# The PURL URL is generated using this prefix + the abbreviation for an ontology.
+# The PURL URL generation algorithm can be altered in app/models/ontology_wrapper.rb
+$PURL_PREFIX = 'http://purl.bioontology.org/ontology'
+
+
+# If your BioPortal installation includes Annotator set this to false
+$ANNOTATOR_DISABLED = false
+# Unique string representing the UI's id for use with the BioPortal Core
+$API_KEY = ENV['API_KEY']
+# BioPortal API service address
+$REST_URL = ENV['API_URL']
+
+# Annotator REST service address
+# $ANNOTATOR_URL = "http://services.stageportal.lirmm.fr/annotator"
+$ANNOTATOR_URL = $PROXY_URL = ENV['ANNOTATOR_URL']
+# NCBO annotator URL and apikey
+$NCBO_ANNOTATORPLUS_ENABLED = ENV['NCBO_ANNOTATORPLUS_ENABLED']
+$NCBO_ANNOTATOR_URL = ENV['NCBO_ANNOTATOR_URL']
+$NCBO_API_KEY = ENV['NCBO_API_KEY']
+
+# Fairness Assessment.
+$FAIRNESS_DISABLED = ENV['FAIRNESS_DISABLED']
+$FAIRNESS_URL = ENV['FAIRNESS_URL']
+
+# Used to define other bioportal that can be mapped to
+# Example to map to ncbo bioportal : {"ncbo" => {"api" => "http://data.bioontology.org", "ui" => "http://bioportal.bioontology.org", "apikey" => ""}
+# Then create the mapping using the following class in JSON : "http://purl.bioontology.org/ontology/MESH/C585345": "ncbo:MESH"
+# Where "ncbo" is the namespace used as key in the interportal_hash
+$INTERPORTAL_HASH = {}
+
+$NOT_DOWNLOADABLE = {}
+
+# Bugsnag API key for monitoring exception
+#$BUGSNAG_API_KEY=
+
+# OAuth2 authentication
+$OMNIAUTH_PROVIDERS = {
+  github: {
+    client_id: 'CLIENT_ID',
+    client_secret: 'CLIENT_SECRET',
+    icon: 'github.svg',
+    enable: true
+  },
+  google: {
+    strategy: :google_oauth2,
+    client_id: 'CLIENT_ID',
+    client_secret: 'CLIENT_SECRET',
+    icon: 'google.svg',
+    enable: true
+  },
+  orcid: {
+    client_id: 'CLIENT_SECRET',
+    client_secret: 'CLIENT_SECRET',
+    icon: 'orcid.svg',
+    enable: false
+  },
+  keycloak: {
+    strategy: :keycloak_openid,
+    client_id: 'YOUR_KEYCLOAK_CLIENT_ID',
+    client_secret: 'YOUR_KEYCLOAK_CLIENT_SECRET',
+    client_options: { site: 'KEYCLOAK_SITE', realm: 'KEYCLOAK_REALM' },
+    name: 'keycloak',
+    icon: 'keycloak.svg',
+    enable: false
+  }
+}.freeze
+
+# Don't load and don't display recent mappings if false, in case of too many mappings (take longer to load homepage)
+$DISPLAY_RECENT = false
+
+# If true then the UI will get available recognize at API_URL/annotators/recognizers
+$MULTIPLE_RECOGNIZERS = false
+
+# Remove download for these ontologies. Default:
+# ["CPT","ICD10","ICNP","ICPC2P","MDDB","MEDDRA","MSHFRE","MSHSPA_1","NDDF","NDFRT","NIC","RCD","SCTSPA","SNOMEDCT","WHO-ART"]
+$RESTRICTED_DOWNLOADS = []
+
+# Ontolobridge endpoint url
+$ONTOLOBRIDGE_BASE_URL = 'https://ontolobridge.ccs.miami.edu/api-test/requests'
+# Ontolobridge authentication token
+$ONTOLOBRIDGE_AUTHENTICATION_TOKEN = 'Token Uq2pae73ktMtmgjUgtnhEOuHxr9sZeuK'
+
+# Ontologies for which to enable the new term request (Ontolobridge) tab
+$NEW_TERM_REQUEST_ONTOLOGIES = []
+
+# Legacy REST core service address (BioPortal v3.x and lower)
+$LEGACY_REST_URL = 'http://example.org:8080/bioportal'
+
+# Release version text (appears in footer of all pages, except 404 and 500 errors)
+$RELEASE_VERSION = ENV['RELEASE_VERSION']
+
+# Enable Slices, filtering of ontologies based on subdomain and ontology groups
+$ENABLE_SLICES = false
+
+# Google Analytics ID (optional)
+$ANALYTICS_ID = ENV['ANALYTICS_ID']
+
+# Enable client request caching
+$CLIENT_REQUEST_CACHING = true
+
+# Email settings
+ActionMailer::Base.smtp_settings = {
+  address: '', # smtp server address, ex: smtp.example.org
+  port: 25, # smtp server port
+  domain: '' # fqdn of rails server, ex: rails.example.org
+}
+# Announcements mailman mailing list REQUEST address, EX: list-request@lists.example.org
+# NOTE: You must use the REQUEST address for the mailing list. ONLY WORKS WITH MAILMAN LISTS.
+$ANNOUNCE_LIST = ENV['SUPPORT_EMAIL']
+# Email addresses used for sending notifications (errors, feedback, support)
+$SUPPORT_EMAIL = ENV['SUPPORT_EMAIL']
+# Email used to send notifications
+$NOTIFICATION_EMAIL = ENV['SUPPORT_EMAIL']
+
+
+
+# reCAPTCHA
+# In order to use reCAPTCHA on the account creation and feedback submission pages:
+#    1. Obtain a reCAPTCHA v2 key from: https://www.google.com/recaptcha/admin
+#    2. Put the site and secret keys in the encrypted credentials file:
+#
+#       recaptcha:
+#         site_key: your_site_key
+#         secret_key: your_secret_key
+#
+#    3. Set the USE_RECAPTCHA option to 'true'
+ENV['USE_RECAPTCHA'] = 'false'
+# Custom BioPortal logging
+require 'log'
+
+# URL where BioMixer GWT app is located
+# $BIOMIXER_URL = "http://bioportal-integration.bio-mixer.appspot.com"
+$BIOMIXER_URL = ENV['BIOMIXER_URL']
+$BIOMIXER_APIKEY = ENV['BIOMIXER_APIKEY']
+
+##
+# Custom Ontology Details
+# Custom details can be added on a per ontology basis using a key/value pair as columns of the details table
+#
+# Example:
+# $ADDITIONAL_ONTOLOGY_DETAILS = { "STY" => { "Additional Detail" => "Text to be shown in the right-hand column." } }
+##
+$ADDITIONAL_ONTOLOGY_DETAILS = {}
+
+# Front notice appears on the front page only and is closable by the user. It remains closed for seven days (stored in cookie)
+$FRONT_NOTICE = ''
+# Site notice appears on all pages and remains closed indefinitely. Stored below as a hash with a unique key and a string message
+# EX: $SITE_NOTICE = { :unique_key => 'Put your message here (can include <a href="/link">html</a> if you use single quotes).' }
+$SITE_NOTICE = {}
+
+$TERMS_AND_CONDITIONS_LINK = 'https://doc.jonquetlab.lirmm.fr/share/e6158eda-c109-4385-852c-51a42de9a412/doc/terms-conditions-naDsDo2Zxq'
+$CITE_ANNOTATOR = 'https://hal.science/hal-00492024'
+$ANNOTATOR_API_DOC = 'https://data.agroportal.lirmm.fr/documentation#nav_annotator'
+$CITE_RECOMMENDER = 'https://doi.org/10.1186/s13326-017-0128-y' 
+# Resource term
+$RESOURCE_TERM = ENV['RESOURCE_TERM'] || 'ontology'
+
+$HOME_PAGE_LOGOS = [
+    {
+      img_src: 'logos/supports/numev.png',
+      url: 'http://www.lirmm.fr/numev',
+      target: '_blank'
+    },
+    {
+      img_src: 'logos/supports/anr.png',
+      url: 'https://anr.fr/en',
+      target: '_blank'
+    },
+    {
+      img_src: 'logos/supports/eu.png',
+      url: 'https://commission.europa.eu/research-and-innovation_en',
+      target: '_blank'
+    },
+    {
+      img_src: 'logos/collaboration/d2kab.png',
+      url: 'http://d2kab.mystrikingly.com',
+      target: '_blank'
+    },
+    {
+      img_src: 'logos/collaboration/lirmm.png',
+      url: 'http://www.lirmm.fr',
+      target: '_blank'
+    },
+    {
+      img_src: 'logos/collaboration/inrae.png',
+      url: 'https://www.inrae.fr/enm',
+      target: '_blank'
+    },
+    {
+      img_src: 'logos/collaboration/stanford.png',
+      url: 'https://www.stanford.edu',
+      target: '_blank'
+    }
+]
+
+$PORTALS_INSTANCES = [
+  {
+    color: '#31b403',
+    portal: 'AgroPortal',
+    link: 'https://agroportal.lirmm.fr/'
+  },
+  {
+    color: '#234979',
+    portal: 'BioPortal',
+    link: 'https://bioportal.bioontology.org/'
+  },
+  {
+    color: '#74a9cb',
+    portal: 'SIFR BioPortal',
+    link: 'https://bioportal.lirmm.fr/'
+  },
+  {
+    color: '#0d508a',
+    portal: 'EcoPortal',
+    link: 'https://ecoportal.lifewatch.eu/'
+  },
+  {
+    color: '#234979',
+    portal: 'MedPortal',
+    link: 'http://medportal.bmicc.cn/'
+  },
+  {
+    color: '#009574',
+    portal: 'MatPortal',
+    link: 'https://matportal.org/'
+  },
+  {
+    color: '#1c0f5d',
+    portal: 'IndustryPortal',
+    link: 'http://industryportal.enit.fr'
+  },
+  {
+    color: '#1e2251',
+    portal: 'EarthPortal',
+    link: 'https://earthportal.eu/'
+  },
+  {
+    color: '#33691B',
+    portal: 'BiodivPortal',
+    link: 'https://biodivportal.gfbio.org/'
+  }
+]
+
+$ONTOPORTAL_WEBSITE_LINK = "https://ontoportal.org/"
+$ONTOPORTAL_GITHUB_REPO = "https://github.com/ontoportal"
+
+$GITHUB_ISSUES = "https://github.com/agroportal/project-management/issues"
+$FOOTER_LINKS = {
+  social: [
+    { logo: "social/people.svg", link: "https://github.com/orgs/agroportal/people" },
+    { logo: "social/github.svg", link: "https://github.com/agroportal" },
+    { logo: "social/twitter.svg", link: "https://twitter.com/lagroportal" },
+    { logo: "json.svg", link: $REST_URL },
+    { logo: "summary/sparql.svg", link: "#{$SPARQL_URL}"},
+    { logo: "social/email.svg", link: "mailto:#{$ANNOUNCE_LIST}" },
+  ],
+  sections: {
+    products: {
+      release_notes: "https://doc.jonquetlab.lirmm.fr/share/e6158eda-c109-4385-852c-51a42de9a412/doc/release-notes-btKjZk5tU2",
+      api: "https://data.agroportal.lirmm.fr/",
+      tools: "/tools",
+      sparql: "https://sparql.agroportal.lirmm.fr/test/",
+      ontoportal: $ONTOPORTAL_WEBSITE_LINK
+    },
+    support: {
+      contact_us: "https://#{$SITE}.lirmm.fr/feedback",
+      documentation: "https://ontoportal.github.io/documentation/",
+      agro_documentation: "https://doc.jonquetlab.lirmm.fr/share/e6158eda-c109-4385-852c-51a42de9a412/doc/public-documentation-QMpsC9aVBb",
+      issues_and_requests: $GITHUB_ISSUES 
+    },
+    agreements: {
+      terms: $TERMS_AND_CONDITIONS_LINK,
+      privacy_policy: "https://doc.jonquetlab.lirmm.fr/share/e6158eda-c109-4385-852c-51a42de9a412/doc/terms-conditions-naDsDo2Zxq#h-privacy-policy",
+      legal_notices: "https://doc.jonquetlab.lirmm.fr/share/e6158eda-c109-4385-852c-51a42de9a412/doc/terms-conditions-naDsDo2Zxq#h-legal-notice"
+    },
+    about: {
+      about_us: "https://github.com/agroportal/project-management",
+      team: "https://github.com/orgs/agroportal/people",
+      cite_us: "https://doc.jonquetlab.lirmm.fr/share/e6158eda-c109-4385-852c-51a42de9a412/doc/publications-and-references-87tEoeoGKy",
+      acknowledgments: "https://doc.jonquetlab.lirmm.fr/share/e6158eda-c109-4385-852c-51a42de9a412/doc/acknowledgments-15GdRXLQdm"
+    }
+  }
+}
+
+$UI_THEME = ENV['UI_THEME'] || 'ontoportal'
+$HOSTNAME = ENV['API_URL']
+
+if $HOSTNAME
+  $HOSTNAME = ENV['API_URL'].split('data.').last
+  # add custom stage server configuration if needed (e.g bioportal_config_development_stageportal.lirmm.fr)
+  if File.exist?("config/bioportal_config_development_#{$HOSTNAME}")
+    require_relative "bioportal_config_development_#{$HOSTNAME}"
+  end
+end
