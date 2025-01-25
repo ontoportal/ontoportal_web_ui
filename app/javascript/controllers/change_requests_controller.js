d8f0fc7eb5cf497262ea6154715cf9974c274113
Jennifer Vendetti <vendetti@stanford.edu>
2025-01-24 17:39:52 -0800

    Refactor code for hiding proposal forms

--- app/javascript/controllers/change_requests_controller.js
+++ app/javascript/controllers/change_requests_controller.js
@@ -2,27 +2,15 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="change-requests"
 export default class extends Controller {
-  static targets = [ 'addProposalForm', 'editDefinitionForm' ]
-
-  /*
-   * TODO: code for hiding the various change request forms on cancel and submit needs to be refactored
-   */
-
-  connect() {
-    this.editDefinitionFormTarget.addEventListener('turbo:submit-end', this.hideForm.bind(this));
-  }
+  static targets = [ 'proposalForm' ]
 
   clearProposalForm() {
-    this.addProposalFormTarget.innerHTML = '';
-  }
-
-  clearEditDefinitionForm() {
-    this.editDefinitionFormTarget.innerHTML = '';
+    this.proposalFormTarget.innerHTML = '';
   }
 
   hideForm(event) {
     if (event.detail.success) {
-      this.editDefinitionFormTarget.innerHTML = '';
+      this.clearProposalForm();
     }
   }
 }
