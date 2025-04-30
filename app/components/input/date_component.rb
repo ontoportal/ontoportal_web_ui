2f17fb2622ce25d2849884cbf13eb3ad737bc2fa
mdorf <mdorf@stanford.edu>
2025-04-30 10:58:07 -0700

    temporary fix for #419 - Date field population does not work

--- app/components/input/date_component.rb
+++ app/components/input/date_component.rb
@@ -2,7 +2,7 @@
 
 class Input::DateComponent < Input::InputFieldComponent
   def initialize(label: '', name:, value: Date.today, placeholder: '', error_message: '', helper_text: '', id: nil, max_date: nil)
-    data_flat_picker = { controller: "flatpickr", flatpickr_date_format: "Y-m-d", flatpickr_alt_input: "true", flatpickr_alt_format: "F j, Y"}
+    data_flat_picker = { flatpickr_date_format: "Y-m-d", flatpickr_alt_input: "true", flatpickr_alt_format: "F j, Y"}
     data_flat_picker[:flatpickr_max_date] = max_date if max_date
 
     super(label: label, name: name, value: value,  placeholder: placeholder, error_message: error_message, helper_text: helper_text, data: data_flat_picker, id: id)
