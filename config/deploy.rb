02a62f5363399995e85cb149bd9efe59de2f33e7
Imad Bourouche <ji_bourouche@esi.dz>
2026-03-16 11:31:13 +0100

    Chore: Agroportal UI changes for migrating to INRAE servers (#1146)
    * install gems without caching in tests
    
    * add rubocop to the test group
    
    * update Gemfile.lock
    
    * hotfix: fix agents table id in admin page
    
    * change deployment configs for migrating to inrae servers
    
    * change puma configurations
    
    * add bundle flags to add verbose

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,29 +13,25 @@ set :deploy_via, :remote_cache
 
 
 # Default deploy_to directory is /var/www/my_app
-set :deploy_to, "/srv/ontoportal/#{fetch(:application)}"
-
-# Default value for :scm is :git
-# set :scm, :git
-
-# Default value for :format is :pretty
-# set :format, :pretty
+set :deploy_to, "/opt/ontoportal/#{fetch(:application)}"
 
 # Default value for :log_level is :debug
 set :log_level, :error
 
-# Default value for :pty is false
-# set :pty, true
-
 # Default value for :linked_files is []
-# set :linked_files, %w{config/bioportal_config.rb config/database.yml public/robots.txt}
+append :linked_files, 'config/database.yml', 'config/bioportal_config_appliance.rb'
+append :linked_files, 'config/secrets.yml', 'config/credentials/appliance.key', 'config/credentials/appliance.yml.enc'
 
 # Default value for linked_dirs is []
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache public/system public/assets config/locales}
-set :linked_dirs, %w{log tmp/pids tmp/cache public/system public/assets}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets public/system public/assets}
 
 # Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+set :default_env, {
+  'PATH' => "/usr/local/rbenv/shims:/usr/local/rbenv/bin:/usr/bin:$PATH"
+}
+
+# set bundle options
+set :bundle_flags, "--verbose"
 
 # Default value for keep_releases is 5
 set :keep_releases, 5
@@ -47,71 +43,22 @@ set :config_folder_path, "#{fetch(:application)}/#{fetch(:stage)}"
 set :assets_roles, [:web, :app]
 set :keep_assets, 3
 
-# If you want to restart using `touch tmp/restart.txt`, add this to your config/deploy.rb:
+# SSH_JUMPHOST = ENV.include?('SSH_JUMPHOST') ? ENV['SSH_JUMPHOST'] : 'jumpbox.hostname.com'
+# SSH_JUMPHOST_USER = ENV.include?('SSH_JUMPHOST_USER') ? ENV['SSH_JUMPHOST_USER'] : 'username'
+# JUMPBOX_PROXY = "#{SSH_JUMPHOST_USER}@#{SSH_JUMPHOST}"
 
-set :passenger_restart_with_touch, true
-# If you want to restart using `passenger-config restart-app`, add this to your config/deploy.rb:
-# set :passenger_restart_with_touch, false # Note that `nil` is NOT the same as `false` here
-# If you don't set `:passenger_restart_with_touch`, capistrano-passenger will check what version of passenger you are running
-# and use `passenger-config restart-app` if it is available in that version.
-
-# you can set custom ssh options
-# it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
-# you can see them in [net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start)
-# set it globally
-#  set :ssh_options, {
-#    keys: %w(/home/rlisowski/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
-# and/or per server
-# server 'example.com',
-#   user: 'user_name',
-#   roles: %w{web app},
-#   ssh_options: {
-#     user: 'user_name', # overrides user setting above
-#     keys: %w(/home/user_name/.ssh/id_rsa),
-#     forward_agent: false,
-#     auth_methods: %w(publickey password)
-#     # password: 'please use keys'
-#   }
-# setting per server overrides global ssh_options
-
-SSH_JUMPHOST = ENV.include?('SSH_JUMPHOST') ? ENV['SSH_JUMPHOST'] : 'jumpbox.hostname.com'
-SSH_JUMPHOST_USER = ENV.include?('SSH_JUMPHOST_USER') ? ENV['SSH_JUMPHOST_USER'] : 'username'
-
-JUMPBOX_PROXY = "#{SSH_JUMPHOST_USER}@#{SSH_JUMPHOST}"
 set :ssh_options, {
   user: 'ontoportal',
-  forward_agent: 'true',
-  keys: %w(config/deploy_id_rsa),
-  auth_methods: %w(publickey),
-  # use ssh proxy if UI servers are on a private network
-  proxy: Net::SSH::Proxy::Command.new("ssh #{JUMPBOX_PROXY} -W %h:%p")
+  # forward_agent: 'true',
+  # keys: %w(config/deploy_id_rsa),
+  # auth_methods: %w(publickey),
+  # proxy: Net::SSH::Proxy::Command.new("ssh #{JUMPBOX_PROXY} -W %h:%p")
 }
 
 #private git repo for configuraiton
-PRIVATE_CONFIG_REPO = ENV.include?('PRIVATE_CONFIG_REPO') ? ENV['PRIVATE_CONFIG_REPO'] : 'https://your_github_pat_token@github.com/your_organization/ontoportal-configs.git'
-desc "Check if agent forwarding is working"
-task :forwarding do
-  on roles(:all) do |h|
-    if test("env | grep SSH_AUTH_SOCK")
-      info "Agent forwarding is up to #{h}"
-    else
-      error "Agent forwarding is NOT up to #{h}"
-    end
-  end
-end
+# PRIVATE_CONFIG_REPO = ENV.include?('PRIVATE_CONFIG_REPO') ? ENV['PRIVATE_CONFIG_REPO'] : 'https://your_github_pat_token@github.com/your_organization/ontoportal-configs.git'
 
 namespace :deploy do
-  desc 'display remote system env vars'
-  task :show_remote_env do
-    on roles(:all) do
-      remote_env = capture("env")
-      puts remote_env
-    end
-  end
-
   desc 'Incorporate the bioportal_conf private repository content'
   # Get cofiguration from repo if PRIVATE_CONFIG_REPO env var is set
   # or get config from local directory if LOCAL_CONFIG_PATH env var is set
@@ -133,21 +80,11 @@ namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      execute :touch, release_path.join('tmp/restart.txt')
+      execute 'sudo systemctl restart ui.service'
     end
   end
 
 
   after :updating, :get_config
   after :publishing, :restart
-
-  after :restart, :clear_cache do
-    on roles(:app), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
 end
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,14 +4,14 @@
 # server in each group is considered to be the first
 # unless any hosts have the primary property set.
 # Don't declare `role :all`, it's a meta role
-role :app, %w{ui1.prd.ontoportal.org ui2.prd.ontoportal.org}
-role :db, %w{ui1.prd.ontoportal.org} # sufficient to run db:migrate only on one system
-set :branch, ENV.include?('BRANCH') ? ENV['BRANCH'] : 'master'
+role :app, %w[agroportal.eu]
+role :db, %w[agroportal.eu] # sufficient to run db:migrate only on one system
+set :branch, ENV.include?('BRANCH') ? ENV['BRANCH'] : 'agroportal-new-deployment-configs'
 # Extended Server Syntax
 # ======================
 # This can be used to drop a more detailed server
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-#server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
+# server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
 set :log_level, :error
