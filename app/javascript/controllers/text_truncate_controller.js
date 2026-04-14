d9dce6eda6154bb8c75dd6f8d07543a083e498a2
Kouicem Wail <kw_kouicem@esi.dz>
2026-04-14 14:48:18 +0200

    Fix: improve views display in summary tab (#1224)
    * Fix: improve views display in summary tab
    
    - Refactor ontology_views to use properties_dropdown
    
    - Apply TextAreaFieldComponent for see more/see less functionality
    
    - Fix visibility bug in text_truncate_controller for closed elements
    
    * refactor: remove redundant comment in text truncate controller connect method

--- app/javascript/controllers/text_truncate_controller.js
+++ app/javascript/controllers/text_truncate_controller.js
@@ -6,8 +6,27 @@ export default class extends ReadMore {
 
     connect() {
         super.connect()
-        if (!this.#isTextClamped()) {
+
+        this.resizeObserver = new ResizeObserver(() => {
+            if (this.contentTarget.clientHeight > 0 && !this.open) {
+                if (!this.#isTextClamped()) {
+                    this.#hideButton()
+                } else {
+                    this.buttonTarget.style.display = ''
+                }
+            }
+        })
+
+        this.resizeObserver.observe(this.contentTarget)
+
+        if (this.contentTarget.clientHeight > 0 && !this.#isTextClamped()) {
             this.#hideButton()
+        }
+    }
+
+    disconnect() {
+        if (this.resizeObserver) {
+            this.resizeObserver.disconnect()
         }
     }
 
