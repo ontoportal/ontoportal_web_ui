f4467b07aa26fb1a9c2cfa7f54bb8aeb67b2f77d
Kouicem Wail <kw_kouicem@esi.dz>
2026-04-27 11:28:36 +0200

    Fix/simplify views display summary (#1236)
    * Fix: improve views display in summary tab
    
    - Refactor ontology_views to use properties_dropdown
    
    - Apply TextAreaFieldComponent for see more/see less functionality
    
    - Fix visibility bug in text_truncate_controller for closed elements
    
    * refactor: remove redundant comment in text truncate controller connect method
    
    * Refactor see more component to support configurable line display
    
    * Format view name to include acronym and capitalize first letter
    
    * Move default line clamp value from CSS to component
    
    * Revert views section layout to restore 'Create new view' button
    
    * style: refine views section presentation with dropdown and formatted titles
    
    * fix(XSS): secure view links and refine UI styling
    
    ---------
    
    Co-authored-by: Mohamed El Amine BOUKERFA <boukerfa.ma@gmail.com>

--- app/decorators/view_decorator.rb
+++ app/decorators/view_decorator.rb
@@ -5,10 +5,6 @@ class ViewDecorator
     @view, @view_context = view, view_context
   end
 
-  def linked_name
-    view_context.link_to(view.name, view_context.ontology_path(view.acronym))
-  end
-
   def description
     latest_submission = view.explore.latest_submission
     latest_submission.nil? ? "No description provided" : latest_submission.description
